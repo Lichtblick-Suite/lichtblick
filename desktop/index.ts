90957b8fc2b87cd6f33b44f2be395ba886aaa095
Roman Shtylman <roman@foxglove.dev>
2021-03-05 13:09:59 -0800

    Quit app when all windows closed (#161)

--- desktop/index.ts
+++ desktop/index.ts
@@ -273,11 +273,7 @@ app.on("ready", async () => {
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  app.quit();
 });
