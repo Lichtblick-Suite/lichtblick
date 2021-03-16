87e809af0bd447632729aa05fcc760d7eef79a7a
Roman Shtylman <roman@foxglove.dev>
2021-03-15 19:25:53 -0700

    Fix wasm loading for rosmsg-bobject (#266)

--- packages/rosmsg-bobject/bin/translator.js
+++ packages/rosmsg-bobject/bin/translator.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-// @ts-nocheck
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
@@ -4709,4 +4708,4 @@ var Module = (function () {
     return Module.ready;
   };
 })();
-export default Module;
+module.exports = Module;
