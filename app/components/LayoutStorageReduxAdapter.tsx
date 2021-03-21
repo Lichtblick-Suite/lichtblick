f9341342b76f57bd650b5ac4b14fb14b96c9ab33
Roman Shtylman <roman@foxglove.dev>
2021-03-21 16:02:08 -0700

    Debounce layout saving in LayoutStorageReduxAdapter (#330)
    Avoids persisting the layout on every tiny change while the user is
    editing the layout.

--- app/components/LayoutStorageReduxAdapter.tsx
+++ app/components/LayoutStorageReduxAdapter.tsx
@@ -5,6 +5,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useAsync } from "react-use";
+import { useDebounce } from "use-debounce";
 import { v4 as uuidv4 } from "uuid";
 
 import { loadLayout } from "@foxglove-studio/app/actions/panels";
@@ -15,8 +16,11 @@ import sendNotification from "@foxglove-studio/app/util/sendNotification";
 
 // LayoutStorageReduxAdapter persists the current panel state from redux to the current LayoutStorage context
 export default function LayoutStorageReduxAdapter() {
-  const { panels } = useSelector((state: State) => ({ panels: state.persistedState.panels }));
+  const panelsState = useSelector((state: State) => state.persistedState.panels);
   const dispatch = useDispatch();
+
+  // Debounce the panel state to avoid persisting the layout constantly as the user is adjusting it
+  const [debouncedState] = useDebounce(panelsState, 1000 /* 1 second */);
 
   const layoutStorage = useLayoutStorage();
   const enableLayoutManagement = useExperimentalFeature("layoutManagement");
@@ -27,19 +31,19 @@ export default function LayoutStorageReduxAdapter() {
       return;
     }
 
-    if (panels.id === undefined) {
+    if (debouncedState.id === undefined) {
       return;
     }
 
     const layout = {
-      id: panels.id,
-      name: panels.name ?? "unnamed",
-      state: panels,
+      id: debouncedState.id,
+      name: debouncedState.name ?? "unnamed",
+      state: debouncedState,
     };
 
     // save to our storage
     await layoutStorage.put(layout);
-  }, [enableLayoutManagement, layoutStorage, panels]);
+  }, [enableLayoutManagement, layoutStorage, debouncedState]);
 
   // set an id if panel is missing one
   useEffect(() => {
@@ -47,12 +51,12 @@ export default function LayoutStorageReduxAdapter() {
       return;
     }
 
-    if (panels.id === undefined) {
-      panels.id = uuidv4();
-      panels.name = panels.name ?? "unnamed";
-      dispatch(loadLayout(panels));
+    if (panelsState.id === undefined) {
+      panelsState.id = uuidv4();
+      panelsState.name = panelsState.name ?? "unnamed";
+      dispatch(loadLayout(panelsState));
     }
-  }, [dispatch, enableLayoutManagement, panels]);
+  }, [dispatch, enableLayoutManagement, panelsState]);
 
   useEffect(() => {
     if (error) {
