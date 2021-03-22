2af4153bb141d7c98c9c5d23208461a0ee7c6852
Roman Shtylman <roman@foxglove.dev>
2021-03-21 17:12:35 -0700

    Remove @ts-nocheck in app/reducers/userNodes.ts (#332)

--- app/reducers/userNodes.ts
+++ app/reducers/userNodes.ts
@@ -11,13 +11,6 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
-// @ts-nocheck
-// Note(Roman): I able unable to figure out what type UserNodeDiagnostics is suppose to have.
-// It seems that it is defined here as a type with a diagnostics and logs field but the code below
-// also indexes with nodeId into the state object. There is another UserNodeDiagnotics type in
-// players/UserNodePlayer/types which does have a specification for lookup by nodeId string.
-// But that type is not what is used in the State
-
 import { ActionTypes } from "@foxglove-studio/app/actions";
 import { Diagnostic, UserNodeLog } from "@foxglove-studio/app/players/UserNodePlayer/types";
 import { State } from "@foxglove-studio/app/reducers";
@@ -32,7 +25,7 @@ export default function userNodes(state: State, action: ActionTypes): State {
     case "SET_USER_NODE_DIAGNOSTICS": {
       const userNodeDiagnostics = { ...state.userNodes.userNodeDiagnostics } as any;
       Object.keys(action.payload.diagnostics).forEach((nodeId) => {
-        const payloadDiagnostics = action.payload.diagnostics[nodeId].diagnostics;
+        const payloadDiagnostics = action.payload.diagnostics[nodeId]?.diagnostics;
         if (action.payload.diagnostics[nodeId] === undefined) {
           delete userNodeDiagnostics[nodeId];
         } else if (!userNodeDiagnostics[nodeId]) {
@@ -50,15 +43,15 @@ export default function userNodes(state: State, action: ActionTypes): State {
     case "ADD_USER_NODE_LOGS": {
       const userNodeDiagnostics = { ...state.userNodes.userNodeDiagnostics };
       for (const nodeId of Object.keys(action.payload)) {
-        const existingLogs = userNodeDiagnostics[nodeId] && userNodeDiagnostics[nodeId].logs;
-        const payloadLogs = action.payload[nodeId].logs;
+        const existingLogs = (userNodeDiagnostics as any)[nodeId]?.logs;
+        const payloadLogs = action.payload[nodeId]?.logs;
         if (action.payload[nodeId] === undefined) {
-          delete userNodeDiagnostics[nodeId];
-        } else if (!userNodeDiagnostics[nodeId]) {
-          userNodeDiagnostics[nodeId] = { diagnostics: [], logs: payloadLogs };
+          delete (userNodeDiagnostics as any)[nodeId];
+        } else if (!(userNodeDiagnostics as any)[nodeId]) {
+          (userNodeDiagnostics as any)[nodeId] = { diagnostics: [], logs: payloadLogs };
         } else {
-          userNodeDiagnostics[nodeId] = {
-            ...userNodeDiagnostics[nodeId],
+          (userNodeDiagnostics as any)[nodeId] = {
+            ...(userNodeDiagnostics as any)[nodeId],
             logs: existingLogs.concat(payloadLogs),
           };
         }
@@ -69,8 +62,11 @@ export default function userNodes(state: State, action: ActionTypes): State {
     case "CLEAR_USER_NODE_LOGS": {
       const userNodeDiagnostics = { ...state.userNodes.userNodeDiagnostics };
       const nodeId = action.payload;
-      if (userNodeDiagnostics[nodeId]) {
-        userNodeDiagnostics[nodeId] = { ...userNodeDiagnostics[nodeId], logs: [] };
+      if ((userNodeDiagnostics as any)[nodeId]) {
+        (userNodeDiagnostics as any)[nodeId] = {
+          ...(userNodeDiagnostics as any)[nodeId],
+          logs: [],
+        };
       }
       return { ...state, userNodes: { ...state.userNodes, userNodeDiagnostics } };
     }
