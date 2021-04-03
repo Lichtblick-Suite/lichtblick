3dc01442b83a9ff3467b9add8bbc986d353aa884
Adrian Macneil <adrian@foxglove.dev>
2021-04-03 16:45:39 -0700

    Add script to bump nightly version (#460)

---
+++ ci/bump-nightly-version.ts
@@ -0,0 +1,37 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+// This script will update "version" in package.json to something like "0.3.0-nightly.20210403.7b01783"
+// Any existing version string extras like "-dev" are removed.
+
+import assert from "assert";
+import { readFile, writeFile } from "fs/promises";
+import path from "path";
+
+import { execOutput } from "./exec";
+
+const PACKAGE_JSON_PATH = path.join(__dirname, "..", "package.json");
+
+export default async function main(): Promise<void> {
+  // Parse package.json
+  const pkg = JSON.parse(await readFile(PACKAGE_JSON_PATH, "utf8"));
+
+  // Generate new package version
+  const ver = (pkg.version as string).replace(/-.*$/, "");
+  const sha = (await execOutput("git", ["rev-parse", "--short", "HEAD"])).trim();
+  const date = new Date().toISOString().replace(/T.*$/, "").replace(/-/g, "");
+
+  assert.ok(ver, "Missing package.json version");
+  assert.ok(sha, "Missing git HEAD shortref");
+
+  pkg.version = `${ver}-nightly.${date}.${sha}`;
+
+  // Write package.json
+  await writeFile(PACKAGE_JSON_PATH, JSON.stringify(pkg, undefined, 2) + "\n", "utf8");
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
---
+++ ci/exec.ts
@@ -0,0 +1,26 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { group } from "@actions/core";
+import { exec as execAction } from "@actions/exec";
+
+export default async function exec(program: string, args?: string[]): Promise<void> {
+  await group(`$ ${program} ${args?.join(" ")}`, async () => {
+    await execAction(program, args);
+  });
+}
+
+export async function execOutput(program: string, args?: string[]): Promise<string> {
+  let output = "";
+  await execAction(program, args, {
+    silent: true,
+    listeners: {
+      stdout: (data) => {
+        output += data.toString();
+      },
+    },
+  });
+
+  return output;
+}
