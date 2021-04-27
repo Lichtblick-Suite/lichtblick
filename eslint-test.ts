83ddcb66c24b4fefec3c43a01e54b992f6612ff6
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-04-26 14:06:25 -1000

    Ensure tests are being typechecked (#671)
    Adds a top level tsc --noEmit to the lint step in CI to catch type errors in files that would otherwise be executed under babel-jest or otherwise not run through the TypeScript compiler.

--- eslint-test.ts
+++ eslint-test.ts
@@ -5,11 +5,14 @@
 // This file is not used in the app, but exists to test that our eslint config catches the code we want it to catch.
 // Because we run eslint with --report-unused-disable-directives, any unused comments will be errors.
 
+export {}; // for isolatedModules
+
 ({
   // eslint-disable-next-line no-restricted-syntax
   get x() {
     return 1;
   },
+  // @ts-expect-error unused variable
   // eslint-disable-next-line no-restricted-syntax
   set x(newX) {},
 });
@@ -48,4 +51,5 @@ void (async () => {
   str == wut; // eslint-disable-line no-restricted-syntax
 };
 
+// @ts-expect-error unused function
 function useEffectOnce() {} // eslint-disable-line id-denylist, @typescript-eslint/no-unused-vars
