528a913df7f5d3bd2095f96c73f071f879f5ed1a
John Hurliman <jhurliman@jhurliman.org>
2021-04-29 17:32:11 -0700

    Add mDNS caching and jitter to retries, broadcast mDNS requests on all network interfaces (#807)
    * Add mDNS caching and jitter to retries, only broadcast one request per hostname at a time
    
    * Broadcast mDNS request on all interfaces

--- packages/electron-socket/src/preloader/dns.ts
+++ packages/electron-socket/src/preloader/dns.ts
@@ -3,8 +3,10 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import dns from "dns";
-import type packet from "dns-packet";
-import MulticastDns from "multicast-dns";
+
+import { MDnsResolver } from "./MDnsResolver";
+
+const mdnsResolver = new MDnsResolver();
 
 const getaddrinfo = dns.lookup.bind(dns);
 
@@ -21,47 +23,16 @@ export function dnsLookup(
   }
 }
 
-export function mdnsLookup(
+export async function mdnsLookup(
   hostname: string,
   options: dns.LookupOneOptions,
   // eslint-disable-next-line no-restricted-syntax
   callback: (err: NodeJS.ErrnoException | null, address: string, family: number) => void,
-): void {
-  const MAX_ATTEMPTS = 5;
-
-  const mdns = MulticastDns({ reuseAddr: true });
-  const recordType = options.family === 6 ? "AAAA" : "A";
-  let attempts = 0;
-  const timer = setInterval(query, 1000);
-  query();
-
-  mdns.on("response", (res: packet.Packet) => {
-    if (res.answers == undefined) {
-      // Ignore this response, wait and see if another comes in
-      return;
-    }
-
-    for (const answer of res.answers) {
-      if (answer.name === hostname && answer.type === recordType) {
-        cleanup();
-        // eslint-disable-next-line no-restricted-syntax
-        callback(null, answer.data, answer.type === "AAAA" ? 6 : 4);
-      }
-    }
-  });
-
-  function cleanup() {
-    mdns.destroy();
-    clearInterval(timer);
+): Promise<void> {
+  const response = await mdnsResolver.mdnsLookup(hostname, options);
+  if (response == undefined) {
+    return callback(new Error(`mDNS resolution timed out for "${hostname}"`), "", 0);
   }
-
-  function query() {
-    if (++attempts >= MAX_ATTEMPTS) {
-      cleanup();
-      callback(new Error("Query timed out"), "", 0);
-      return;
-    }
-
-    mdns.query([{ type: recordType, name: hostname }]);
-  }
+  // eslint-disable-next-line no-restricted-syntax
+  callback(null, response.address, response.family);
 }
