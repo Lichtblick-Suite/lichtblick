0c4b888fb4a265b1e3971cac56871ea8bc3ff80a
Roman Shtylman <roman@foxglove.dev>
2021-05-13 13:47:05 -0700

    Fix macOS open window from dock (#941)
    The change to add support for multiple windows broke launching
    a new window from the macOS dock when no windows were open. This
    change fixes the bug by calling "load" on the new studio window instance.
    
    Fixes #939

--- desktop/main/index.ts
+++ desktop/main/index.ts
@@ -281,7 +281,7 @@ app.on("ready", async () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
-      new StudioWindow();
+      new StudioWindow().load();
     }
   });
 
