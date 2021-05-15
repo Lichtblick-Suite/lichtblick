dae51bdad67590fe04832811f39c3f0fa78f1eac
Roman Shtylman <roman@foxglove.dev>
2021-05-15 08:04:45 -0700

    Remove globals.d.ts from desktop/renderer (#977)
    Replace with a reference entry in app/index.ts

--- app/index.ts
+++ app/index.ts
@@ -2,6 +2,12 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+// Bring in global modules and overrides required by studio source files
+// This adds type declarations for scss, bag, etc imports
+// This adds type declarations for global react
+// See typings/index.d.ts for additional included references
+/// <reference types="./typings" />
+
 import App from "@foxglove-studio/app/App";
 import ErrorBoundary from "@foxglove-studio/app/components/ErrorBoundary";
 import MultiProvider from "@foxglove-studio/app/components/MultiProvider";
