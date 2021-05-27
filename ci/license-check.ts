301cf956c5c45e264603391943ca60d0cc39863b
Adrian Macneil <adrian@foxglove.dev>
2021-05-27 14:37:12 -0700

    Move license-check to CI script (#1088)

---
+++ ci/license-check.ts
@@ -0,0 +1,52 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import checker from "license-checker";
+import path from "path";
+import { promisify } from "util";
+
+const initChecker = promisify(checker.init);
+
+const ALLOWED_LICENSES = [
+  "MPL-2.0",
+  "MIT",
+  "BSD",
+  "BSD-2-clause",
+  "Apache",
+  "ISC",
+  "Python-2.0",
+  "PSF",
+  "CC-BY",
+  "CC0",
+  "Public Domain",
+  "WTFPL",
+  "Unlicense",
+  "OFL-1.1",
+];
+
+const EXCLUDED_PACKAGES = [
+  "@react-leaflet/core@1.1.0",
+  "gl-vec3@1.1.3",
+  "pngparse@2.0.1",
+  "react-leaflet@3.2.0",
+];
+
+async function main() {
+  const output = await initChecker({
+    start: path.join(__dirname, ".."),
+    summary: true,
+    onlyAllow: ALLOWED_LICENSES.join(";"),
+    excludePackages: EXCLUDED_PACKAGES.join(";"),
+    excludePrivatePackages: true,
+    color: false,
+  });
+
+  // eslint-disable-next-line
+  console.log((checker as any).asSummary(output));
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
