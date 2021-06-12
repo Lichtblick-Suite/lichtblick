fdd486a54ab28fcbe6a57c5c76e41c81edc736bf
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-06-11 17:06:26 -1000

    Upgrade jest (#1207)
    jest-circus is now the default runner so it can be removed from package.json and config files.

--- packages/studio-base/src/test/setupTestFramework.ts
+++ packages/studio-base/src/test/setupTestFramework.ts
@@ -11,7 +11,7 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
-import diff from "jest-diff";
+import { diff } from "jest-diff";
 import { isEqual } from "lodash";
 
 import {
