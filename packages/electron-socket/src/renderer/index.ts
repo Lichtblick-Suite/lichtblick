db91c54ec36c983e279e98c4f4373bd5dcc2008b
Adrian Macneil <adrian@foxglove.dev>
2021-06-30 09:47:50 -0700

    Extract @foxglove/electron-socket package (#1343)

--- packages/electron-socket/src/renderer/index.ts
+++
@@ -1,8 +0,0 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/
-
-export * from "./Sockets";
-export * from "./HttpServerRenderer";
-export * from "./TcpServerRenderer";
-export * from "./TcpSocketRenderer";
