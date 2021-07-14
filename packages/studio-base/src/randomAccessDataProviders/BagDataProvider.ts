cf451383cc37aff43c96eeaa5ba14ced0ec7701e
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-07-14 16:45:16 -0600

    Switch to external `@foxglove/wasm-bz2` package (#1455)
    Migrate to the new external package https://github.com/foxglove/wasm-bz2

--- packages/studio-base/src/randomAccessDataProviders/BagDataProvider.ts
+++ packages/studio-base/src/randomAccessDataProviders/BagDataProvider.ts
@@ -114,6 +114,7 @@ export default class BagDataProvider implements RandomAccessDataProvider {
   _bag?: Bag;
   _lastPerformanceStatsToLog?: TimedDataThroughput;
   _extensionPoint?: ExtensionPoint;
+  private bzip2?: Bzip2;
 
   constructor(options: Options, children: RandomAccessDataProviderDescriptor[]) {
     if (children.length > 0) {
@@ -126,7 +127,7 @@ export default class BagDataProvider implements RandomAccessDataProvider {
     this._extensionPoint = extensionPoint;
     const { bagPath, cacheSizeInBytes } = this._options;
     await decompressLZ4.isLoaded;
-    await Bzip2.isLoaded;
+    this.bzip2 = await Bzip2.init();
 
     try {
       if (bagPath.type === "remoteBagUrl") {
@@ -319,8 +320,11 @@ export default class BagDataProvider implements RandomAccessDataProvider {
       noParse: true,
       decompress: {
         bz2: (buffer: Buffer, size: number) => {
+          if (!this.bzip2) {
+            throw new Error("bzip2 not initialized");
+          }
           try {
-            return Buffer.from(Bzip2.decompress(buffer, size, { small: false }));
+            return Buffer.from(this.bzip2.decompress(buffer, size, { small: false }));
           } catch (error) {
             reportMalformedError("bz2 decompression", error);
             throw error;
--- packages/wasm-bz2/wasm/module.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a5ead211a28aee0c7cd2dc1a87e67892ef22963649f8f1ece1cd2ad04799b84d
-size 98763
