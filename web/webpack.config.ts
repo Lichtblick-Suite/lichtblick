94ddd7b9e93fd82841383cbc273410dd95361ba9
Roman Shtylman <roman@foxglove.dev>
2021-07-15 12:44:16 -0600

    Add fathom stats to web build (#1468)

--- web/webpack.config.ts
+++ web/webpack.config.ts
@@ -120,6 +120,8 @@ const mainConfig = (env: unknown, argv: WebpackArgv): Configuration => {
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <title>Foxglove Studio</title>
+      
+      <script src="https://cdn.usefathom.com/script.js" data-site="NJCSCNBX" defer></script>
     </head>
     <script>
       global = globalThis;
