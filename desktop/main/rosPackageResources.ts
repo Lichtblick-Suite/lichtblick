ccdab877da255397cf1e5d0f0d0f079aa199e957
Adrian Macneil <adrian@foxglove.dev>
2021-08-23 22:00:12 -0700

    Upgrade xmldom (#1695)

--- desktop/main/rosPackageResources.ts
+++ desktop/main/rosPackageResources.ts
@@ -1,12 +1,12 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
+import { DOMParser } from "@xmldom/xmldom";
 import { protocol } from "electron";
 import { promises as fs } from "fs";
 import path from "path";
 import { PNG } from "pngjs";
 import UTIF from "utif";
-import { DOMParser } from "xmldom";
 
 import Logger from "@foxglove/log";
 
