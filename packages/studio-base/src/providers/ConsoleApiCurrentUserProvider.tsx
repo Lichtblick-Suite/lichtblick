fe0d96c257a31916b1dd9463f6f39d25aaae3959
Roman Shtylman <roman@foxglove.dev>
2021-09-13 10:58:29 -0700

    Fix: keep the user signed in when opening the app offline (#1836)
    If the user is currently offline, studio will still open and show their
    account information.
    
    Fixes: #1819

--- packages/studio-base/src/providers/ConsoleApiCurrentUserProvider.tsx
+++ packages/studio-base/src/providers/ConsoleApiCurrentUserProvider.tsx
@@ -2,19 +2,21 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { PropsWithChildren, useCallback, useState } from "react";
+import { PropsWithChildren, useCallback, useMemo } from "react";
 import { useAsync, useLocalStorage } from "react-use";
 
 import { useShallowMemo } from "@foxglove/hooks";
 import Logger from "@foxglove/log";
 import { useConsoleApi } from "@foxglove/studio-base/context/ConsoleApiContext";
-import CurrentUserContext from "@foxglove/studio-base/context/CurrentUserContext";
+import CurrentUserContext, { User } from "@foxglove/studio-base/context/CurrentUserContext";
 
 const log = Logger.getLogger(__filename);
 
 /**
  * CurrentUserProvider attempts to load the current user's profile if there is an authenticated
- * session
+ * session.
+ *
+ * The provider also exposes function to set and clear the current session token.
  */
 export default function ConsoleApiCurrentUserProvider(
   props: PropsWithChildren<unknown>,
@@ -22,31 +24,54 @@ export default function ConsoleApiCurrentUserProvider(
   const api = useConsoleApi();
   const [bearerToken, setBearerToken, removeBearerToken] =
     useLocalStorage<string>("fox.bearer-token");
-  const [completedFirstLoad, setCompletedFirstLoad] = useState(false);
 
-  const { value: currentUser } = useAsync(async () => {
+  // We want to support starting the app while offline. Various children components of the provider
+  // use the presence of the user as an indicator of sign-in state. We cache the user to provide a
+  // stale-while-loading value so the app can open with no connection and update the cached record
+  // once online.
+  const [cachedCurrentUser, setCachedCurrentUser, removeCachedCurrentUser] = useLocalStorage<User>(
+    "fox.current-user",
+    undefined,
+    {
+      raw: false,
+      serializer: (value: User) => JSON.stringify(value),
+      deserializer: (value: string) => JSON.parse(value) as User,
+    },
+  );
+
+  // When we have a valid token, we need to set the api auth header so child components can make
+  // authenticated requests
+  useMemo(() => {
+    if (!bearerToken) {
+      return;
+    }
+    api.setAuthHeader(`Bearer ${bearerToken}`);
+  }, [api, bearerToken]);
+
+  const { loading } = useAsync(async () => {
     try {
       if (!bearerToken) {
         return undefined;
       }
-      api.setAuthHeader(`Bearer ${bearerToken}`);
-      return await api.me();
+      const me = await api.me();
+      setCachedCurrentUser(me);
+      return me;
     } catch (error) {
       log.error(error);
       return undefined;
-    } finally {
-      setCompletedFirstLoad(true);
     }
-  }, [api, bearerToken]);
+  }, [api, bearerToken, setCachedCurrentUser]);
 
   const signOut = useCallback(async () => {
     removeBearerToken();
+    removeCachedCurrentUser();
     await api.signout();
-  }, [api, removeBearerToken]);
+  }, [api, removeBearerToken, removeCachedCurrentUser]);
 
-  const value = useShallowMemo({ currentUser, setBearerToken, signOut });
+  const value = useShallowMemo({ currentUser: cachedCurrentUser, setBearerToken, signOut });
 
-  if (!completedFirstLoad) {
+  // Wait for first time loading to complete
+  if (!cachedCurrentUser && loading) {
     return <></>;
   }
 
