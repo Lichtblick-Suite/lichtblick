5d97d62d38184d0cfb63b0ffb626368d1c879bc9
Andrew Carr <2metres@users.noreply.github.com>
2021-09-15 08:39:23 +1000

    Refactor ExpandingToolbar component to use CSS-in-JS (#1852)

--- packages/studio-base/src/components/ExpandingToolbar.tsx
+++ packages/studio-base/src/components/ExpandingToolbar.tsx
@@ -11,25 +11,60 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
+import { mergeStyleSets } from "@fluentui/merge-styles";
 import ArrowCollapseIcon from "@mdi/svg/svg/arrow-collapse.svg";
 import cx from "classnames";
 import { ReactElement } from "react";
-import styled from "styled-components";
 
 import Button from "@foxglove/studio-base/components/Button";
 import Flex from "@foxglove/studio-base/components/Flex";
 import Icon from "@foxglove/studio-base/components/Icon";
-
-import styles from "./ExpandingToolbar.module.scss";
+import { colors } from "@foxglove/studio-base/util/sharedStyleConstants";
 
 const PANE_WIDTH = 268;
 const PANE_HEIGHT = 240;
 
-export const SToolGroupFixedSizePane = styled.div`
-  overflow-x: hidden;
-  overflow-y: auto;
-  padding: 8px 0;
-`;
+const classes = mergeStyleSets({
+  expandButton: {
+    backgroundColor: "transparent !important",
+    border: "none !important",
+    padding: "8px 4px !important",
+  },
+  iconButton: {
+    backgroundColor: "transparent !important",
+    border: "none !important",
+    padding: "8px 4px !important",
+    alignItems: "start !important",
+    marginRight: "4px !important",
+    marginLeft: "4px !important",
+  },
+  tab: {
+    margin: "0",
+    paddingLeft: "8px !important",
+    paddingRight: "8px !important",
+    backgroundColor: "transparent !important",
+    border: "none !important",
+    borderRadius: "0 !important",
+    borderTop: "2px solid transparent !important",
+    borderBottom: "2px solid transparent !important",
+  },
+  tabSelected: {
+    borderBottom: `2px solid ${colors.TEXT_NORMAL} !important`,
+  },
+  fixedSizePane: {
+    height: PANE_HEIGHT,
+    width: PANE_WIDTH - 28,
+    overflow: "hidden auto",
+    padding: "8px 0",
+  },
+  tabBar: {
+    justifyContent: "space-between",
+  },
+  tabBody: {
+    backgroundColor: colors.DARK,
+    padding: "4px 12px 12px 12px",
+  },
+});
 
 export function ToolGroup<T>({ children }: { name: T; children: React.ReactElement }): JSX.Element {
   return children;
@@ -40,11 +75,7 @@ export function ToolGroupFixedSizePane({
 }: {
   children: React.ReactElement | React.ReactElement[];
 }): JSX.Element {
-  return (
-    <SToolGroupFixedSizePane style={{ width: PANE_WIDTH - 28, height: PANE_HEIGHT }}>
-      {children}
-    </SToolGroupFixedSizePane>
-  );
+  return <div className={classes.fixedSizePane}>{children}</div>;
 }
 
 type Props<T extends string> = {
@@ -80,7 +111,7 @@ export default function ExpandingToolbar<T extends string>({
     return (
       <div data-test={dataTest} className={className}>
         <Button
-          className={styles.iconButton}
+          className={classes.iconButton}
           tooltip={tooltip}
           onClick={() => onSelectTab(selectedTabLocal)}
         >
@@ -96,13 +127,15 @@ export default function ExpandingToolbar<T extends string>({
     }
   });
   return (
-    <div data-test={dataTest} className={cx(className, styles.expanded)}>
-      <Flex row className={styles.tabBar}>
+    <div data-test={dataTest} className={className}>
+      <Flex row className={classes.tabBar}>
         <Flex row>
           {React.Children.map(children, (child) => {
             return (
               <Button
-                className={cx(styles.tab, { [styles.selected!]: child === selectedChild })}
+                className={cx(classes.tab, {
+                  [classes.tabSelected]: child === selectedChild,
+                })}
                 onClick={() => onSelectTab(child.props.name as T)}
               >
                 {child.props.name}
@@ -110,13 +143,13 @@ export default function ExpandingToolbar<T extends string>({
             );
           })}
         </Flex>
-        <Button className={styles.expandButton} onClick={() => onSelectTab(undefined)}>
+        <Button className={classes.expandButton} onClick={() => onSelectTab(undefined)}>
           <Icon>
             <ArrowCollapseIcon />
           </Icon>
         </Button>
       </Flex>
-      <div className={styles.tabBody} style={style}>
+      <div className={classes.tabBody} style={style}>
         {selectedChild}
       </div>
     </div>
