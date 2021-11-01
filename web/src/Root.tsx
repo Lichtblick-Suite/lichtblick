e367b3864c6f1ade35110cbfd63d48e549de20a0
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-11-01 13:18:59 -0400

    Add more explanation for unavailable data sources in web app (#2056)
    **User-Facing Changes**
    None
    
    **Description**
    Per https://github.com/foxglove/studio/issues/2049, add some text to explain why the native connections are not available in the web app.

--- web/src/Root.tsx
+++ web/src/Root.tsx
@@ -2,7 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { Link } from "@fluentui/react";
+import { Link, Text } from "@fluentui/react";
 import { useMemo } from "react";
 
 import {
@@ -34,10 +34,19 @@ export function Root({ loadWelcomeLayout }: { loadWelcomeLayout: boolean }): JSX
       type: "ros1-socket",
       disabledReason: (
         <>
-          ROS 1 Native connections are only available in our desktop app.&nbsp;
-          <Link href="https://foxglove.dev/download" target="_blank" rel="noreferrer">
-            Download it here.
-          </Link>
+          <Text block as="p">
+            ROS 1 Native connections are only available in our desktop app.&nbsp;
+            <Link href="https://foxglove.dev/download" target="_blank" rel="noreferrer">
+              Download it here.
+            </Link>
+          </Text>
+          <Text
+            block
+            as="p"
+            styles={(_props, theme) => ({ root: { color: theme.semanticColors.disabledText } })}
+          >
+            Native TCP and UDP sockets are not available in a standard browser environment.
+          </Text>
         </>
       ),
     },
@@ -59,10 +68,19 @@ export function Root({ loadWelcomeLayout }: { loadWelcomeLayout: boolean }): JSX
       badgeText: "beta",
       disabledReason: (
         <>
-          ROS 2 Native connections are only available in our desktop app.&nbsp;
-          <Link href="https://foxglove.dev/download" target="_blank" rel="noreferrer">
-            Download it here.
-          </Link>
+          <Text block as="p">
+            ROS 2 Native connections are only available in our desktop app.&nbsp;
+            <Link href="https://foxglove.dev/download" target="_blank" rel="noreferrer">
+              Download it here.
+            </Link>
+          </Text>
+          <Text
+            block
+            as="p"
+            styles={(_props, theme) => ({ root: { color: theme.semanticColors.disabledText } })}
+          >
+            Native TCP and UDP sockets are not available in a standard browser environment.
+          </Text>
         </>
       ),
     },
@@ -79,10 +97,19 @@ export function Root({ loadWelcomeLayout }: { loadWelcomeLayout: boolean }): JSX
       type: "velodyne-device",
       disabledReason: (
         <>
-          Velodyne connections are only available in our desktop app.&nbsp;
-          <Link href="https://foxglove.dev/download" target="_blank" rel="noreferrer">
-            Download it here.
-          </Link>
+          <Text block as="p">
+            Velodyne connections are only available in our desktop app.&nbsp;
+            <Link href="https://foxglove.dev/download" target="_blank" rel="noreferrer">
+              Download it here.
+            </Link>
+          </Text>
+          <Text
+            block
+            as="p"
+            styles={(_props, theme) => ({ root: { color: theme.semanticColors.disabledText } })}
+          >
+            Native TCP and UDP sockets are not available in a standard browser environment.
+          </Text>
         </>
       ),
     },
