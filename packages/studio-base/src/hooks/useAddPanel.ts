48366d18c09432b43f3c0514192e62033d1b0ae7
foxymiles <93935560+foxymiles@users.noreply.github.com>
2021-11-12 15:03:30 -0500

    Don't open panel settings after adding a new panel via the add panel sidebar. (#2143)

--- packages/studio-base/src/hooks/useAddPanel.ts
+++ packages/studio-base/src/hooks/useAddPanel.ts
@@ -8,20 +8,17 @@ import {
   useCurrentLayoutActions,
   useSelectedPanels,
 } from "@foxglove/studio-base/context/CurrentLayoutContext";
-import { useWorkspace } from "@foxglove/studio-base/context/WorkspaceContext";
 import { getPanelIdForType } from "@foxglove/studio-base/util/layout";
 
 export default function useAddPanel(): (selection: PanelSelection) => void {
   const { addPanel } = useCurrentLayoutActions();
-  const { openPanelSettings } = useWorkspace();
   const { setSelectedPanelIds } = useSelectedPanels();
   return useCallback(
     ({ type, config, relatedConfigs }: PanelSelection) => {
       const id = getPanelIdForType(type);
       addPanel({ id, config, relatedConfigs });
       setSelectedPanelIds([id]);
-      openPanelSettings();
     },
-    [addPanel, setSelectedPanelIds, openPanelSettings],
+    [addPanel, setSelectedPanelIds],
   );
 }
