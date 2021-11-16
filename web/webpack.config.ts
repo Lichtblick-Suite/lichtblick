758a633e0f5d064721c4e511355cef4d8268724b
Esther S. Weon <esther.s.weon@gmail.com>
2021-11-15 18:31:56 -0800

    Add link rel=canonical to web build (#2158)

--- web/webpack.config.ts
+++ web/webpack.config.ts
@@ -131,6 +131,7 @@ const mainConfig = (env: unknown, argv: WebpackArgv): Configuration => {
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="canonical" href="https://studio.foxglove.dev"/>
       <title>Foxglove Studio</title>
     </head>
     <script>
