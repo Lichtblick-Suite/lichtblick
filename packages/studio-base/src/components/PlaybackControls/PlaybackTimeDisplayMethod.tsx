b0f796dbf8e0b2bd9f462ac878e9b91315e0a8fa
Esther S. Weon <esther.s.weon@gmail.com>
2021-12-20 14:58:00 -0800

    Fix padding on playback bar timestamp display (#2409)

--- packages/studio-base/src/components/PlaybackControls/PlaybackTimeDisplayMethod.tsx
+++ packages/studio-base/src/components/PlaybackControls/PlaybackTimeDisplayMethod.tsx
@@ -75,7 +75,7 @@ const PlaybackTimeDisplayMethod = ({
         },
         field: {
           margin: 0,
-          padding: 0,
+          padding: "0px 8px",
           whiteSpace: "nowrap",
           fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, 'zero'`,
           backgroundColor: "transparent",
