2beb4b557b137bbe819651ba09db9b4405d31cad
Roman Shtylman <roman@foxglove.dev>
2022-01-05 12:26:45 -0800

    Remove old ConnectionList sources list code (#2580)
    This code is no longer relevant with the new open dialog workflow.
    
    Fixes: #2578

--- packages/studio-base/src/components/ConnectionList/index.tsx
+++ packages/studio-base/src/components/ConnectionList/index.tsx
@@ -2,29 +2,20 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { ActionButton, Icon, makeStyles, Text, useTheme } from "@fluentui/react";
-import { useCallback, useContext, useMemo } from "react";
+import { Icon, makeStyles, useTheme } from "@fluentui/react";
+import { useCallback, useContext } from "react";
 
-import { AppSetting } from "@foxglove/studio-base/AppSetting";
 import {
   MessagePipelineContext,
   useMessagePipeline,
 } from "@foxglove/studio-base/components/MessagePipeline";
 import NotificationModal from "@foxglove/studio-base/components/NotificationModal";
 import ModalContext from "@foxglove/studio-base/context/ModalContext";
-import {
-  IDataSourceFactory,
-  usePlayerSelection,
-} from "@foxglove/studio-base/context/PlayerSelectionContext";
-import { useAppConfigurationValue } from "@foxglove/studio-base/hooks/useAppConfigurationValue";
-import { useConfirm } from "@foxglove/studio-base/hooks/useConfirm";
-import { PlayerPresence, PlayerProblem } from "@foxglove/studio-base/players/types";
+import { PlayerProblem } from "@foxglove/studio-base/players/types";
 
 import { DataSourceInfo } from "./DataSourceInfo";
 
-const selectPlayerPresence = ({ playerState }: MessagePipelineContext) => playerState.presence;
 const selectPlayerProblems = (ctx: MessagePipelineContext) => ctx.playerState.problems;
-const selectPlayerName = (ctx: MessagePipelineContext) => ctx.playerState.name;
 
 const emptyArray: PlayerProblem[] = [];
 
@@ -48,33 +39,11 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function ConnectionList(): JSX.Element {
-  const [enableOpenDialog] = useAppConfigurationValue(AppSetting.OPEN_DIALOG);
-  const { selectSource, availableSources } = usePlayerSelection();
-  const confirm = useConfirm();
   const modalHost = useContext(ModalContext);
   const theme = useTheme();
   const classes = useStyles();
 
   const playerProblems = useMessagePipeline(selectPlayerProblems) ?? emptyArray;
-  const playerPresence = useMessagePipeline(selectPlayerPresence);
-  const playerName = useMessagePipeline(selectPlayerName);
-
-  const onSourceClick = useCallback(
-    (source: IDataSourceFactory) => {
-      if (source.disabledReason != undefined) {
-        void confirm({
-          title: "Unsupported data source",
-          prompt: source.disabledReason,
-          variant: "primary",
-          cancel: false,
-        });
-        return;
-      }
-
-      selectSource(source.id);
-    },
-    [confirm, selectSource],
-  );
 
   const showProblemModal = useCallback(
     (problem: PlayerProblem) => {
@@ -93,74 +62,9 @@ export default function ConnectionList(): JSX.Element {
     [modalHost],
   );
 
-  // When using the open dialog we don't display the available sources items and instead display
-  // and open button to open the dialog.
-  const sourcesListElements = useMemo(() => {
-    if (enableOpenDialog === true) {
-      return ReactNull;
-    }
-
-    return (
-      <>
-        <Text
-          block
-          styles={{
-            root: { color: theme.palette.neutralTertiary, marginBottom: theme.spacing.l1 },
-          }}
-        >
-          {playerPresence === PlayerPresence.NOT_PRESENT
-            ? "Not connected. Choose a data source below to get started."
-            : playerName}
-        </Text>
-        {availableSources.map((source) => {
-          if (source.hidden === true) {
-            return ReactNull;
-          }
-
-          const iconName: RegisteredIconNames = source.iconName as RegisteredIconNames;
-          return (
-            <div key={source.id}>
-              <ActionButton
-                styles={{
-                  root: {
-                    margin: 0,
-                    padding: 0,
-                    width: "100%",
-                    textAlign: "left",
-                    // sources with a disabled reason are clickable to show the reason
-                    // a lower opacity makes the option look disabled to avoid drawing attention
-                    opacity: source.disabledReason != undefined ? 0.5 : 1,
-                  },
-                }}
-                iconProps={{
-                  iconName,
-                  styles: { root: { "& span": { verticalAlign: "baseline" } } },
-                }}
-                onClick={() => onSourceClick(source)}
-              >
-                {source.displayName}
-                {source.badgeText && <span className={classes.badge}>{source.badgeText}</span>}
-              </ActionButton>
-            </div>
-          );
-        })}
-      </>
-    );
-  }, [
-    availableSources,
-    classes.badge,
-    enableOpenDialog,
-    onSourceClick,
-    playerName,
-    playerPresence,
-    theme.palette.neutralTertiary,
-    theme.spacing.l1,
-  ]);
-
   return (
     <>
-      {sourcesListElements}
-      {enableOpenDialog === true && <DataSourceInfo />}
+      <DataSourceInfo />
       {playerProblems.length > 0 && <hr className={classes.divider} />}
       {playerProblems.map((problem, idx) => {
         const iconName = problem.severity === "error" ? "Error" : "Warning";
