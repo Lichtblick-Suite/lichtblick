Lichtblick-Suite/lichtblick

commit 7fdb70fce0c2adb33d3a0875dd7c1ef1c236e7cb

Fix: remove sendNotification from PlaybackControls slider (#1805)
Instead of notifying when the max/min range is inverted, the slider will
render nothing and log a console error.

This change updates the slider component from a class component to a functional
component. It also removes the document events module and listens to events on
the slider div directly.

Fixes #1804

packages/studio-base/src/components/PlaybackControls/Scrubber.tsx
@@ -105,7 +105,6 @@ export default function Scrubber(props: Props): JSX.Element {
 
   const [hoverComponentId] = useState<string>(() => uuidv4());
   const el = useRef<HTMLDivElement>(ReactNull);
-  const slider = useRef<Slider>(ReactNull);
 
   const startTime = useMessagePipeline(selectStartTime);
   const currentTime = useMessagePipeline(selectCurrentTime);
@@ -119,18 +118,16 @@ export default function Scrubber(props: Props): JSX.Element {
   const onChange = useCallback((value: number) => onSeek(fromSec(value)), [onSeek]);
 
   const latestStartTime = useLatest(startTime);
-  const onMouseMove = useCallback(
-    (e: React.MouseEvent<HTMLDivElement>) => {
-      if (!latestStartTime.current || el.current == undefined || slider.current == undefined) {
+  const onHoverOver = useCallback(
+    (ev: React.MouseEvent<HTMLDivElement>, value: number) => {
+      if (!latestStartTime.current || el.current == undefined) {
         return;
       }
       const currentEl = el.current;
-      const currentSlider = slider.current;
-      const x = e.clientX;
+      const x = ev.clientX;
       // fix the y position of the tooltip to float on top of the playback bar
       const y = currentEl.getBoundingClientRect().top;
 
-      const value = currentSlider.getValueAtMouse(e);
       const stamp = fromSec(value);
       const timeFromStart = subtractTimes(stamp, latestStartTime.current);
 
@@ -162,13 +159,13 @@ export default function Scrubber(props: Props): JSX.Element {
 
   const clearHoverValue = useClearHoverValue();
 
-  const onMouseLeave = useCallback(() => {
+  const onHoverOut = useCallback(() => {
     setTooltipState(undefined);
     clearHoverValue(hoverComponentId);
   }, [clearHoverValue, hoverComponentId]);
 
   // Clean up the hover value when we are unmounted -- important for storybook.
-  useEffect(() => onMouseLeave, [onMouseLeave]);
+  useEffect(() => onHoverOut, [onHoverOut]);
 
   const renderSlider = useCallback(
     (val?: number) => {
@@ -209,20 +206,16 @@ export default function Scrubber(props: Props): JSX.Element {
       <Stack className={classes.stateBar}>
         <ProgressPlot progress={progress} />
       </Stack>
-      <div
-        ref={el}
-        className={classes.sliderContainer}
-        onMouseMove={onMouseMove}
-        onMouseLeave={onMouseLeave}
-      >
+      <div ref={el} className={classes.sliderContainer}>
         <Slider
-          ref={slider}
           min={min ?? 0}
           max={max ?? 100}
           disabled={min == undefined || max == undefined}
           step={step}
           value={value}
           draggable
+          onHoverOver={onHoverOver}
+          onHoverOut={onHoverOut}
           onChange={onChange}
           renderSlider={renderSlider}
         />

packages/studio-base/src/components/PlaybackControls/Slider.stories.tsx
@@ -1,0 +1,105 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+//
+// This file incorporates work covered by the following copyright and
+// permission notice:
+//
+//   Copyright 2018-2021 Cruise LLC
+//
+//   This source code is licensed under the Apache License, Version 2.0,
+//   found at http://www.apache.org/licenses/LICENSE-2.0
+//   You may not use this file except in compliance with the License.
+
+import { Story } from "@storybook/react";
+import { useState } from "react";
+import styled from "styled-components";
+
+import Slider from "./Slider";
+
+const StyledRange = styled.div<{ width?: number }>`
+  position: absolute;
+  top: 40%;
+  left: 0;
+  background-color: #b3ecf9bd;
+  height: 20%;
+  width: ${(props) => (props.width ?? 0) * 100}%;
+  border-radius: 2px;
+`;
+
+const StyledMarker = styled.div<{ width?: number }>`
+  background-color: white;
+  position: absolute;
+  height: 150%;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  width: 6px;
+  top: -25%;
+  left: ${(props) => (props.width ?? 0) * 100}%;
+`;
+
+export default {
+  title: "components/PlaybackControls/Slider",
+};
+
+export const Examples: Story = () => {
+  const [value, setValue] = useState(50);
+  const [draggableValue, setDraggableValue] = useState(25);
+  return (
+    <div style={{ margin: 50 }}>
+      <p>standard (clickable)</p>
+      <div style={{ backgroundColor: "pink", height: 30, width: 300 }}>
+        <Slider min={10} max={200} onChange={(v) => setValue(v)} value={value} />
+      </div>
+      <p>disabled (not clickable)</p>
+      <div style={{ backgroundColor: "pink", height: 30, width: 300 }}>
+        <Slider disabled min={10} max={200} onChange={(v) => setValue(v)} value={value} />
+      </div>
+      <p>no value</p>
+      <div style={{ backgroundColor: "pink", height: 30, width: 300 }}>
+        <Slider
+          min={10}
+          max={200}
+          onChange={() => {
+            // no-op
+          }}
+          value={undefined}
+        />
+      </div>
+      <p>draggable</p>
+      <div style={{ backgroundColor: "cornflowerblue", height: 20, width: 500 }}>
+        <Slider
+          draggable
+          min={10}
+          max={200}
+          onChange={(v) => setDraggableValue(v)}
+          value={draggableValue}
+        />
+      </div>
+    </div>
+  );
+};
+
+export const CustomRenderer: Story = () => {
+  const [draggableValue, setDraggableValue] = useState(25);
+
+  return (
+    <div style={{ margin: 50 }}>
+      <p>Customize slider UI using renderSlider</p>
+      <div style={{ backgroundColor: "cornflowerblue", height: 20, width: 500 }}>
+        <Slider
+          draggable
+          min={10}
+          max={200}
+          onChange={(v) => setDraggableValue(v)}
+          value={draggableValue}
+          renderSlider={(width) => (
+            <div>
+              <StyledRange width={width} />
+              <StyledMarker width={width} />
+            </div>
+          )}
+        />
+      </div>
+    </div>
+  );
+};

packages/studio-base/src/components/PlaybackControls/Slider.tsx
@@ -3,20 +3,24 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { clamp } from "lodash";
-import DocumentEvents from "react-document-events";
+import { useCallback, useEffect, useRef, MouseEvent, ReactNode, useMemo } from "react";
 import styled from "styled-components";
 
-import sendNotification from "@foxglove/studio-base/util/sendNotification";
+import Logger from "@foxglove/log";
+
+const log = Logger.getLogger(__filename);
 
 type Props = {
   value: number | undefined;
   min: number;
   max: number;
-  disabled: boolean;
+  disabled?: boolean;
   step?: number;
-  draggable: boolean;
-  onChange: (arg0: number) => void;
-  renderSlider: (value?: number) => React.ReactNode;
+  draggable?: boolean;
+  onChange: (value: number) => void;
+  onHoverOver?: (ev: MouseEvent<HTMLDivElement>, value: number) => void;
+  onHoverOut?: () => void;
+  renderSlider?: (value?: number) => ReactNode;
 };
 
 const StyledSlider = styled.div<{ disabled?: boolean }>`
@@ -27,7 +31,7 @@ const StyledSlider = styled.div<{ disabled?: boolean }>`
   border-radius: 2px;
 `;
 
-export const StyledRange = styled.div.attrs<{ width: number }>(({ width }) => ({
+const StyledRange = styled.div.attrs<{ width: number }>(({ width }) => ({
   style: { width: `${width * 100}%` },
 }))<{ width: number }>`
   background-color: rgba(255, 255, 255, 0.2);
@@ -36,127 +40,115 @@ export const StyledRange = styled.div.attrs<{ width: number }>(({ width }) => ({
   border-radius: 2px;
 `;
 
-function defaultRenderSlider(value: number | undefined): React.ReactNode {
+function defaultRenderSlider(value: number | undefined): ReactNode {
   if (value == undefined || isNaN(value)) {
     return ReactNull;
   }
   return <StyledRange width={value} />;
 }
 
-export default class Slider extends React.Component<Props> {
-  static defaultProps = {
-    min: 0,
-    draggable: false,
-    disabled: false,
-    renderSlider: defaultRenderSlider,
-  };
+export default function Slider(props: Props): JSX.Element {
+  const {
+    value,
+    step = 0,
+    max = 0,
+    min = 0,
+    draggable = false,
+    disabled = false,
+    renderSlider = defaultRenderSlider,
+    onHoverOver,
+    onHoverOut,
+    onChange,
+  } = props;
 
-  // mouseDown is kept as a variable rather than inside state to avoid problems if mousedown/up are
-  // called very quickly before the state update is applied.
-  mouseDown: boolean = false;
+  const elRef = useRef<HTMLDivElement | ReactNull>(ReactNull);
 
-  el?: HTMLDivElement;
+  const mouseDownRef = useRef(false);
 
-  override shouldComponentUpdate(nextProps: Props): boolean {
-    const { value, min, max, draggable } = this.props;
-    return (
-      nextProps.value !== value ||
-      nextProps.min !== min ||
-      nextProps.max !== max ||
-      nextProps.draggable !== draggable
-    );
-  }
+  const getValueAtMouse = useCallback(
+    (ev: MouseEvent<HTMLDivElement>): number => {
+      if (!elRef.current) {
+        return 0;
+      }
+      const { left, width } = elRef.current.getBoundingClientRect();
+      const { clientX } = ev;
+      const t = (clientX - left) / width;
+      let interpolated = min + t * (max - min);
+      if (step != undefined && step !== 0) {
+        interpolated = Math.round(interpolated / step) * step;
+      }
+      return clamp(interpolated, min, max);
+    },
+    [max, min, step],
+  );
 
-  getValueAtMouse(e: React.MouseEvent<HTMLDivElement>): number {
-    const { min, max, step } = this.props;
-    // this should never happen since you can't interact with an unmounted component
-    // but to appease TypeScript we need to check if the ref is defined
-    if (!this.el) {
-      console.warn("No dom ref available for click handler");
-      return 0;
-    }
-    const { left, width } = this.el.getBoundingClientRect();
-    const { clientX } = e;
-    const t = (clientX - left) / width;
-    let interpolated = min + t * (max - min);
-    if (step != undefined && step !== 0) {
-      interpolated = Math.round(interpolated / step) * step;
-    }
-    return clamp(interpolated, min, max);
-  }
+  const onClick = useCallback(
+    (ev: MouseEvent<HTMLDivElement>): void => {
+      if (disabled || draggable) {
+        return;
+      }
+      onChange(getValueAtMouse(ev));
+    },
+    [disabled, draggable, getValueAtMouse, onChange],
+  );
 
-  _onClick = (e: React.MouseEvent<HTMLDivElement>): void => {
-    const { draggable, onChange, disabled } = this.props;
-    if (disabled) {
-      return;
-    }
-    // handled in mouse up/out if draggable
-    if (draggable) {
-      return;
-    }
-    const value = this.getValueAtMouse(e);
-    onChange(value);
-  };
+  const onMouseUp = useCallback((): void => {
+    mouseDownRef.current = false;
+  }, []);
 
-  _onMouseUp = (): void => {
-    this.mouseDown = false;
-    this.forceUpdate();
-  };
+  const onMouseMove = useCallback(
+    (ev: MouseEvent<HTMLDivElement>): void => {
+      const val = getValueAtMouse(ev);
+      if (disabled) {
+        return;
+      }
 
-  _onMouseMove = (e: React.MouseEvent<HTMLDivElement>): void => {
-    const { draggable, onChange, disabled } = this.props;
-    if (disabled || !draggable || !this.mouseDown) {
-      return;
-    }
-    const value = this.getValueAtMouse(e);
-    onChange(value);
-  };
+      onHoverOver?.(ev, val);
+      if (!draggable || !mouseDownRef.current) {
+        return;
+      }
+      onChange(val);
+    },
+    [disabled, draggable, getValueAtMouse, onChange, onHoverOver],
+  );
 
-  _onMouseDown = (e: React.MouseEvent<HTMLDivElement>): void => {
-    const { draggable, onChange, disabled } = this.props;
-    if (disabled || !draggable) {
-      return;
-    }
-    if (document.activeElement instanceof HTMLElement) {
-      document.activeElement.blur();
-    }
-    e.preventDefault();
-    const value = this.getValueAtMouse(e);
-    onChange(value);
-    this.mouseDown = true;
-    this.forceUpdate();
-  };
+  const onMouseDown = useCallback(
+    (ev: MouseEvent<HTMLDivElement>): void => {
+      if (disabled || !draggable) {
+        return;
+      }
+      if (document.activeElement instanceof HTMLElement) {
+        document.activeElement.blur();
+      }
+      ev.preventDefault();
+      onChange(getValueAtMouse(ev));
+      mouseDownRef.current = true;
+    },
+    [disabled, draggable, getValueAtMouse, onChange],
+  );
 
-  override render(): JSX.Element {
-    const { min, max, value, renderSlider, draggable, disabled } = this.props;
-    const { mouseDown } = this;
-
+  useEffect(() => {
     if (max < min) {
       const msg = `Slider component given invalid range: ${min}, ${max}`;
-      const err = new Error(msg);
-      sendNotification(err.message, err, "app", "error");
+      log.error(new Error(msg));
     }
+  }, [min, max]);
 
-    return (
-      <StyledSlider
-        disabled={disabled}
-        ref={(el) => (this.el = el ?? undefined)}
-        onClick={this._onClick}
-        onMouseDown={this._onMouseDown}
-      >
-        <DocumentEvents
-          target={window}
-          enabled={mouseDown && draggable}
-          onMouseUp={this._onMouseUp}
-          onMouseMove={this._onMouseMove}
-        />
-        <DocumentEvents
-          target={window}
-          enabled={mouseDown && draggable}
-          onMouseUp={this._onMouseUp}
-        />
-        {renderSlider(value != undefined && min !== max ? (value - min) / (max - min) : undefined)}
-      </StyledSlider>
-    );
-  }
+  const sliderValue = useMemo(() => {
+    return value != undefined && max > min ? (value - min) / (max - min) : undefined;
+  }, [max, min, value]);
+
+  return (
+    <StyledSlider
+      disabled={disabled}
+      ref={elRef}
+      onClick={onClick}
+      onMouseUp={onMouseUp}
+      onMouseDown={onMouseDown}
+      onMouseMove={onMouseMove}
+      onMouseOut={onHoverOut}
+    >
+      {renderSlider(sliderValue)}
+    </StyledSlider>
+  );
 }

packages/studio-base/src/components/Slider.stories.tsx
@@ -1,101 +1,0 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/
-//
-// This file incorporates work covered by the following copyright and
-// permission notice:
-//
-//   Copyright 2018-2021 Cruise LLC
-//
-//   This source code is licensed under the Apache License, Version 2.0,
-//   found at http://www.apache.org/licenses/LICENSE-2.0
-//   You may not use this file except in compliance with the License.
-
-import { storiesOf } from "@storybook/react";
-import { useState } from "react";
-import styled from "styled-components";
-
-import Slider from "@foxglove/studio-base/components/PlaybackControls/Slider";
-
-const StyledRange = styled.div<{ width?: number }>`
-  position: absolute;
-  top: 40%;
-  left: 0;
-  background-color: #b3ecf9bd;
-  height: 20%;
-  width: ${(props) => (props.width ?? 0) * 100}%;
-  border-radius: 2px;
-`;
-
-const StyledMarker = styled.div<{ width?: number }>`
-  background-color: white;
-  position: absolute;
-  height: 150%;
-  border: 1px solid rgba(0, 0, 0, 0.3);
-  width: 6px;
-  top: -25%;
-  left: ${(props) => (props.width ?? 0) * 100}%;
-`;
-
-storiesOf("components/Slider", module)
-  .add("examples", () => {
-    const [value, setValue] = useState(50);
-    const [draggableValue, setDraggableValue] = useState(25);
-    return (
-      <div style={{ margin: 50 }}>
-        <p>standard (clickable)</p>
-        <div style={{ backgroundColor: "pink", height: 30, width: 300 }}>
-          <Slider min={10} max={200} onChange={(v) => setValue(v)} value={value} />
-        </div>
-        <p>disabled (not clickable)</p>
-        <div style={{ backgroundColor: "pink", height: 30, width: 300 }}>
-          <Slider disabled min={10} max={200} onChange={(v) => setValue(v)} value={value} />
-        </div>
-        <p>no value</p>
-        <div style={{ backgroundColor: "pink", height: 30, width: 300 }}>
-          <Slider
-            min={10}
-            max={200}
-            onChange={() => {
-              // no-op
-            }}
-            value={undefined}
-          />
-        </div>
-        <p>draggable</p>
-        <div style={{ backgroundColor: "cornflowerblue", height: 20, width: 500 }}>
-          <Slider
-            draggable
-            min={10}
-            max={200}
-            onChange={(v) => setDraggableValue(v)}
-            value={draggableValue}
-          />
-        </div>
-      </div>
-    );
-  })
-  .add("custom renderer", () => {
-    const [draggableValue, setDraggableValue] = useState(25);
-
-    return (
-      <div style={{ margin: 50 }}>
-        <p>Customize slider UI using renderSlider</p>
-        <div style={{ backgroundColor: "cornflowerblue", height: 20, width: 500 }}>
-          <Slider
-            draggable
-            min={10}
-            max={200}
-            onChange={(v) => setDraggableValue(v)}
-            value={draggableValue}
-            renderSlider={(width) => (
-              <div>
-                <StyledRange width={width} />
-                <StyledMarker width={width} />
-              </div>
-            )}
-          />
-        </div>
-      </div>
-    );
-  });

commit 3abb216500b07983eff9847d73c1e373c4f688e9

Fix playback bar getting stuck in drag mode (#2912)
**User-Facing Changes**
Fixed an issue where scrubbing on the playback bar only worked as long as the mouse was actually kept on the playback bar, and could get "stuck" if the mouse was released when outside of the playback bar.

**Description**
Fixes https://github.com/foxglove/studio/issues/2900. This behavior was previously working and regressed in https://github.com/foxglove/studio/pull/1805. The mouseup and mousemove events are now tracked on the window instead of the slider element when the mouse is pressed, which improves the UX by allowing a drag to move outside the playback bar and continue working, and allows us to correctly track mouseup even when it happens outside the playback bar (or outside the window). Additionally, a mouseenter/mouseleave pair are used to track the "mouse inside" state, which allows us to hide the tooltip on mouse up, if the mouse was outside the slider.

packages/studio-base/src/components/PlaybackControls/PlaybackBarHoverTicks.tsx
@@ -34,7 +34,7 @@ const sharedTickStyles = css`
   border-left: 5px solid transparent;
   border-right: 5px solid transparent;
 
-  margin-left: -6px; /* -6px seems to line up better than -5px */
+  margin-left: -5px;
 `;
 
 const TopTick = styled.div`

packages/studio-base/src/components/PlaybackControls/Scrubber.tsx
@@ -127,12 +127,11 @@ export default function Scrubber(props: Props): JSX.Element {
 
   const latestStartTime = useLatest(startTime);
   const onHoverOver = useCallback(
-    (ev: React.MouseEvent<HTMLDivElement>, value: number) => {
+    (x: number, value: number) => {
       if (!latestStartTime.current || el.current == undefined) {
         return;
       }
       const currentEl = el.current;
-      const x = ev.clientX;
       // fix the y position of the tooltip to float on top of the playback bar
       const y = currentEl.getBoundingClientRect().top;
 
@@ -224,7 +223,6 @@ export default function Scrubber(props: Props): JSX.Element {
           disabled={min == undefined || max == undefined}
           step={step}
           value={value}
-          draggable
           onHoverOver={onHoverOver}
           onHoverOut={onHoverOut}
           onChange={onChange}

packages/studio-base/src/components/PlaybackControls/Slider.stories.tsx
@@ -67,13 +67,7 @@ export const Examples: Story = () => {
       </div>
       <p>draggable</p>
       <div style={{ backgroundColor: "cornflowerblue", height: 20, width: 500 }}>
-        <Slider
-          draggable
-          min={10}
-          max={200}
-          onChange={(v) => setDraggableValue(v)}
-          value={draggableValue}
-        />
+        <Slider min={10} max={200} onChange={(v) => setDraggableValue(v)} value={draggableValue} />
       </div>
     </div>
   );
@@ -87,7 +81,6 @@ export const CustomRenderer: Story = () => {
       <p>Customize slider UI using renderSlider</p>
       <div style={{ backgroundColor: "cornflowerblue", height: 20, width: 500 }}>
         <Slider
-          draggable
           min={10}
           max={200}
           onChange={(v) => setDraggableValue(v)}

packages/studio-base/src/components/PlaybackControls/Slider.tsx
@@ -3,7 +3,15 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { clamp } from "lodash";
-import { useCallback, useEffect, useRef, MouseEvent, ReactNode, useMemo } from "react";
+import {
+  useCallback,
+  useEffect,
+  useRef,
+  ReactNode,
+  useMemo,
+  useState,
+  useLayoutEffect,
+} from "react";
 import styled from "styled-components";
 
 import Logger from "@foxglove/log";
@@ -16,9 +24,8 @@ type Props = {
   max: number;
   disabled?: boolean;
   step?: number;
-  draggable?: boolean;
   onChange: (value: number) => void;
-  onHoverOver?: (ev: MouseEvent<HTMLDivElement>, value: number) => void;
+  onHoverOver?: (clientX: number, value: number) => void;
   onHoverOut?: () => void;
   renderSlider?: (value?: number) => ReactNode;
 };
@@ -53,7 +60,6 @@ export default function Slider(props: Props): JSX.Element {
     step = 0,
     max = 0,
     min = 0,
-    draggable = false,
     disabled = false,
     renderSlider = defaultRenderSlider,
     onHoverOver,
@@ -63,10 +69,8 @@ export default function Slider(props: Props): JSX.Element {
 
   const elRef = useRef<HTMLDivElement | ReactNull>(ReactNull);
 
-  const mouseDownRef = useRef(false);
-
   const getValueAtMouse = useCallback(
-    (ev: MouseEvent<HTMLDivElement>): number => {
+    (ev: React.MouseEvent | MouseEvent): number => {
       if (!elRef.current) {
         return 0;
       }
@@ -82,39 +86,58 @@ export default function Slider(props: Props): JSX.Element {
     [max, min, step],
   );
 
-  const onClick = useCallback(
-    (ev: MouseEvent<HTMLDivElement>): void => {
-      if (disabled || draggable) {
-        return;
-      }
-      onChange(getValueAtMouse(ev));
-    },
-    [disabled, draggable, getValueAtMouse, onChange],
-  );
+  const [mouseDown, setMouseDown] = useState(false);
+  const mouseDownRef = useRef(mouseDown);
+  useLayoutEffect(() => {
+    mouseDownRef.current = mouseDown;
+  }, [mouseDown]);
 
-  const onMouseUp = useCallback((): void => {
-    mouseDownRef.current = false;
+  const [mouseInside, setMouseInside] = useState(false);
+  const mouseInsideRef = useRef(mouseInside);
+  useLayoutEffect(() => {
+    mouseInsideRef.current = mouseInside;
+  }, [mouseInside]);
+
+  const onMouseEnter = useCallback(() => {
+    setMouseInside(true);
   }, []);
 
+  const onMouseLeave = useCallback(() => {
+    setMouseInside(false);
+    if (!mouseDownRef.current) {
+      onHoverOut?.();
+    }
+  }, [onHoverOut]);
+
+  const onMouseUp = useCallback((): void => {
+    setMouseDown(false);
+    if (!mouseInsideRef.current) {
+      onHoverOut?.();
+    }
+  }, [onHoverOut]);
+
   const onMouseMove = useCallback(
-    (ev: MouseEvent<HTMLDivElement>): void => {
+    (ev: React.MouseEvent | MouseEvent): void => {
       const val = getValueAtMouse(ev);
       if (disabled) {
         return;
       }
 
-      onHoverOver?.(ev, val);
-      if (!draggable || !mouseDownRef.current) {
+      if (elRef.current) {
+        const { left, right } = elRef.current.getBoundingClientRect();
+        onHoverOver?.(clamp(ev.clientX, left, right), val);
+      }
+      if (!mouseDownRef.current) {
         return;
       }
       onChange(val);
     },
-    [disabled, draggable, getValueAtMouse, onChange, onHoverOver],
+    [disabled, getValueAtMouse, onChange, onHoverOver],
   );
 
   const onMouseDown = useCallback(
-    (ev: MouseEvent<HTMLDivElement>): void => {
-      if (disabled || !draggable) {
+    (ev: React.MouseEvent<HTMLDivElement>): void => {
+      if (disabled) {
         return;
       }
       if (document.activeElement instanceof HTMLElement) {
@@ -122,9 +145,9 @@ export default function Slider(props: Props): JSX.Element {
       }
       ev.preventDefault();
       onChange(getValueAtMouse(ev));
-      mouseDownRef.current = true;
+      setMouseDown(true);
     },
-    [disabled, draggable, getValueAtMouse, onChange],
+    [disabled, getValueAtMouse, onChange],
   );
 
   useEffect(() => {
@@ -138,15 +161,26 @@ export default function Slider(props: Props): JSX.Element {
     return value != undefined && max > min ? (value - min) / (max - min) : undefined;
   }, [max, min, value]);
 
+  useEffect(() => {
+    if (mouseDown) {
+      window.addEventListener("mouseup", onMouseUp);
+      window.addEventListener("mousemove", onMouseMove);
+      return () => {
+        window.removeEventListener("mouseup", onMouseUp);
+        window.removeEventListener("mousemove", onMouseMove);
+      };
+    }
+    return undefined;
+  }, [mouseDown, onMouseMove, onMouseUp]);
+
   return (
     <StyledSlider
       disabled={disabled}
       ref={elRef}
-      onClick={onClick}
-      onMouseUp={onMouseUp}
       onMouseDown={onMouseDown}
       onMouseMove={onMouseMove}
-      onMouseOut={onHoverOut}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
       {renderSlider(sliderValue)}
     </StyledSlider>