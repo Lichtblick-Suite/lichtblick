29b457f72905bef8705424208763fba0a26f0c25
Miles Egan <93935560+foxymiles@users.noreply.github.com>
2022-06-09 06:08:25 -0500

    Don't show pointer cursor on empty settings nodes (#3528)

--- packages/studio-base/src/components/SettingsTreeEditor/NodeEditor.tsx
+++ packages/studio-base/src/components/SettingsTreeEditor/NodeEditor.tsx
@@ -67,20 +67,22 @@ const NodeHeader = muiStyled("div")(({ theme }) => {
 });
 
 const NodeHeaderToggle = muiStyled("div", {
-  shouldForwardProp: (prop) => prop !== "indent" && prop !== "visible",
-})<{ indent: number; visible: boolean }>(({ theme, indent, visible }) => {
-  return {
-    display: "grid",
-    alignItems: "center",
-    cursor: "pointer",
-    gridTemplateColumns: "auto 1fr auto",
-    marginLeft: theme.spacing(0.75 + 2 * indent),
-    opacity: visible ? 1 : 0.6,
-    position: "relative",
-    userSelect: "none",
-    width: "100%",
-  };
-});
+  shouldForwardProp: (prop) => prop !== "hasProperties" && prop !== "indent" && prop !== "visible",
+})<{ hasProperties: boolean; indent: number; visible: boolean }>(
+  ({ hasProperties, theme, indent, visible }) => {
+    return {
+      display: "grid",
+      alignItems: "center",
+      cursor: hasProperties ? "pointer" : "auto",
+      gridTemplateColumns: "auto 1fr auto",
+      marginLeft: theme.spacing(0.75 + 2 * indent),
+      opacity: visible ? 1 : 0.6,
+      position: "relative",
+      userSelect: "none",
+      width: "100%",
+    };
+  },
+);
 
 const IconWrapper = muiStyled("div")({
   position: "absolute",
@@ -149,7 +151,12 @@ function NodeEditorComponent(props: NodeEditorProps): JSX.Element {
   return (
     <>
       <NodeHeader>
-        <NodeHeaderToggle indent={indent} onClick={() => setOpen(!open)} visible={visible}>
+        <NodeHeaderToggle
+          hasProperties={hasProperties}
+          indent={indent}
+          onClick={() => setOpen(!open)}
+          visible={visible}
+        >
           {hasProperties && <ExpansionArrow expanded={open} />}
           {IconComponent && (
             <IconComponent
