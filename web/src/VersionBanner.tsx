aab10bef88445712d4904e9a02395bc422e22459
Roman Shtylman <roman@foxglove.dev>
2022-07-13 14:24:26 -0700

    Change VersionBanner action button to mui (#3577)
    * Change VersionBanner action button to mui
    
    * Connect ThemeProvider and use theme styles (#3627)
    
    * remove unneeded type casting
    
    Co-authored-by: Andrew Carr <2metres@users.noreply.github.com>
    Co-authored-by: Andrew Carr <andrew@foxglove.dev>

--- web/src/VersionBanner.tsx
+++ web/src/VersionBanner.tsx
@@ -2,49 +2,50 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { DefaultButton } from "@fluentui/react";
 import CloseIcon from "@mui/icons-material/Close";
-import { IconButton, Typography, Link } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-import cx from "classnames";
-import { useState, ReactElement } from "react";
+import {
+  IconButton,
+  Typography,
+  Link,
+  Button,
+  styled as muiStyled,
+  ThemeProvider as MuiThemeProvider,
+} from "@mui/material";
+import { useState, useMemo, ReactElement } from "react";
+
+import Stack from "@foxglove/studio-base/components/Stack";
+import { createMuiTheme } from "@foxglove/studio-base/theme";
 
 const MINIMUM_CHROME_VERSION = 76;
 
-const useStyles = makeStyles({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    textAlign: "center",
-    width: "100%",
-    color: "white",
-    backgroundColor: "rgba(99, 102, 241, 0.9)",
-    zIndex: 100,
-  },
-  rootPersistant: {
+const Root = muiStyled("div", {
+  shouldForwardProp: (prop) => prop !== "isDismissable",
+})<{ isDismissable: boolean }>(({ isDismissable, theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.primary.contrastText,
+  zIndex: 100,
+
+  ...(!isDismissable && {
     position: "fixed",
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
     height: "100vh",
-  },
-  inner: {
-    display: "flex",
-    flexDirection: "column",
-    padding: 12,
-    gap: 8,
-    alignItems: "center",
-  },
-  closeButton: {
-    position: "absolute !important" as unknown as "absolute",
-    margin: 8,
-    right: 0,
-    top: 0,
-  },
-});
+  }),
+}));
+
+const DismissButton = muiStyled(IconButton)(({ theme }) => ({
+  position: "absolute",
+  margin: theme.spacing(1),
+  right: 0,
+  top: 0,
+}));
 
 const VersionBanner = function ({
   isChrome,
@@ -55,8 +56,8 @@ const VersionBanner = function ({
   currentVersion: number;
   isDismissable: boolean;
 }): ReactElement | ReactNull {
-  const classes = useStyles();
   const [showBanner, setShowBanner] = useState(true);
+  const muiTheme = useMemo(() => createMuiTheme("dark"), []);
 
   if (!showBanner || currentVersion >= MINIMUM_CHROME_VERSION) {
     return ReactNull;
@@ -68,54 +69,43 @@ const VersionBanner = function ({
   const fixText = isChrome ? "Update Chrome" : "Download Chrome";
 
   return (
-    <div className={cx(classes.root, { [classes.rootPersistant]: !isDismissable })}>
-      <div className={classes.inner}>
-        {isDismissable && (
-          <IconButton
+    <MuiThemeProvider theme={muiTheme}>
+      <Root isDismissable={isDismissable}>
+        <Stack padding={2} gap={1.5} alignItems="center">
+          {isDismissable && (
+            <DismissButton color="inherit" onClick={() => setShowBanner(false)}>
+              <CloseIcon />
+            </DismissButton>
+          )}
+
+          <div>
+            <Typography align="center" variant="h6">
+              {prompt} Foxglove Studio currently requires Chrome v{MINIMUM_CHROME_VERSION}+.
+            </Typography>
+
+            {!isChrome && (
+              <Typography align="center" variant="subtitle1">
+                Check out our cross-browser support progress in GitHub issue{" "}
+                <Link color="inherit" href="https://github.com/foxglove/studio/issues/1511">
+                  #1511
+                </Link>
+                .
+              </Typography>
+            )}
+          </div>
+
+          <Button
+            href="https://www.google.com/chrome/"
+            target="_blank"
+            rel="noreferrer"
             color="inherit"
-            className={classes.closeButton}
-            onClick={() => setShowBanner(false)}
+            variant="outlined"
           >
-            <CloseIcon />
-          </IconButton>
-        )}
-
-        <Typography color="common.white" fontSize="1.1em">
-          {prompt} Foxglove Studio currently requires Chrome v{MINIMUM_CHROME_VERSION}+.
-        </Typography>
-
-        {!isChrome && (
-          <Typography color="common.white" fontSize="1.1em">
-            Check out our cross-browser support progress in GitHub issue{" "}
-            <Link color="inherit" href="https://github.com/foxglove/studio/issues/1511">
-              #1511
-            </Link>
-            .
-          </Typography>
-        )}
-
-        <DefaultButton
-          href="https://www.google.com/chrome/"
-          target="_blank"
-          rel="noreferrer"
-          styles={{
-            root: {
-              color: "white",
-              backgroundColor: "rgba(255,255, 255, 0.1)",
-              borderRadius: "4px",
-              border: "1px solid rgba(255,255,255,0.3)",
-              fontSize: "1em",
-            },
-            rootHovered: {
-              color: "white",
-              backgroundColor: "rgba(255,255, 255, 0.3)",
-            },
-          }}
-        >
-          {fixText}
-        </DefaultButton>
-      </div>
-    </div>
+            {fixText}
+          </Button>
+        </Stack>
+      </Root>
+    </MuiThemeProvider>
   );
 };
 
