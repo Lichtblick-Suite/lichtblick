334f7fd9cddd067abc795a0b62036bd8349710b8
Andrew Carr <2metres@users.noreply.github.com>
2022-07-21 09:15:18 +1000

    Refactor TextContent to avoid muiStyled (#3880)
    * Refactor TextContent to avoid muiStyled and remove reference to styled-components
    
    * Fix broken parenthesis

--- packages/studio-base/src/components/TextContent.tsx
+++ packages/studio-base/src/components/TextContent.tsx
@@ -11,10 +11,10 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
-import { Link, styled as muiStyled } from "@mui/material";
-import { PropsWithChildren, useCallback, useContext, Suspense } from "react";
+import { Link } from "@mui/material";
+import { PropsWithChildren, CSSProperties, useCallback, useContext, Suspense } from "react";
 import { useAsync } from "react-use";
-import { CSSProperties } from "styled-components";
+import { withStyles } from "tss-react/mui";
 
 import LinkHandlerContext from "@foxglove/studio-base/context/LinkHandlerContext";
 import { fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
@@ -22,124 +22,139 @@ import { fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
 // workaround for ESM in jest: https://github.com/foxglove/studio/issues/1854
 const Markdown = React.lazy(async () => await import("react-markdown"));
 
-const TextContentRoot = muiStyled("div")(({ theme }) => ({
-  ...theme.typography.body2,
-  lineHeight: "1.6",
-  backgroundColor: "transparent",
-  color: theme.palette.text.secondary,
-
-  "h1, h2, h3, h4, h5, h6": {
-    color: theme.palette.text.primary,
-
-    ":first-child": {
-      marginTop: 0,
-    },
-  },
-  h1: {
-    ...theme.typography.h4,
-    marginBottom: theme.spacing(1),
-    fontWeight: 500,
-  },
-  h2: {
-    ...theme.typography.h5,
-    marginBottom: theme.spacing(1),
-    fontWeight: 500,
-  },
-  h3: {
-    ...theme.typography.h6,
-    marginBottom: theme.spacing(1),
-    color: theme.palette.text.secondary,
-    fontWeight: 500,
-  },
-  h4: {
-    ...theme.typography.subtitle1,
-    marginBottom: theme.spacing(0.5),
-    color: theme.palette.text.secondary,
-    fontWeight: 500,
-  },
-  "h5, h6": {
-    ...theme.typography.body2,
-    marginBottom: theme.spacing(0.5),
-    color: theme.palette.text.secondary,
-    fontWeight: 500,
-  },
-  "ol, ul": {
-    paddingLeft: theme.spacing(2.5),
-    marginBottom: theme.spacing(2.5),
-  },
-  li: {
-    margin: theme.spacing(0.5, 0),
-  },
-  "b, strong": {
-    fontWeight: "700 !important",
-  },
-  "p, ul": {
-    margin: theme.spacing(1, 0),
-
-    ":only-child": {
-      margin: theme.spacing(0.5, 0),
-    },
-  },
-  img: {
-    maxWidth: "100%",
-  },
-  pre: {
-    whiteSpace: "pre-wrap",
-    fontFamily: fonts.MONOSPACE,
-    backgroundColor: theme.palette.action.hover,
-    padding: `0 ${theme.spacing(0.5)}`,
-    borderRadius: theme.shadows[2],
-
-    code: {
+const TextContentRoot = withStyles("div", (theme) => {
+  const { palette, shape, spacing, typography, shadows } = theme;
+  return {
+    root: {
+      fontFamily: typography.body2.fontFamily,
+      fontSize: typography.body2.fontSize,
+      fontWeight: typography.body2.fontWeight,
+      lineHeight: typography.body2.lineHeight,
       backgroundColor: "transparent",
-      padding: 0,
-    },
-  },
-  code: {
-    fontFamily: fonts.MONOSPACE,
-    backgroundColor: theme.palette.action.hover,
-    borderRadius: "0.2em",
-    padding: `0 ${theme.spacing(0.5)}`,
-  },
-  kbd: {
-    display: "inline-flex",
-    flex: "none",
-    fontFamily: fonts.MONOSPACE,
-    color: theme.palette.text.secondary,
-    backgroundColor: theme.palette.background.default,
-    boxShadow: `inset 0 1px 0 ${theme.palette.action.hover}`,
-    borderRadius: theme.shape.borderRadius,
-    fontSize: theme.typography.body2.fontSize,
-    padding: `0 ${theme.spacing(0.5)}`,
-    fontWeight: 500,
-    minWidth: 20,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  table: {
-    borderCollapse: "collapse",
-    borderSpacing: 0,
-    margin: `${theme.spacing(1)} -${theme.spacing(0.5)}`,
-    border: `1px solid ${theme.palette.divider}`,
-  },
-  "td, th": {
-    padding: theme.spacing(0.5),
-    borderBottom: `1px solid ${theme.palette.divider}`,
-    borderRight: `1px solid ${theme.palette.divider}`,
+      color: palette.text.secondary,
 
-    ":last-child": {
-      borderRight: "none",
+      "h1, h2, h3, h4, h5, h6": {
+        color: palette.text.primary,
+
+        "&:first-child": { marginTop: 0 },
+      },
+      h1: {
+        fontFamily: typography.h4.fontFamily,
+        fontSize: typography.h4.fontSize,
+        lineHeight: typography.h4.lineHeight,
+        marginBottom: spacing(1),
+        fontWeight: 500,
+      },
+      h2: {
+        fontFamily: typography.h5.fontFamily,
+        fontSize: typography.h5.fontSize,
+        lineHeight: typography.h5.lineHeight,
+        marginBottom: spacing(1),
+        fontWeight: 500,
+      },
+      h3: {
+        fontFamily: typography.h6.fontFamily,
+        fontSize: typography.h6.fontSize,
+        lineHeight: typography.h6.lineHeight,
+        marginBottom: spacing(1),
+        color: palette.text.secondary,
+        fontWeight: 500,
+      },
+      h4: {
+        fontFamily: typography.subtitle1.fontFamily,
+        fontSize: typography.subtitle1.fontSize,
+        lineHeight: typography.subtitle1.lineHeight,
+        marginBottom: spacing(0.5),
+        color: palette.text.secondary,
+        fontWeight: 500,
+      },
+      "h5, h6": {
+        fontFamily: typography.body1.fontFamily,
+        fontSize: typography.body1.fontSize,
+        lineHeight: typography.body1.lineHeight,
+        marginBottom: spacing(0.5),
+        color: palette.text.secondary,
+        fontWeight: 500,
+      },
+      "ol, ul": {
+        paddingLeft: spacing(2.5),
+        marginBottom: spacing(2.5),
+      },
+      li: {
+        margin: spacing(0.5, 0),
+      },
+      "b, strong": {
+        fontWeight: "700 !important",
+      },
+      "p, ul": {
+        margin: spacing(1, 0),
+
+        "&:only-child": {
+          margin: spacing(0.5, 0),
+        },
+      },
+      img: {
+        maxWidth: "100%",
+      },
+      pre: {
+        whiteSpace: "pre-wrap",
+        fontFamily: fonts.MONOSPACE,
+        backgroundColor: palette.action.hover,
+        padding: spacing(0, 0.5),
+        borderRadius: shadows[2],
+
+        code: {
+          backgroundColor: "transparent",
+          padding: 0,
+        },
+      },
+      code: {
+        fontFamily: fonts.MONOSPACE,
+        backgroundColor: palette.action.hover,
+        borderRadius: "0.2em",
+        padding: spacing(0, 0.5),
+      },
+      kbd: {
+        display: "inline-flex",
+        flex: "none",
+        fontFamily: fonts.MONOSPACE,
+        color: palette.text.secondary,
+        backgroundColor: palette.background.default,
+        boxShadow: `inset 0 1px 0 ${palette.action.hover}`,
+        borderRadius: shape.borderRadius,
+        fontSize: typography.body2.fontSize,
+        padding: spacing(0, 0.5),
+        fontWeight: 500,
+        minWidth: 20,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+      table: {
+        borderCollapse: "collapse",
+        borderSpacing: 0,
+        margin: spacing(1, -0.5),
+        border: `1px solid ${palette.divider}`,
+      },
+      "td, th": {
+        padding: spacing(0.5),
+        borderBottom: `1px solid ${palette.divider}`,
+        borderRight: `1px solid ${palette.divider}`,
+
+        "&:last-child": {
+          borderRight: "none",
+        },
+      },
+      th: {
+        whiteSpace: "nowrap",
+      },
+      tr: {
+        "&:last-child": {
+          "td, th": { borderBottom: "none" },
+        },
+      },
     },
-  },
-  th: {
-    whiteSpace: "nowrap",
-  },
-  tr: {
-    ":last-child": {
-      "td, th": { borderBottom: "none" },
-    },
-  },
-}));
+  };
+});
 
 type Props = {
   style?: CSSProperties;
@@ -150,6 +165,7 @@ export default function TextContent(
   props: PropsWithChildren<Props>,
 ): React.ReactElement | ReactNull {
   const { children, style, allowMarkdownHtml } = props;
+
   const handleLink = useContext(LinkHandlerContext);
 
   const linkRenderer = useCallback(
