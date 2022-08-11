Lichtblick-Suite/lichtblick

commit 97e55e772a4e2e3a12685d66bdda73bdae2f3ddf

Fix Amplitude analytics in web build (#1638)
**User-Facing Changes**
None

**Description**
Fixes #1622 by working around the webpack bug that causes it.

packages/studio-base/src/services/AmplitudeAnalytics.ts
@@ -17,6 +17,8 @@ const USER_ID_KEY = "analytics_user_id";
 
 const log = Logger.getLogger(__filename);
 
+const os = OsContextSingleton; // workaround for https://github.com/webpack/webpack/issues/12960
+
 export class AmplitudeAnalytics implements IAnalytics {
   private _amplitude: Promise<amplitude.AmplitudeClient | undefined>;
   private _crashReporting: boolean;
@@ -71,7 +73,7 @@ export class AmplitudeAnalytics implements IAnalytics {
   }
 
   getAppVersion(): string {
-    return OsContextSingleton?.getAppVersion() ?? "0.0.0";
+    return os?.getAppVersion() ?? "0.0.0";
   }
 
   getUserId(): string {
@@ -85,7 +87,7 @@ export class AmplitudeAnalytics implements IAnalytics {
 
   // OsContextSingleton.getMachineId() can take 500-2000ms on macOS
   async getDeviceId(): Promise<string> {
-    return (await OsContextSingleton?.getMachineId()) ?? UUID_ZERO;
+    return (await os?.getMachineId()) ?? UUID_ZERO;
   }
 
   async logEvent(event: AppEvent, data?: { [key: string]: unknown }): Promise<void> {

packages/studio-base/src/components/PlayerManager.tsx
@@ -332,10 +332,11 @@ async function roscoreSource(options: FactoryOptions) {
   } else {
     const value = options.storage.getItem<string>(storageCacheKey);
 
+    const os = OsContextSingleton; // workaround for https://github.com/webpack/webpack/issues/12960
     maybeUrl = await options.prompt({
       title: "ROS 1 TCP connection",
       placeholder: "localhost:11311",
-      value: value ?? OsContextSingleton?.getEnvVar("ROS_MASTER_URI") ?? "localhost:11311",
+      value: value ?? os?.getEnvVar("ROS_MASTER_URI") ?? "localhost:11311",
       transformer: (str) => {
         const result = parseInputUrl(str, "ros:", {
           "http:": { port: 80 },

commit 3d41d0a15763b2d05b8b6317f7675baf779efbf2

clean up workarounds for webpack bug that has been fixed (#4112)
**User-Facing Changes**
None

**Description**
Upgrades webpack and removes the workaround for https://github.com/webpack/webpack/issues/12960 introduced in https://github.com/foxglove/studio/pull/1638. The bug was fixed upstream in webpack 5.71.0.

packages/studio-base/src/components/Preferences.tsx
@@ -31,8 +31,6 @@ import isDesktopApp from "@foxglove/studio-base/util/isDesktopApp";
 
 const MESSAGE_RATES = [1, 3, 5, 10, 15, 20, 30, 60];
 
-const os = OsContextSingleton; // workaround for https://github.com/webpack/webpack/issues/12960
-
 const useStyles = makeStyles()({
   autocompleteInput: {
     "&.MuiOutlinedInput-input": {
@@ -279,7 +277,10 @@ function RosPackagePath(): React.ReactElement {
     AppSetting.ROS_PACKAGE_PATH,
   );
 
-  const rosPackagePathPlaceholder = useMemo(() => os?.getEnvVar("ROS_PACKAGE_PATH"), []);
+  const rosPackagePathPlaceholder = useMemo(
+    () => OsContextSingleton?.getEnvVar("ROS_PACKAGE_PATH"),
+    [],
+  );
 
   return (
     <TextField
@@ -305,7 +306,7 @@ export default function Preferences(): React.ReactElement {
   // electron-updater does not provide a way to detect if we are on a supported update platform
   // so we hard-code linux as an _unsupported_ auto-update platform since we cannot auto-update
   // with our .deb package install method on linux.
-  const supportsAppUpdates = isDesktopApp() && os?.platform !== "linux";
+  const supportsAppUpdates = isDesktopApp() && OsContextSingleton?.platform !== "linux";
 
   const { classes } = useStyles();
 

packages/studio-base/src/dataSources/Ros1SocketDataSourceFactory.tsx
@@ -11,8 +11,6 @@ import {
 import Ros1Player from "@foxglove/studio-base/players/Ros1Player";
 import { Player } from "@foxglove/studio-base/players/types";
 
-const os = OsContextSingleton; // workaround for https://github.com/webpack/webpack/issues/12960
-
 class Ros1SocketDataSourceFactory implements IDataSourceFactory {
   id = "ros1-socket";
   type: IDataSourceFactory["type"] = "connection";
@@ -27,14 +25,18 @@ class Ros1SocketDataSourceFactory implements IDataSourceFactory {
       {
         id: "url",
         label: "ROS_MASTER_URI",
-        defaultValue: os?.getEnvVar("ROS_MASTER_URI") ?? "http://localhost:11311",
+        defaultValue: OsContextSingleton?.getEnvVar("ROS_MASTER_URI") ?? "http://localhost:11311",
         description: "Tells ROS nodes where they can locate the master",
       },
       {
         id: "hostname",
         label: "ROS_HOSTNAME",
-        defaultValue: os
-          ? RosNode.GetRosHostname(os.getEnvVar, os.getHostname, os.getNetworkInterfaces)
+        defaultValue: OsContextSingleton
+          ? RosNode.GetRosHostname(
+              OsContextSingleton.getEnvVar,
+              OsContextSingleton.getHostname,
+              OsContextSingleton.getNetworkInterfaces,
+            )
           : "localhost",
         description: "Acts as the declared network address of a ROS node or tool",
       },

packages/studio-base/src/players/Ros2Player.ts
@@ -149,8 +149,7 @@ export default class Ros2Player implements Player {
   }
 
   private _open = async (): Promise<void> => {
-    const os = OsContextSingleton;
-    if (this._closed || os == undefined) {
+    if (this._closed || OsContextSingleton == undefined) {
       return;
     }
     this._presence = PlayerPresence.INITIALIZING;
@@ -161,10 +160,10 @@ export default class Ros2Player implements Player {
 
     if (this._rosNode == undefined) {
       const rosNode = new RosNode({
-        name: `/foxglovestudio_${os.pid}`,
+        name: `/foxglovestudio_${OsContextSingleton.pid}`,
         domainId: this._domainId,
         udpSocketCreate,
-        getNetworkInterfaces: os.getNetworkInterfaces,
+        getNetworkInterfaces: OsContextSingleton.getNetworkInterfaces,
         log: rosLog,
       });
       this._rosNode = rosNode;

packages/studio-base/src/services/AmplitudeAnalytics.ts
@@ -18,14 +18,12 @@ import IAnalytics, {
 
 const log = Logger.getLogger("Analytics");
 
-const os = OsContextSingleton; // workaround for https://github.com/webpack/webpack/issues/12960
-
 export class AmplitudeAnalytics implements IAnalytics {
   private _amp?: AmplitudeClient;
 
   constructor(options: { enableTelemetry: boolean; amplitudeApiKey?: string }) {
     const platform = getPlatformName();
-    const appVersion = os?.getAppVersion();
+    const appVersion = OsContextSingleton?.getAppVersion();
     const { glVendor, glRenderer } = getWebGLInfo() ?? {
       glVendor: "(unknown)",
       glRenderer: "(unknown)",
@@ -94,7 +92,7 @@ export class AmplitudeAnalytics implements IAnalytics {
 }
 
 function getPlatformName(): string {
-  const platform = os?.platform ?? "web";
+  const platform = OsContextSingleton?.platform ?? "web";
   switch (platform) {
     case "darwin":
       return "macOS";