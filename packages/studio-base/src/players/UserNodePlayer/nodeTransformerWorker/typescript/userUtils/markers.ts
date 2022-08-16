1c425cf4b38e2da5af261894321c730745d5232a
Dimitriy Leonov <dleonov@nvidia.com>
2022-08-16 20:55:49 +0200

    feat: buildImageMarker helper function (#4141)

--- packages/studio-base/src/players/UserNodePlayer/nodeTransformerWorker/typescript/userUtils/markers.ts
+++ packages/studio-base/src/players/UserNodePlayer/nodeTransformerWorker/typescript/userUtils/markers.ts
@@ -20,6 +20,22 @@ export interface IMarker {
 
 export type IRosMarker = IMarker;
 
+export interface ImageMarker {
+  header: Header;
+  ns: string;
+  id: number;
+  type: number;
+  action: number;
+  position: Point;
+  scale: number;
+  outline_color: RGBA;
+  filled: boolean;
+  fill_color: RGBA;
+  lifetime: Time;
+  points: Point[];
+  outline_colors: RGBA[];
+}
+
 /**
  * buildRosMarker builds a complete Marker message from an optional set of args.
  *
@@ -82,6 +98,59 @@ export function buildRosMarker(args?: Partial<IRosMarker>): IRosMarker {
     text: text ?? "",
     mesh_resource: mesh_resource ?? "",
     mesh_use_embedded_materials: mesh_use_embedded_materials ?? false,
+  };
+}
+
+/**
+ * buildImageMarker builds a complete Marker message from an optional set of args.
+ *
+ * See https://foxglove.dev/docs/studio/panels/image for a list of supported Marker types
+ *
+ * @param args override any defaults in the marker fields
+ * @returns an ImageMarker instance with default values for any omitted args
+ */
+export function buildImageMarker(args?: Partial<ImageMarker>): ImageMarker {
+  const {
+    header,
+    ns,
+    id,
+    type,
+    action,
+    lifetime,
+    points,
+    outline_color,
+    outline_colors,
+    filled,
+    fill_color,
+    position,
+    scale,
+  } = args ?? {};
+
+  return {
+    header: header ?? {
+      frame_id: "",
+      stamp: {
+        sec: 0,
+        nsec: 0,
+      },
+      seq: 0,
+    },
+    ns: ns ?? "",
+    id: id ?? 0,
+    type: type ?? 0,
+    action: action ?? 0,
+    position: position ?? {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    scale: scale ?? 1,
+    outline_color: outline_color ?? { r: 0, g: 0, b: 0, a: 0 },
+    lifetime: lifetime ?? { sec: 0, nsec: 0 },
+    points: points ?? [],
+    outline_colors: outline_colors ?? [],
+    filled: filled ?? false,
+    fill_color: fill_color ?? { r: 0, g: 0, b: 0, a: 0 },
   };
 }
 
