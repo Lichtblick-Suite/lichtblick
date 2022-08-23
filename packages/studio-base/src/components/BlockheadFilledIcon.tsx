8fa4061d1e91efb4af5a629155b25c8cc877484e
Andrew Carr <2metres@users.noreply.github.com>
2022-08-23 12:21:58 +1000

    Refactor BlockheadIcons to use MUI SvgIcon instead of Fluent-esque styles (#4198)
    **User-Facing Changes**
    None
    
    **Description**
    Change BlockheadIcon and BlockheadFilledIcon to use MUI SvgIcon
    
    <!-- link relevant github issues -->
    <!-- add `docs` label if this PR requires documentation updates -->

--- packages/studio-base/src/components/BlockheadFilledIcon.tsx
+++ packages/studio-base/src/components/BlockheadFilledIcon.tsx
@@ -2,35 +2,15 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { makeStyles } from "tss-react/mui";
+import { SvgIcon, SvgIconProps } from "@mui/material";
 
-const useStyles = makeStyles()({
-  // trying to match the wrapper provided by @fluentui here
-  root: {
-    display: "inline-block",
-    verticalAlign: "top",
-    speak: "none",
-    width: "1em",
-    height: "1em",
-  },
-});
-
-export default function BlockheadFilledIcon({ className }: { className?: string }): JSX.Element {
-  const { classes, cx } = useStyles();
-
+export default function BlockheadFilledIcon(props: SvgIconProps): JSX.Element {
   return (
-    <span className={cx(classes.root, className)}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-        focusable="false"
-      >
-        <g fill="currentColor">
-          <rect x="8.07" y="5.38" width="7.87" height="7.87" rx="2" />
-          <path d="M12,1.53A10.5,10.5,0,1,0,22.5,12,10.51,10.51,0,0,0,12,1.53ZM18.41,19l-1.67-2.61a2.47,2.47,0,0,0-2-1H9.26a2.48,2.48,0,0,0-2,1L5.59,19a9.5,9.5,0,1,1,12.82,0Z" />
-        </g>
-      </svg>
-    </span>
+    <SvgIcon {...props}>
+      <g fill="currentColor">
+        <rect x="8.07" y="5.38" width="7.87" height="7.87" rx="2" />
+        <path d="M12,1.53A10.5,10.5,0,1,0,22.5,12,10.51,10.51,0,0,0,12,1.53ZM18.41,19l-1.67-2.61a2.47,2.47,0,0,0-2-1H9.26a2.48,2.48,0,0,0-2,1L5.59,19a9.5,9.5,0,1,1,12.82,0Z" />
+      </g>
+    </SvgIcon>
   );
 }
