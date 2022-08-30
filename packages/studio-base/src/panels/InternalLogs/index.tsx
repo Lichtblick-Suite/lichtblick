a412a8206d55ff6fc83197a5c15fa415eb62b83b
Andrew Carr <2metres@users.noreply.github.com>
2022-08-30 13:20:16 +1000

    Refactor panels/InternalLogs to use MUI (#4301)
    **User-Facing Changes**
    None
    
    **Description**
    Swap panels/InternalLogs to use mui checkbox
    
    Closes #4209 
    
    <!-- link relevant github issues -->
    <!-- add `docs` label if this PR requires documentation updates -->

--- packages/studio-base/src/panels/InternalLogs/index.tsx
+++ packages/studio-base/src/panels/InternalLogs/index.tsx
@@ -2,13 +2,13 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { Checkbox } from "@fluentui/react";
-import { Stack } from "@mui/material";
+import { Checkbox, FormControlLabel } from "@mui/material";
 import { useMemo } from "react";
 
 import log from "@foxglove/log";
 import Panel from "@foxglove/studio-base/components/Panel";
 import PanelToolbar from "@foxglove/studio-base/components/PanelToolbar";
+import Stack from "@foxglove/studio-base/components/Stack";
 
 import helpContent from "./index.help.md";
 
@@ -43,30 +43,34 @@ function InternalLogs(props: Props) {
   }, [disabledChannels]);
 
   return (
-    <Stack maxWidth="100%" style={{ overflowY: "auto" }}>
+    <Stack fullWidth overflowY="auto">
       <PanelToolbar helpContent={helpContent} />
-      <Stack padding={1} spacing={1}>
+      <Stack padding={1} gap={1}>
         {channels.map((logger) => {
           const label = logger.name().length === 0 ? "default" : logger.name();
           return (
-            <Checkbox
+            <FormControlLabel
               key={label}
               label={label}
-              styles={{ label: { wordBreak: "break-word" } }}
-              checked={logger.isEnabled()}
-              // eslint-disable-next-line @foxglove/no-boolean-parameters
-              onChange={(_event, newChecked = false) => {
-                // track disabled channels so loggers are on by default
-                if (newChecked) {
-                  disabledChannels.delete(label);
-                } else {
-                  disabledChannels.add(label);
-                }
-
-                props.saveConfig({
-                  disabledChannels: Array.from(disabledChannels.values()),
-                });
+              style={{
+                wordBreak: "break-word",
               }}
+              control={
+                <Checkbox
+                  checked={logger.isEnabled()}
+                  onChange={(event) => {
+                    // track disabled channels so loggers are on by default
+                    if (event.target.checked) {
+                      disabledChannels.delete(label);
+                    } else {
+                      disabledChannels.add(label);
+                    }
+                    props.saveConfig({
+                      disabledChannels: Array.from(disabledChannels.values()),
+                    });
+                  }}
+                />
+              }
             />
           );
         })}
