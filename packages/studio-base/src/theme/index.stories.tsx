f99f5aed0591b2715c02a542f3bd0324f3a0b2df
Miles Egan <93935560+foxymiles@users.noreply.github.com>
2022-09-19 10:59:15 -0400

    Add typography catalog story (#4441)
    Add a new story demonstrating all variants of the `Typography`
    component. This is useful as a reference for styling work and also to
    catch any unintended theme changes.

--- packages/studio-base/src/theme/index.stories.tsx
+++ packages/studio-base/src/theme/index.stories.tsx
@@ -3,7 +3,8 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { useTheme as useFluentTheme } from "@fluentui/react";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import { ReactNode } from "react";
 
 export default {
   title: "Theme",
@@ -37,3 +38,88 @@ export function SemanticColors(): JSX.Element {
     />
   );
 }
+
+function Wrapper({ children }: { children: ReactNode }): JSX.Element {
+  return <Box style={{ border: "1px dotted blue" }}>{children}</Box>;
+}
+
+export function TypographyCatalog(): JSX.Element {
+  return (
+    <Stack gap={1} padding={1}>
+      <Wrapper>
+        <Typography variant="h1" gutterBottom>
+          h1. Heading
+        </Typography>
+      </Wrapper>
+      <Wrapper>
+        <Typography variant="h2" gutterBottom>
+          h2. Heading
+        </Typography>
+      </Wrapper>
+      <Wrapper>
+        <Typography variant="h3" gutterBottom>
+          h3. Heading
+        </Typography>
+      </Wrapper>
+      <Wrapper>
+        <Typography variant="h4" gutterBottom>
+          h4. Heading
+        </Typography>
+      </Wrapper>
+      <Wrapper>
+        <Typography variant="h5" gutterBottom>
+          h5. Heading
+        </Typography>
+      </Wrapper>
+      <Wrapper>
+        <Typography variant="h6" gutterBottom>
+          h6. Heading
+        </Typography>
+      </Wrapper>
+      <Wrapper>
+        <Typography variant="subtitle1" gutterBottom>
+          subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
+          tenetur
+        </Typography>
+      </Wrapper>
+      <Wrapper>
+        <Typography variant="subtitle2" gutterBottom>
+          subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
+          tenetur
+        </Typography>
+      </Wrapper>
+      <Wrapper>
+        <Typography variant="body1" gutterBottom>
+          body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+          unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate
+          numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+      </Wrapper>
+      <Wrapper>
+        <Typography variant="body2" gutterBottom>
+          body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+          unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate
+          numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+      </Wrapper>
+      <Wrapper>
+        <Typography variant="button" display="block" gutterBottom>
+          button text
+        </Typography>
+      </Wrapper>
+      <Wrapper>
+        <Typography variant="caption" display="block" gutterBottom>
+          caption text
+        </Typography>
+      </Wrapper>
+      <Wrapper>
+        <Typography variant="overline" display="block" gutterBottom>
+          overline text
+        </Typography>
+      </Wrapper>
+    </Stack>
+  );
+}
+TypographyCatalog.parameters = {
+  colorScheme: "light",
+};
