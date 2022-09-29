c1dea5faa0e22c77b87fab1043e132d354989715
Roman Shtylman <roman@foxglove.dev>
2022-09-29 14:10:52 -0700

    Use corepack for yarn installs (#4476)
    **User-Facing Changes**
    None
    
    **Description**
    Remove yarn from the repo and manage installs with corepack.
    
    Co-authored-by: Jacob Bandes-Storch <jacob@foxglove.dev>

--- .yarn/yarn-wrapper.js
+++ .yarn/yarn-wrapper.js
@@ -3,26 +3,15 @@
 // This script does some pre-configure checks and environment setup for yarn runs
 
 const path = require("path");
-const fs = require("fs");
 
-const REAL_YARN = path.join(__dirname, "releases", "yarn-3.1.0.cjs");
+if (process.env.COREPACK_ROOT == undefined) {
+  console.error("This repository uses corepack. Enable corepack by running `corepack enable`");
+  console.error("Learn more at: https://nodejs.org/api/corepack.html");
+  process.exit(1);
+}
 
 // Increases the v8 old memory space (effectively increasing v8 heap space beyond the 2GB default)
 process.env.NODE_OPTIONS = "--max-old-space-size=6144";
 
-// Check for missing Git LFS before attempting to run yarn
-try {
-  if (fs.statSync(REAL_YARN).size < 10000) {
-    throw new Error(
-      "Foxglove Error: Please configure Git LFS ( https://git-lfs.github.com/ ) " +
-        "then run `git lfs pull` before running yarn.",
-    );
-  }
-} catch (e) {
-  // Catch missing file or LFS error
-  console.error(e.message);
-  process.exit(1);
-}
-
-// Handoff to real yarn
-require(REAL_YARN);
+const yarncmd = path.join(process.env.COREPACK_ROOT, "dist", "yarn.js");
+require(yarncmd);
