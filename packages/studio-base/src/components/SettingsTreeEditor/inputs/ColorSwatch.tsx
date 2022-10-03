1bbb23d68bada6d7af93d5b87b420ee5426b617d
Miles Egan <93935560+foxymiles@users.noreply.github.com>
2022-10-03 11:32:02 -0500

    Safely parse color values in the ColorSwatch component (#4510)
    Use tinycolor to parse the incoming color value before passing it on to MUI's color utility to avoid throwing an exception on an invalid color.

--- packages/studio-base/src/components/SettingsTreeEditor/inputs/ColorSwatch.tsx
+++ packages/studio-base/src/components/SettingsTreeEditor/inputs/ColorSwatch.tsx
@@ -2,7 +2,15 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { styled as muiStyled } from "@mui/material";
+import { styled as muiStyled, Theme } from "@mui/material";
+import tinycolor from "tinycolor2";
+
+function calculateBorderColor(theme: Theme, color: string): string {
+  const parsedColor = tinycolor(color);
+  return parsedColor.isValid()
+    ? theme.palette.getContrastText(parsedColor.toHexString())
+    : theme.palette.text.primary;
+}
 
 export const ColorSwatch = muiStyled("div", {
   shouldForwardProp: (prop) => prop !== "color",
@@ -12,6 +20,6 @@ export const ColorSwatch = muiStyled("div", {
   width: theme.spacing(2.5),
   margin: theme.spacing(0.625),
   borderRadius: 1,
-  border: `1px solid ${theme.palette.getContrastText(color)}`,
+  border: `1px solid ${calculateBorderColor(theme, color)}`,
   flexShrink: 0,
 }));
