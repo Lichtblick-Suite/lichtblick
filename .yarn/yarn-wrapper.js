f3660ad73e5f19b4a04dc5bd216fd5bc842d3d52
Roman Shtylman <roman@foxglove.dev>
2022-10-14 18:07:23 -0700

    Improve yarn-wrapper warning (#4600)
    Even after running `corepack enable` some developers report continuing
    to see the error
    message for enabling corepack. We've tracked this down to instances
    where the user installed
    yarn at the system level in a way that would take precedence over the
    version corepack installs.
    
    This change adds language to the error message guiding users to
    uninstall their global version
    so the corepack version runs instead.

--- .yarn/yarn-wrapper.js
+++ .yarn/yarn-wrapper.js
@@ -7,6 +7,10 @@ const path = require("path");
 if (process.env.COREPACK_ROOT == undefined) {
   console.error("This repository uses corepack. Enable corepack by running `corepack enable`");
   console.error("Learn more at: https://nodejs.org/api/corepack.html");
+  console.error("");
+  console.error(
+    "If you have run `corepack enable` and still see this error, you have likely installed yarn globally or using your system package manager. Your installed version of yarn is superseding corepack's version. Delete or uninstall your version of yarn to use the corepack version.",
+  );
   process.exit(1);
 }
 
