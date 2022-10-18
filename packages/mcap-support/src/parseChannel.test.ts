fb2cbb1d42fe5411494a87297304dcd6d685011e
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-10-18 12:38:31 -0700

    Upgrade protobufjs to fix syntax error (#4663)
    **User-Facing Changes**
    Fixed an issue where certain Protobuf enum values would fail to load
    with a SyntaxError.
    
    **Description**
    Fix was in https://github.com/protobufjs/protobuf.js/pull/1801. Added a
    unit test to ensure parsing succeeds.
    
    Example file containing dummy enum that reproduces the issue before this
    fix:
    [dupe_enum.mcap.zip](https://github.com/foxglove/studio/files/9814444/dupe_enum.mcap.zip)

--- packages/mcap-support/src/parseChannel.test.ts
+++ packages/mcap-support/src/parseChannel.test.ts
@@ -50,6 +50,37 @@ describe("parseChannel", () => {
     expect(deserialized.file[0]!.name).toEqual("google_protobuf.proto");
   });
 
+  it("handles protobuf repeated enum having multiple default value aliases", () => {
+    /*
+    syntax = "proto3";
+
+    enum ExampleEnum {
+        option allow_alias = true;
+        UNKNOWN = 0;
+        WHATEVER = 0;
+        FOO = 1;
+        BAR = 2;
+    }
+
+    message ExampleMessage {
+        repeated ExampleEnum data = 1;
+    }
+    */
+    const channel = parseChannel({
+      messageEncoding: "protobuf",
+      schema: {
+        name: "ExampleMessage",
+        encoding: "protobuf",
+        data: Buffer.from(
+          "0A8D010A156578616D706C655F6D6573736167652E70726F746F222C0A0E4578616D706C654D657373616765121A0A046461746118012003280E320C2E4578616D706C65456E756D2A3E0A0B4578616D706C65456E756D120B0A07554E4B4E4F574E1000120C0A085748415445564552100012070A03464F4F100112070A0342415210021A021001620670726F746F33",
+          "hex",
+        ),
+      },
+    });
+    expect(channel.fullSchemaName).toEqual("ExampleMessage");
+    expect(channel.deserializer(Buffer.from("0A0101", "hex"))).toEqual({ data: [1] });
+  });
+
   it("works with ros1", () => {
     const channel = parseChannel({
       messageEncoding: "ros1",
