5100a09ef28bceac276b21e7f75b1e14339ad3a2
Adrian Macneil <adrian@foxglove.dev>
2023-01-04 12:50:22 +1300

    Analytics: update capitalization (#5062)
    Consistent with console

--- packages/studio-base/src/services/IAnalytics.ts
+++ packages/studio-base/src/services/IAnalytics.ts
@@ -14,49 +14,49 @@ enum AppEventCategory {
 }
 
 enum AppEvent {
-  APP_INIT = "Studio: App initiated",
+  APP_INIT = "Studio: App Initialized",
 
   // Dialog events
-  DIALOG_SELECT_VIEW = "Studio: Dialog view selected",
-  DIALOG_CLOSE = "Studio: Dialog closed",
-  DIALOG_CLICK_CTA = "Studio: Dialog CTA clicked",
+  DIALOG_SELECT_VIEW = "Studio: Dialog View Selected",
+  DIALOG_CLOSE = "Studio: Dialog Closed",
+  DIALOG_CLICK_CTA = "Studio: Dialog CTA Clicked",
 
   // Player events
-  PLAYER_CONSTRUCTED = "Studio: Player constructed",
-  PLAYER_PLAY = "Studio: Player played",
-  PLAYER_SEEK = "Studio: Player seeked",
-  PLAYER_SET_SPEED = "Studio: Player speed set",
-  PLAYER_PAUSE = "Studio: Player paused",
-  PLAYER_CLOSE = "Studio: Player closed",
+  PLAYER_CONSTRUCTED = "Studio: Player Constructed",
+  PLAYER_PLAY = "Studio: Player Played",
+  PLAYER_SEEK = "Studio: Player Seeked",
+  PLAYER_SET_SPEED = "Studio: Player Speed Set",
+  PLAYER_PAUSE = "Studio: Player Paused",
+  PLAYER_CLOSE = "Studio: Player Closed",
 
   // Layout events
-  LAYOUT_UPDATE = "Studio: Layout updated",
-  LAYOUT_CREATE = "Studio: Layout created",
-  LAYOUT_DUPLICATE = "Studio: Layout duplicated",
-  LAYOUT_RENAME = "Studio: Layout renamed",
-  LAYOUT_DELETE = "Studio: Layout deleted",
-  LAYOUT_SELECT = "Studio: Layout selected",
-  LAYOUT_IMPORT = "Studio: Layout imported",
-  LAYOUT_EXPORT = "Studio: Layout exported",
-  LAYOUT_SHARE = "Studio: Layout shared",
-  LAYOUT_OVERWRITE = "Studio: Layout overwritten",
-  LAYOUT_REVERT = "Studio: Layout reverted",
-  LAYOUT_MAKE_PERSONAL_COPY = "Studio: Layout personal copy made",
+  LAYOUT_UPDATE = "Studio: Layout Updated",
+  LAYOUT_CREATE = "Studio: Layout Created",
+  LAYOUT_DUPLICATE = "Studio: Layout Duplicated",
+  LAYOUT_RENAME = "Studio: Layout Renamed",
+  LAYOUT_DELETE = "Studio: Layout Deleted",
+  LAYOUT_SELECT = "Studio: Layout Selected",
+  LAYOUT_IMPORT = "Studio: Layout Imported",
+  LAYOUT_EXPORT = "Studio: Layout Exported",
+  LAYOUT_SHARE = "Studio: Layout Shared",
+  LAYOUT_OVERWRITE = "Studio: Layout Overwritten",
+  LAYOUT_REVERT = "Studio: Layout Reverted",
+  LAYOUT_MAKE_PERSONAL_COPY = "Studio: Layout Personal Copy Made",
 
   // Panel events
-  PANEL_ADD = "Studio: Panel added",
-  PANEL_DELETE = "Studio: Panel deleted",
+  PANEL_ADD = "Studio: Panel Added",
+  PANEL_DELETE = "Studio: Panel Deleted",
 
   // Variable events
-  VARIABLE_ADD = "Studio: Variable added",
-  VARIABLE_DELETE = "Studio: Variable deleted",
+  VARIABLE_ADD = "Studio: Variable Added",
+  VARIABLE_DELETE = "Studio: Variable Deleted",
 
   // Extension events
-  EXTENSION_INSTALL = "Studio: Extension installed",
-  EXTENSION_UNINSTALL = "Studio: Extension uninstalled",
+  EXTENSION_INSTALL = "Studio: Extension Installed",
+  EXTENSION_UNINSTALL = "Studio: Extension Uninstalled",
 
   // Experimental features
-  EXPERIMENTAL_FEATURE_TOGGLE = "Studio: Experimental feature toggled",
+  EXPERIMENTAL_FEATURE_TOGGLE = "Studio: Experimental Feature Toggled",
 }
 
 /** https://develop.sentry.dev/sdk/event-payloads/breadcrumbs/#breadcrumb-types */
