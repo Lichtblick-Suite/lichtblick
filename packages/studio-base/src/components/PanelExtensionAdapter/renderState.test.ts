18e1b0268d306c720dd6b3970b4e7e7f3566b230
Sam Nosenzo <sam@foxglove.dev>
2023-01-31 13:33:46 -0500

    `renderState.allFrames` made sorted by `msg.receiveTime` (#5226)
    **User-Facing Changes**
    <!-- will be used as a changelog entry -->
    - [extension-panels] - `renderState.allFrames` will be sorted by message
    `receiveTime`
    
    **Description**
    Add function to merge sorted topics arrays to form sorted `allFrames`.
    This allows extension panels to have some more stability in allFrames
    and make assumptions about it that could allow for performance
    optimizations. This does scale the time-complexity by the number of
    preloaded topics, but in most cases that should be fairly small.
    
    
    <!-- link relevant GitHub issues -->
    FG-1631
    <!-- add `docs` label if this PR requires documentation updates -->
    <!-- add relevant metric tracking for experimental / new features -->

--- packages/studio-base/src/components/PanelExtensionAdapter/renderState.test.ts
+++ packages/studio-base/src/components/PanelExtensionAdapter/renderState.test.ts
@@ -4,7 +4,7 @@
 
 import { PlayerPresence } from "@foxglove/studio-base/players/types";
 
-import { initRenderStateBuilder } from "./renderState";
+import { forEachSortedArrays, initRenderStateBuilder } from "./renderState";
 
 describe("renderState", () => {
   it("should include convertibleTo when there are message converters", () => {
@@ -118,6 +118,171 @@ describe("renderState", () => {
           message: {},
           receiveTime: { sec: 0, nsec: 0 },
           sizeInBytes: 1,
+        },
+      ],
+    });
+  });
+
+  it("should make allFrames sorted receive time across sorted messagesInTopic", () => {
+    const buildRenderState = initRenderStateBuilder();
+    const state = buildRenderState({
+      watchedFields: new Set(["topics", "allFrames"]),
+      playerState: {
+        presence: PlayerPresence.INITIALIZING,
+        capabilities: [],
+        profile: undefined,
+        playerId: "test",
+        progress: {
+          messageCache: {
+            startTime: { sec: 0, nsec: 0 },
+            blocks: [
+              {
+                sizeInBytes: 0,
+                messagesByTopic: {
+                  test1: [
+                    {
+                      topic: "test1",
+                      schemaName: "schema",
+                      receiveTime: { sec: 1, nsec: 0 },
+                      sizeInBytes: 1,
+                      message: { from: "allFrames" },
+                    },
+                    {
+                      topic: "test1",
+                      schemaName: "schema",
+                      receiveTime: { sec: 3, nsec: 0 },
+                      sizeInBytes: 1,
+                      message: { from: "allFrames" },
+                    },
+                    {
+                      topic: "test1",
+                      schemaName: "schema",
+                      receiveTime: { sec: 5, nsec: 0 },
+                      sizeInBytes: 1,
+                      message: { from: "allFrames" },
+                    },
+                    {
+                      topic: "test1",
+                      schemaName: "schema",
+                      receiveTime: { sec: 6, nsec: 0 },
+                      sizeInBytes: 1,
+                      message: { from: "allFrames" },
+                    },
+                  ],
+                  test2: [
+                    {
+                      topic: "test2",
+                      schemaName: "schema",
+                      receiveTime: { sec: 2, nsec: 0 },
+                      sizeInBytes: 1,
+                      message: { from: "allFrames" },
+                    },
+                    {
+                      topic: "test2",
+                      schemaName: "schema",
+                      receiveTime: { sec: 4, nsec: 0 },
+                      sizeInBytes: 1,
+                      message: { from: "allFrames" },
+                    },
+                    {
+                      topic: "test2",
+                      schemaName: "schema",
+                      receiveTime: { sec: 7, nsec: 0 },
+                      sizeInBytes: 1,
+                      message: { from: "allFrames" },
+                    },
+                    {
+                      topic: "test2",
+                      schemaName: "schema",
+                      receiveTime: { sec: 8, nsec: 0 },
+                      sizeInBytes: 1,
+                      message: { from: "allFrames" },
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      },
+      appSettings: undefined,
+      currentFrame: [],
+      colorScheme: undefined,
+      globalVariables: {},
+      hoverValue: undefined,
+      sharedPanelState: {},
+      sortedTopics: [
+        { name: "test1", schemaName: "schema" },
+        { name: "test2", schemaName: "schema" },
+      ],
+      subscriptions: [
+        { topic: "test1", preload: true },
+        { topic: "test2", preload: true },
+      ],
+      messageConverters: [],
+    });
+
+    expect(state).toEqual({
+      topics: [
+        { name: "test1", schemaName: "schema", datatype: "schema" },
+        { name: "test2", schemaName: "schema", datatype: "schema" },
+      ],
+      allFrames: [
+        {
+          topic: "test1",
+          receiveTime: { nsec: 0, sec: 1 },
+          schemaName: "schema",
+          sizeInBytes: 1,
+          message: { from: "allFrames" },
+        },
+        {
+          topic: "test2",
+          schemaName: "schema",
+          receiveTime: { nsec: 0, sec: 2 },
+          sizeInBytes: 1,
+          message: { from: "allFrames" },
+        },
+        {
+          topic: "test1",
+          receiveTime: { nsec: 0, sec: 3 },
+          schemaName: "schema",
+          sizeInBytes: 1,
+          message: { from: "allFrames" },
+        },
+        {
+          topic: "test2",
+          schemaName: "schema",
+          receiveTime: { nsec: 0, sec: 4 },
+          sizeInBytes: 1,
+          message: { from: "allFrames" },
+        },
+        {
+          topic: "test1",
+          receiveTime: { nsec: 0, sec: 5 },
+          schemaName: "schema",
+          sizeInBytes: 1,
+          message: { from: "allFrames" },
+        },
+        {
+          topic: "test1",
+          schemaName: "schema",
+          receiveTime: { nsec: 0, sec: 6 },
+          sizeInBytes: 1,
+          message: { from: "allFrames" },
+        },
+        {
+          topic: "test2",
+          receiveTime: { nsec: 0, sec: 7 },
+          schemaName: "schema",
+          sizeInBytes: 1,
+          message: { from: "allFrames" },
+        },
+        {
+          topic: "test2",
+          schemaName: "schema",
+          receiveTime: { nsec: 0, sec: 8 },
+          sizeInBytes: 1,
+          message: { from: "allFrames" },
         },
       ],
     });
@@ -313,5 +478,82 @@ describe("renderState", () => {
         },
       ],
     });
+  });
+});
+
+describe("forEachSortedArrays", () => {
+  it("should not call forEach for empty arrays", () => {
+    const forEach = jest.fn();
+    const arr: number[] = [];
+    forEachSortedArrays([arr, arr], (a, b) => a - b, forEach);
+    expect(forEach).not.toHaveBeenCalled();
+  });
+  it("merges arrays with exclusive ranges", () => {
+    const acc: number[] = [];
+    const forEach = (item: number) => acc.push(item);
+    const arr1 = [1, 2, 3];
+    const arr2 = [4, 5, 6];
+
+    forEachSortedArrays([arr1, arr2], (a, b) => a - b, forEach);
+    expect(acc).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+  it("merges two interleaved arrays", () => {
+    const acc: number[] = [];
+    const forEach = (item: number) => acc.push(item);
+    const arr1 = [1, 3, 5];
+    const arr2 = [2, 4, 6];
+
+    forEachSortedArrays([arr1, arr2], (a, b) => a - b, forEach);
+    expect(acc).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+  it("merges three interleaved arrays", () => {
+    const acc: number[] = [];
+    const forEach = (item: number) => acc.push(item);
+    const arr1 = [1, 4, 7];
+    const arr2 = [2, 5, 8];
+    const arr3 = [3, 6, 9];
+
+    forEachSortedArrays([arr1, arr2, arr3], (a, b) => a - b, forEach);
+    expect(acc).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+  it("merges three exclusive arrays", () => {
+    const acc: number[] = [];
+    const forEach = (item: number) => acc.push(item);
+    const arr1 = [4, 5, 6];
+    const arr2 = [1, 2, 3];
+    const arr3 = [7, 8, 9];
+
+    forEachSortedArrays([arr1, arr2, arr3], (a, b) => a - b, forEach);
+    expect(acc).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+  it("merges three identical arrays", () => {
+    const acc: number[] = [];
+    const forEach = (item: number) => acc.push(item);
+    const arr1 = [1, 2, 3];
+    const arr2 = [1, 2, 3];
+    const arr3 = [1, 2, 3];
+
+    forEachSortedArrays([arr1, arr2, arr3], (a, b) => a - b, forEach);
+    expect(acc).toEqual([1, 1, 1, 2, 2, 2, 3, 3, 3]);
+  });
+  it("merges two identical arrays and one empty array", () => {
+    const acc: number[] = [];
+    const forEach = (item: number) => acc.push(item);
+    const arr1 = [1, 2, 3];
+    const arr2 = [1, 2, 3];
+    const arr3: number[] = [];
+
+    forEachSortedArrays([arr1, arr2, arr3], (a, b) => a - b, forEach);
+    expect(acc).toEqual([1, 1, 2, 2, 3, 3]);
+  });
+  it("merge arrays of all the same number and a sequence of numbers", () => {
+    const acc: number[] = [];
+    const forEach = (item: number) => acc.push(item);
+    const arr1 = [3, 3, 3];
+    const arr2 = [1, 2, 3, 4];
+    const arr3: number[] = [];
+
+    forEachSortedArrays([arr1, arr2, arr3], (a, b) => a - b, forEach);
+    expect(acc).toEqual([1, 2, 3, 3, 3, 3, 4]);
   });
 });
--- packages/studio-base/src/components/PanelExtensionAdapter/renderState.ts
+++ packages/studio-base/src/components/PanelExtensionAdapter/renderState.ts
@@ -4,7 +4,7 @@
 
 import { filterMap } from "@foxglove/den/collection";
 import Log from "@foxglove/log";
-import { toSec } from "@foxglove/rostime";
+import { compare, toSec } from "@foxglove/rostime";
 import {
   AppSettingValue,
   MessageEvent,
@@ -280,22 +280,23 @@ function initRenderStateBuilder(): BuildRenderStateFn {
         shouldRender = true;
         const frames: MessageEvent<unknown>[] = (renderState.allFrames = []);
         // only populate allFrames with topics that the panel wants to preload
-        const topicsToPreloadForPanel = new Set<string>(
-          Array.from(
+        const topicsToPreloadForPanel = Array.from(
+          new Set<string>(
             filterMap(subscriptions, (sub) => (sub.preload === true ? sub.topic : undefined)),
           ),
         );
+
         for (const block of newBlocks) {
           if (!block) {
             continue;
           }
 
-          for (const topic of topicsToPreloadForPanel) {
-            const messageEvents = block.messagesByTopic[topic];
-            if (!messageEvents) {
-              continue;
-            }
-            for (const messageEvent of messageEvents) {
+          // Given that messagesByTopic should be in order by receiveTime
+          // We need to combine all of the messages into a single array and sorted by receive time
+          forEachSortedArrays(
+            topicsToPreloadForPanel.map((topic) => block.messagesByTopic[topic] ?? []),
+            (a, b) => compare(a.receiveTime, b.receiveTime),
+            (messageEvent) => {
               // Message blocks may contain topics that we are not subscribed to so we need to filter those out.
               // We use the topicNoConversions and topicConversions to determine if we should include the message event
 
@@ -320,8 +321,8 @@ function initRenderStateBuilder(): BuildRenderStateFn {
                   });
                 }
               }
-            }
-          }
+            },
+          );
         }
       }
       prevBlocks = newBlocks;
@@ -381,3 +382,52 @@ function initRenderStateBuilder(): BuildRenderStateFn {
 }
 
 export { initRenderStateBuilder };
+
+/**
+ * Function to iterate and call function over multiple sorted arrays in sorted order across all items in all arrays.
+ * Time complexity is O(t*n) where t is the number of arrays and n is the total number of items in all arrays.
+ * Space complexity is O(t) where t is the number of arrays.
+ * @param arrays - sorted arrays to iterate over
+ * @param compareFn - function called to compare items in arrays. Returns a positive value if left is larger than right,
+ *  a negative value if right is larger than left, or zero if both are equal
+ * @param forEach - callback to be executed on all items in the arrays to iterate over in sorted order across all arrays
+ */
+export function forEachSortedArrays<Item>(
+  arrays: Item[][],
+  compareFn: (a: Item, b: Item) => number,
+  forEach: (item: Item) => void,
+): void {
+  const cursors: number[] = Array(arrays.length).fill(0);
+  if (arrays.length === 0) {
+    return;
+  }
+  for (;;) {
+    let minCursorIndex = undefined;
+    for (let i = 0; i < cursors.length; i++) {
+      const cursor = cursors[i]!;
+      const array = arrays[i]!;
+      if (cursor >= array.length) {
+        continue;
+      }
+      const item = array[cursor]!;
+      if (minCursorIndex == undefined) {
+        minCursorIndex = i;
+      } else {
+        const minItem = arrays[minCursorIndex]![cursors[minCursorIndex]!]!;
+        if (compareFn(item, minItem) < 0) {
+          minCursorIndex = i;
+        }
+      }
+    }
+    if (minCursorIndex == undefined) {
+      break;
+    }
+    const minItem = arrays[minCursorIndex]![cursors[minCursorIndex]!];
+    if (minItem != undefined) {
+      forEach(minItem);
+      cursors[minCursorIndex]++;
+    } else {
+      break;
+    }
+  }
+}
