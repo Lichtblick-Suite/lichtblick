2e8ceef16807fb536b29d46c7eaa98a71254852c
John Hurliman <jhurliman@jhurliman.org>
2023-02-17 11:33:16 -0800

    Switch from `zstd-codec` to `@foxglove/wasm-zstd` (#5378)
    **User-Facing Changes**
    - None
    
    **Description**
    This switches from the zstd-codec module, which could only decompress
    with the zstd streaming API, to @foxglove/wasm-zstd which has an API
    similar to @foxglove/wasm-lz4 and can decompress given a byte buffer and
    target decompressed size. A few related packages were bumped along the
    way.

--- packages/mcap-support/src/decompressHandlers.ts
+++ packages/mcap-support/src/decompressHandlers.ts
@@ -3,7 +3,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { McapTypes } from "@mcap/core";
-import type { ZstdModule, ZstdStreaming } from "zstd-codec";
 
 let handlersPromise: Promise<McapTypes.DecompressHandlers> | undefined;
 export async function loadDecompressHandlers(): Promise<McapTypes.DecompressHandlers> {
@@ -12,9 +11,10 @@ export async function loadDecompressHandlers(): Promise<McapTypes.DecompressHand
 
 // eslint-disable-next-line no-underscore-dangle
 async function _loadDecompressHandlers(): Promise<McapTypes.DecompressHandlers> {
-  const [zstd, decompressLZ4, bzip2] = await Promise.all([
-    import("zstd-codec").then(async ({ ZstdCodec }) => {
-      return await new Promise<ZstdModule>((resolve) => ZstdCodec.run(resolve));
+  const [decompressZstd, decompressLZ4, bzip2] = await Promise.all([
+    import("@foxglove/wasm-zstd").then(async (mod) => {
+      await mod.isLoaded;
+      return mod.decompress;
     }),
     import("@foxglove/wasm-lz4").then(async (mod) => {
       await mod.default.isLoaded;
@@ -23,36 +23,12 @@ async function _loadDecompressHandlers(): Promise<McapTypes.DecompressHandlers> 
     import("@foxglove/wasm-bz2").then(async (mod) => await mod.default.init()),
   ]);
 
-  let zstdStreaming: ZstdStreaming | undefined;
-
   return {
     lz4: (buffer, decompressedSize) => decompressLZ4(buffer, Number(decompressedSize)),
 
     bz2: (buffer, decompressedSize) =>
       bzip2.decompress(buffer, Number(decompressedSize), { small: false }),
 
-    zstd: (buffer, decompressedSize) => {
-      if (!zstdStreaming) {
-        zstdStreaming = new zstd.Streaming();
-      }
-      // We use streaming decompression because the zstd-codec package has a limited (and
-      // non-growable) amount of WASM memory, and does not currently support passing the
-      // decompressedSize into the simple one-shot decode() function.
-      // https://github.com/yoshihitoh/zstd-codec/issues/223
-      const result = zstdStreaming.decompressChunks(
-        (function* () {
-          const chunkSize = 4 * 1024 * 1024;
-          const endOffset = buffer.byteOffset + buffer.byteLength;
-          for (let offset = buffer.byteOffset; offset < endOffset; offset += chunkSize) {
-            yield new Uint8Array(buffer.buffer, offset, Math.min(chunkSize, endOffset - offset));
-          }
-        })(),
-        Number(decompressedSize),
-      );
-      if (!result) {
-        throw new Error("Decompression failed");
-      }
-      return result;
-    },
+    zstd: (buffer, decompressedSize) => decompressZstd(buffer, Number(decompressedSize)),
   };
 }
