6ba03d6d7963148b4e50f51dc5390b615bb70026
Roman Shtylman <roman@foxglove.dev>
2023-02-23 15:16:08 -0800

    Change web-integration test to launch local web server (#5407)
    Rather than trying to query the vercel preview deployment, change the
    web integration test to build locally, start a local web server, and
    open a url to the local webserver. This matches what we do for the
    desktop integration test.

--- web/integration-test/startup.test.ts
+++ web/integration-test/startup.test.ts
@@ -2,7 +2,10 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import http from "http";
+import path from "path";
 import { chromium } from "playwright";
+import serveHandler from "serve-handler";
 
 import Logger from "@foxglove/log";
 
@@ -11,28 +14,46 @@ const log = Logger.getLogger(__filename);
 describe("startup", () => {
   it("should start the application", async () => {
     expect.assertions(0); // just needs to complete without error
-    if (typeof process.env.BASE_URL === "string") {
-      const browser = await chromium.launch();
-      const page = await browser.newPage();
-      await page.goto(process.env.BASE_URL);
 
-      await new Promise<void>((resolve, reject) => {
-        page.on("console", (message) => {
-          if (message.type() === "error") {
-            reject(new Error(message.text()));
-            return;
-          }
-          log.info(message.text());
+    const publicPath = path.join(__dirname, "..", ".webpack");
 
-          if (message.text().includes("App rendered")) {
-            resolve();
-          }
-        });
+    const server = http.createServer(async (request, response) => {
+      return await serveHandler(request, response, {
+        public: publicPath,
       });
+    });
 
-      await browser.close();
-    } else {
-      throw new Error("BASE_URL is not set");
-    }
+    await new Promise<void>((resolve) => {
+      server.listen(0, resolve);
+    });
+
+    const port = (server.address() as any).port;
+    const url = `http://localhost:${port}`;
+
+    // eslint-disable-next-line no-restricted-syntax
+    console.info(`Running at ${url}`);
+
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    await new Promise<void>((resolve, reject) => {
+      page.on("console", (message) => {
+        if (message.type() === "error") {
+          reject(new Error(message.text()));
+          return;
+        }
+        log.info(message.text());
+
+        if (message.text().includes("App rendered")) {
+          resolve();
+        }
+      });
+    });
+
+    await browser.close();
+    await new Promise<void>((resolve) => {
+      server.close(() => resolve());
+    });
   }, 15_000);
 });
