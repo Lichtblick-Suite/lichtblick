562a1306684094a72ebcf7295d3322ee48907a5c
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-03-08 10:20:57 -0800

    Fix uncaught error in desktop on non-Windows platforms (#5507)
    **User-Facing Changes**
    None
    
    **Description**
    Fixes STUDIO-BC5
    Fixes STUDIO-BCP

--- desktop/main/StudioWindow.ts
+++ desktop/main/StudioWindow.ts
@@ -161,7 +161,10 @@ function newStudioWindow(deepLinks: string[] = []): BrowserWindow {
 
   const browserWindow = new BrowserWindow(windowOptions);
   nativeTheme.on("updated", () => {
-    browserWindow.setTitleBarOverlay(getTitleBarOverlayOptions());
+    if (isWindows) {
+      // Although the TS types say this function is always available, it is undefined on non-Windows platforms
+      browserWindow.setTitleBarOverlay(getTitleBarOverlayOptions());
+    }
   });
 
   // Forward full screen events to the renderer
@@ -204,7 +207,9 @@ function newStudioWindow(deepLinks: string[] = []): BrowserWindow {
     switch (channel) {
       case "titleBarDoubleClicked": {
         const action: string =
-          systemPreferences.getUserDefault("AppleActionOnDoubleClick", "string") || "Maximize";
+          // Although the TS types say this function is always available, it is undefined on non-Mac platforms
+          (isMac && systemPreferences.getUserDefault("AppleActionOnDoubleClick", "string")) ||
+          "Maximize";
         if (action === "Minimize") {
           browserWindow.minimize();
         } else if (action === "Maximize") {
