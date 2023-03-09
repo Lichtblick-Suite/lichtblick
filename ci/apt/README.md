72e759bef626e9e0fde8a820b66294e7dc3fcb99
Roman Shtylman <roman@foxglove.dev>
2023-03-09 06:46:27 -0800

    Move apt and snap release to release repo (#5509)
    Remove the apt and snap release process. These live in the release repo.
    
    Fixes: FG-2385
    Fixes: FG-2384

--- ci/apt/README.md
+++
@@ -1,8 +0,0 @@
-# Foxglove Apt Repository
-
-These are scripts to manage our apt repository (hosted at https://apt.foxglove.dev).
-
-## How it works
-
-- GitHub Actions runs [Aptly](https://www.aptly.info/) on every release.
-- The generated repos are published to [foxglove/apt.foxglove.dev](https://github.com/foxglove/apt.foxglove.dev) and served by Github Pages at https://apt.foxglove.dev.
--- ci/exec.ts
+++ ci/exec.ts
@@ -2,14 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { group } from "@actions/core";
 import { exec as execAction, ExecOptions } from "@actions/exec";
-
-export async function exec(program: string, args?: string[], options?: ExecOptions): Promise<void> {
-  await group(`$ ${program} ${args?.join(" ")}`, async () => {
-    await execAction(program, args, options);
-  });
-}
 
 export async function execOutput(
   program: string,
