Lichtblick-Suite/lichtblick

commit e41862dbd8a8ca0636d1f14d18042afe76e7fc25

Delete URDF Viewer panel and urdf/xacro drag & drop functionality (#5634)
**User-Facing Changes**
The deprecated URDF Viewer panel has been removed in this release. Use
the 3D panel to view URDF models:
https://foxglove.dev/blog/adding-urdfs-to-foxglove-studios-3d-panel

**Description**

- Removes URDFViewer panel and feature flag from #5442
- Removes Assets context and URDFAssetLoader
- Removes urdf/xacro file icons and file associations
- Removes ability to drag & drop urdf/xacro files, or double-click them
to open with Studio

Closes FG-1116
Closes FG-1547
See also https://github.com/foxglove/studio/pull/4875

packages/studio-base/src/Workspace.tsx
@@ -52,8 +52,6 @@ import {
 } from "@foxglove/studio-base/components/StudioLogsSettings";
 import { SyncAdapters } from "@foxglove/studio-base/components/SyncAdapters";
 import VariablesList from "@foxglove/studio-base/components/VariablesList";
-import { useAnalytics } from "@foxglove/studio-base/context/AnalyticsContext";
-import { useAssets } from "@foxglove/studio-base/context/AssetsContext";
 import {
   LayoutState,
   useCurrentLayoutSelector,
@@ -199,7 +197,7 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
 
   // file types we support for drag/drop
   const allowedDropExtensions = useMemo(() => {
-    const extensions = [".foxe", ".urdf", ".xacro"];
+    const extensions = [".foxe"];
     for (const source of availableSources) {
       if (source.type === "file" && source.supportedFileTypes) {
         extensions.push(...source.supportedFileTypes);
@@ -361,9 +359,6 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
 
   const { enqueueSnackbar } = useSnackbar();
 
-  const { loadFromFile } = useAssets();
-  const analytics = useAnalytics();
-
   const installExtension = useExtensionCatalog((state) => state.installExtension);
 
   const openHandle = useCallback(
@@ -372,8 +367,6 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
     ) => {
       log.debug("open handle", handle);
       const file = await handle.getFile();
-      // electron extends File with a `path` field which is not available in browsers
-      const basePath = (file as { path?: string }).path ?? "";
 
       if (file.name.endsWith(".foxe")) {
         // Extension installation
@@ -388,15 +381,6 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
             variant: "error",
           });
         }
-      } else {
-        try {
-          if (await loadFromFile(file, { basePath, analytics, source: "local_file" })) {
-            return;
-          }
-        } catch (err) {
-          log.error(err);
-          enqueueSnackbar(`Failed to load ${file.name}: ${err.message}`, { variant: "error" });
-        }
       }
 
       // Look for a source that supports the file extensions
@@ -408,7 +392,7 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
         selectSource(matchedSource.id, { type: "file", handle });
       }
     },
-    [analytics, availableSources, enqueueSnackbar, installExtension, loadFromFile, selectSource],
+    [availableSources, enqueueSnackbar, installExtension, selectSource],
   );
 
   const openFiles = useCallback(
@@ -417,9 +401,6 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
       log.debug("open files", files);
 
       for (const file of files) {
-        // electron extends File with a `path` field which is not available in browsers
-        const basePath = (file as { path?: string }).path ?? "";
-
         if (file.name.endsWith(".foxe")) {
           // Extension installation
           try {
@@ -432,15 +413,6 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
             enqueueSnackbar(`Failed to install extension ${file.name}: ${err.message}`, {
               variant: "error",
             });
-          }
-        } else {
-          try {
-            if (!(await loadFromFile(file, { basePath, analytics, source: "local_file" }))) {
-              otherFiles.push(file);
-            }
-          } catch (err) {
-            log.error(err);
-            enqueueSnackbar(`Failed to load ${file.name}: ${err.message}`, { variant: "error" });
           }
         }
       }
@@ -461,7 +433,7 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
         }
       }
     },
-    [analytics, availableSources, enqueueSnackbar, installExtension, loadFromFile, selectSource],
+    [availableSources, enqueueSnackbar, installExtension, selectSource],
   );
 
   // files the main thread told us to open

packages/studio-base/src/App.tsx
@@ -2,7 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { useState, Suspense, Fragment, useEffect } from "react";
+import { Suspense, Fragment, useEffect } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
@@ -22,7 +22,6 @@ import PlayerManager from "./components/PlayerManager";
 import SendNotificationToastAdapter from "./components/SendNotificationToastAdapter";
 import StudioToastProvider from "./components/StudioToastProvider";
 import AppConfigurationContext, { IAppConfiguration } from "./context/AppConfigurationContext";
-import { AssetsProvider } from "./context/AssetsContext";
 import LayoutStorageContext from "./context/LayoutStorageContext";
 import NativeAppMenuContext, { INativeAppMenu } from "./context/NativeAppMenuContext";
 import NativeWindowContext, { INativeWindow } from "./context/NativeWindowContext";
@@ -37,7 +36,6 @@ import UserProfileLocalStorageProvider from "./providers/UserProfileLocalStorage
 import { LaunchPreference } from "./screens/LaunchPreference";
 import { ExtensionLoader } from "./services/ExtensionLoader";
 import { ILayoutStorage } from "./services/ILayoutStorage";
-import URDFAssetLoader from "./services/URDFAssetLoader";
 
 type AppProps = CustomWindowControlsProps & {
   deepLinks: string[];
@@ -65,8 +63,6 @@ function contextMenuHandler(event: MouseEvent) {
 }
 
 export function App(props: AppProps): JSX.Element {
-  const [assetLoaders] = useState(() => [new URDFAssetLoader()]);
-
   const {
     appConfiguration,
     dataSources,
@@ -87,7 +83,6 @@ export function App(props: AppProps): JSX.Element {
     <LayoutStorageContext.Provider value={layoutStorage} />,
     <UserProfileLocalStorageProvider />,
     <LayoutManagerProvider />,
-    <AssetsProvider loaders={assetLoaders} />,
     <TimelineInteractionStateProvider />,
     <UserNodeStateProvider />,
     <CurrentLayoutProvider />,

packages/studio-base/src/AppSetting.ts
@@ -22,7 +22,6 @@ export enum AppSetting {
   SHOW_DEBUG_PANELS = "showDebugPanels",
   ENABLE_LEGACY_PLOT_PANEL = "enableLegacyPlotPanel",
   ENABLE_NEW_TOPNAV = "enableNewTopNav",
-  ENABLE_URDF_VIEWER = "enableUrdfViewer",
   ENABLE_ROS2_NATIVE_DATA_SOURCE = "enableRos2NativeDataSource",
 
   // Miscellaneous

packages/studio-base/src/PanelAPI/index.ts
@@ -22,6 +22,4 @@ export { useMessageReducer } from "./useMessageReducer";
 export { useMessagesByTopic } from "./useMessagesByTopic";
 export { useBlocksByTopic } from "./useBlocksByTopic";
 
-export { default as useParameter } from "./useParameter";
-
 export { default as useConfigById } from "./useConfigById";

packages/studio-base/src/PanelAPI/useParameter.ts
@@ -1,24 +1,0 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/
-
-import { useCallback } from "react";
-
-import { ParameterValue } from "@foxglove/studio";
-import { useMessagePipeline } from "@foxglove/studio-base/components/MessagePipeline";
-
-export default function useParameter<T extends ParameterValue>(
-  key: string,
-): [T | undefined, (value: T) => void] {
-  const value = useMessagePipeline(
-    useCallback((context) => context.playerState.activeData?.parameters?.get(key), [key]),
-  );
-  const setParameter = useMessagePipeline(useCallback((context) => context.setParameter, []));
-  const setValue = useCallback(
-    (newValue: T) => {
-      setParameter(key, newValue);
-    },
-    [key, setParameter],
-  );
-  return [value as T | undefined, setValue];
-}

packages/studio-base/src/components/ExperimentalFeatureSettings.tsx
@@ -56,11 +56,6 @@ function useFeatures(): Feature[] {
       description: <>{t("legacyPlotPanelDescription")}</>,
     },
     {
-      key: AppSetting.ENABLE_URDF_VIEWER,
-      name: t("urdfPanel"),
-      description: <>{t("urdfPanelDescription")}</>,
-    },
-    {
       key: AppSetting.ENABLE_MEMORY_USE_INDICATOR,
       name: t("memoryUseIndicator"),
       description: <>{t("memoryUseIndicatorDescription")}</>,

packages/studio-base/src/context/AssetsContext.tsx
@@ -1,94 +1,0 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/
-
-import { createContext, useCallback, useContext, useState } from "react";
-import { URDFRobot } from "urdf-loader";
-import { v4 as uuidv4 } from "uuid";
-
-import { useShallowMemo } from "@foxglove/hooks";
-import IAnalytics from "@foxglove/studio-base/services/IAnalytics";
-
-export type Asset = { name: string } & { type: "urdf"; model: URDFRobot };
-type AssetWithUUID = Asset & { uuid: string };
-
-type Assets = {
-  /** Returns true if the file was successfully loaded as an asset. */
-  loadFromFile: (
-    file: File,
-    options: {
-      basePath: string | undefined;
-      analytics: IAnalytics;
-      source: "param" | "local_file";
-    },
-  ) => Promise<boolean>;
-  assets: readonly AssetWithUUID[];
-};
-
-const AssetsContext = createContext<Assets>({
-  loadFromFile: async () => {
-    throw new Error("Cannot load assets outside AssetsProvider");
-  },
-  assets: [],
-});
-AssetsContext.displayName = "AssetsContext";
-
-export interface AssetLoader {
-  /**
-   * @returns A successfully loaded asset, or undefined if this loader should not be used to load this asset.
-   */
-  load(
-    file: File,
-    options: {
-      basePath: string | undefined;
-      analytics: IAnalytics;
-      source: "param" | "local_file";
-    },
-  ): Promise<Asset | undefined>;
-}
-
-/**
- * Parse a ROS `package://` URL (ex: "package://camera_drivers/resource/calibration/001.yaml")
- * into targetPkg ("camera_drivers)") and relPath ("/resource/calibration/001.yaml") components.
- */
-export function parsePackageUrl(url: string): { targetPkg: string; relPath: string } | undefined {
-  const match = url.match(/^package:\/\/([^/]+)\/(.+)$/);
-  return match ? { targetPkg: match[1]!, relPath: match[2]! } : undefined;
-}
-
-export function useAssets(): Assets {
-  return useContext(AssetsContext);
-}
-
-export function AssetsProvider({
-  loaders,
-  children,
-}: React.PropsWithChildren<{ loaders: AssetLoader[] }>): React.ReactElement {
-  const [assets, setAssets] = useState<AssetWithUUID[]>([]);
-
-  const value: Assets = useShallowMemo({
-    assets,
-    loadFromFile: useCallback(
-      async (file, options) => {
-        for (const loader of loaders) {
-          const result = await loader.load(file, options);
-          if (result != undefined) {
-            setAssets((items) =>
-              items
-                .concat({ ...result, uuid: uuidv4() })
-                .sort((a, b) => a.name.localeCompare(b.name)),
-            );
-            return true;
-          }
-        }
-        return false;
-      },
-      [loaders],
-    ),
-  });
-
-  return <AssetsContext.Provider value={value}>{children}</AssetsContext.Provider>;
-}
-
-// ts-prune-ignore-next
-export default AssetsContext;

packages/studio-base/src/i18n/en/preferences.ts
@@ -31,8 +31,6 @@ export default {
   studioDebugPanelsDescription: "Show Foxglove Studio debug panels in the “Add panel” list.",
   legacyPlotPanel: "Legacy Plot panel",
   legacyPlotPanelDescription: "Enable the Legacy Plot panel.",
-  urdfPanel: "URDF Viewer panel",
-  urdfPanelDescription: "Enable the deprecated URDF Viewer panel.",
   memoryUseIndicator: "Memory use indicator",
   memoryUseIndicatorDescription: "Show the app memory use in the sidebar.",
   newNavigation: "New navigation",

commit 5e78015b4e74be4fee135c9847fbd4998200765b

Fix double-click to open bag/mcap files (#5693)
**User-Facing Changes**
Fixed an issue with double-clicking to open files in the desktop app
(bag, mcap, etc.).

**Description**
This regressed in https://github.com/foxglove/studio/pull/5634 since
this line of code was accidentally deleted.
Fixes #5692

packages/studio-base/src/Workspace.tsx
@@ -417,6 +417,8 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
               variant: "error",
             });
           }
+        } else {
+          otherFiles.push(file);
         }
       }
 