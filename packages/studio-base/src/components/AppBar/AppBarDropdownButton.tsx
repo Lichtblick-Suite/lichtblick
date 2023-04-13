33f1b440e980ffd28ffb315e468a005e03640118
Andrew Carr <2metres@users.noreply.github.com>
2023-04-13 11:39:42 +1000

    Allow optional label to be passed to AppBarDropdownButton (#5766)
    **User-Facing Changes**
    None (behind feature flag)
    
    **Description**
    <img width="348" alt="Screen Shot 2023-04-13 at 11 14 47 am"
    src="https://user-images.githubusercontent.com/924528/231621437-edbfd95b-fa8d-4f7c-962e-6f2424038055.png">

--- packages/studio-base/src/components/AppBar/AppBarDropdownButton.tsx
+++ packages/studio-base/src/components/AppBar/AppBarDropdownButton.tsx
@@ -3,25 +3,36 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { ChevronDown12Filled } from "@fluentui/react-icons";
-import { ButtonBase } from "@mui/material";
+import { ButtonBase, Typography } from "@mui/material";
 import { forwardRef } from "react";
 import tinycolor2 from "tinycolor2";
 import { makeStyles } from "tss-react/mui";
 
+import {
+  APP_BAR_FOREGROUND_COLOR,
+  APP_BAR_HEIGHT,
+  APP_BAR_PRIMARY_COLOR,
+} from "@foxglove/studio-base/components/AppBar/constants";
+import Stack from "@foxglove/studio-base/components/Stack";
 import TextMiddleTruncate from "@foxglove/studio-base/components/TextMiddleTruncate";
-
-import { APP_BAR_FOREGROUND_COLOR, APP_BAR_PRIMARY_COLOR } from "./constants";
 
 const useStyles = makeStyles()((theme) => ({
   textTruncate: {
     maxWidth: "18vw",
     overflow: "hidden",
   },
+  subheader: {
+    fontSize: 8,
+    opacity: 0.6,
+  },
   layoutButton: {
     font: "inherit",
+    height: APP_BAR_HEIGHT,
     fontSize: theme.typography.body2.fontSize,
-    padding: theme.spacing(1.875, 2),
-    gap: theme.spacing(0.5),
+    justifyContent: "space-between",
+    minWidth: 120,
+    padding: theme.spacing(1.125, 1.5),
+    gap: theme.spacing(1.5),
     borderRadius: 0,
 
     ":hover": {
@@ -35,6 +46,7 @@ const useStyles = makeStyles()((theme) => ({
 
 type Props = {
   title: string;
+  subheader?: string;
   selected: boolean;
   onClick: () => void;
 };
@@ -43,7 +55,7 @@ type Props = {
  * A button that can be used in the app bar to open a dropdown menu.
  */
 const AppBarDropdownButton = forwardRef<HTMLButtonElement, Props>((props, ref) => {
-  const { title, onClick, selected } = props;
+  const { title, subheader, onClick, selected } = props;
   const { classes, cx } = useStyles();
 
   return (
@@ -53,9 +65,16 @@ const AppBarDropdownButton = forwardRef<HTMLButtonElement, Props>((props, ref) =
       onClick={onClick}
       ref={ref}
     >
-      <div className={classes.textTruncate}>
-        <TextMiddleTruncate text={title} />
-      </div>
+      <Stack alignItems="flex-start">
+        {subheader && (
+          <Typography variant="overline" className={classes.subheader}>
+            {subheader}
+          </Typography>
+        )}
+        <div className={classes.textTruncate}>
+          <TextMiddleTruncate text={title} />
+        </div>
+      </Stack>
       <ChevronDown12Filled />
     </ButtonBase>
   );
