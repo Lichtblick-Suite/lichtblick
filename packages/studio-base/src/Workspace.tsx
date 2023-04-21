Lichtblick-Suite/lichtblick

commit 57e4487c2e8f3180c8a0c7c1f98c6c810351a520

Move Data Source dialog state into workspace context (and rename) (#5792)
**User-Facing Changes**
None

**Description**
Moves OpenDialog state into workspace context so that it can be opened
from within the AppBar and AppMenu. Rename to more accurately describe
component's functionality in the global workspace.

<!-- link relevant GitHub issues -->
<!-- add `docs` label if this PR requires documentation updates -->
<!-- add relevant metric tracking for experimental / new features -->

---------

Co-authored-by: Miles Egan <93935560+foxymiles@users.noreply.github.com>
Co-authored-by: Jacob Bandes-Storch <jacob@foxglove.dev>

packages/studio-base/src/Workspace.tsx
@@ -14,7 +14,7 @@ import { Link, Typography } from "@mui/material";
 import { useSnackbar } from "notistack";
 import { extname } from "path";
 import { useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
-import { useTranslation, Trans } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { makeStyles } from "tss-react/mui";
 
 import Logger from "@foxglove/log";
@@ -22,6 +22,10 @@ import { AppSetting } from "@foxglove/studio-base/AppSetting";
 import AccountSettings from "@foxglove/studio-base/components/AccountSettingsSidebar/AccountSettings";
 import { AppBar } from "@foxglove/studio-base/components/AppBar";
 import { CustomWindowControlsProps } from "@foxglove/studio-base/components/AppBar/CustomWindowControls";
+import {
+  DataSourceDialog,
+  DataSourceDialogItem,
+} from "@foxglove/studio-base/components/DataSourceDialog";
 import { DataSourceSidebar } from "@foxglove/studio-base/components/DataSourceSidebar";
 import { EventsList } from "@foxglove/studio-base/components/DataSourceSidebar/EventsList";
 import { TopicList } from "@foxglove/studio-base/components/DataSourceSidebar/TopicList";
@@ -35,7 +39,6 @@ import {
   useMessagePipelineGetter,
 } from "@foxglove/studio-base/components/MessagePipeline";
 import MultiProvider from "@foxglove/studio-base/components/MultiProvider";
-import { OpenDialog, OpenDialogViews } from "@foxglove/studio-base/components/OpenDialog";
 import PanelLayout from "@foxglove/studio-base/components/PanelLayout";
 import PanelList from "@foxglove/studio-base/components/PanelList";
 import PanelSettings from "@foxglove/studio-base/components/PanelSettings";
@@ -61,17 +64,14 @@ import { useCurrentUser } from "@foxglove/studio-base/context/CurrentUserContext
 import { EventsStore, useEvents } from "@foxglove/studio-base/context/EventsContext";
 import { useExtensionCatalog } from "@foxglove/studio-base/context/ExtensionCatalogContext";
 import { useNativeAppMenu } from "@foxglove/studio-base/context/NativeAppMenuContext";
-import {
-  IDataSourceFactory,
-  usePlayerSelection,
-} from "@foxglove/studio-base/context/PlayerSelectionContext";
+import { usePlayerSelection } from "@foxglove/studio-base/context/PlayerSelectionContext";
 import {
   LeftSidebarItemKey,
   RightSidebarItemKey,
   SidebarItemKey,
+  WorkspaceContextStore,
   useWorkspaceActions,
   useWorkspaceStore,
-  WorkspaceContextStore,
 } from "@foxglove/studio-base/context/WorkspaceContext";
 import { useAppConfigurationValue } from "@foxglove/studio-base/hooks";
 import useAddPanel from "@foxglove/studio-base/hooks/useAddPanel";
@@ -158,6 +158,8 @@ type WorkspaceProps = CustomWindowControlsProps & {
 const DEFAULT_DEEPLINKS = Object.freeze([]);
 
 const selectPlayerPresence = ({ playerState }: MessagePipelineContext) => playerState.presence;
+const selectPlayerIsPresent = ({ playerState }: MessagePipelineContext) =>
+  playerState.presence !== PlayerPresence.NOT_PRESENT;
 const selectPlayerProblems = ({ playerState }: MessagePipelineContext) => playerState.problems;
 const selectIsPlaying = (ctx: MessagePipelineContext) =>
   ctx.playerState.activeData?.isPlaying === true;
@@ -167,6 +169,8 @@ const selectSeek = (ctx: MessagePipelineContext) => ctx.seekPlayback;
 const selectPlayUntil = (ctx: MessagePipelineContext) => ctx.playUntil;
 const selectPlayerId = (ctx: MessagePipelineContext) => ctx.playerState.playerId;
 const selectEventsSupported = (store: EventsStore) => store.eventsSupported;
+
+const selectWorkspaceDataSourceDialog = (store: WorkspaceContextStore) => store.dataSourceDialog;
 const selectWorkspaceSidebarItem = (store: WorkspaceContextStore) => store.sidebarItem;
 const selectWorkspaceLeftSidebarItem = (store: WorkspaceContextStore) => store.leftSidebarItem;
 const selectWorkspaceLeftSidebarOpen = (store: WorkspaceContextStore) => store.leftSidebarOpen;
@@ -183,6 +187,7 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
   const playerProblems = useMessagePipeline(selectPlayerProblems);
 
   const sidebarItem = useWorkspaceStore(selectWorkspaceSidebarItem);
+  const dataSourceDialog = useWorkspaceStore(selectWorkspaceDataSourceDialog);
   const leftSidebarItem = useWorkspaceStore(selectWorkspaceLeftSidebarItem);
   const leftSidebarOpen = useWorkspaceStore(selectWorkspaceLeftSidebarOpen);
   const leftSidebarSize = useWorkspaceStore(selectWorkspaceLeftSidebarSize);
@@ -191,14 +196,15 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
   const rightSidebarSize = useWorkspaceStore(selectWorkspaceRightSidebarSize);
 
   const {
+    dataSourceDialogActions,
     prefsDialogActions,
-    setLeftSidebarOpen,
-    setRightSidebarOpen,
     selectLeftSidebarItem,
     selectRightSidebarItem,
-    setLeftSidebarSize,
-    setRightSidebarSize,
     selectSidebarItem,
+    setLeftSidebarOpen,
+    setLeftSidebarSize,
+    setRightSidebarOpen,
+    setRightSidebarSize,
   } = useWorkspaceActions();
 
   // file types we support for drag/drop
@@ -216,8 +222,6 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
   // see comment below above the RemountOnValueChange component
   const playerId = useMessagePipeline(selectPlayerId);
 
-  const isPlayerPresent = playerPresence !== PlayerPresence.NOT_PRESENT;
-
   const { currentUser, signIn } = useCurrentUser();
   const supportsAccountSettings = signIn != undefined;
 
@@ -225,9 +229,6 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
 
   useDefaultWebLaunchPreference();
 
-  const [showOpenDialogOnStartup = true] = useAppConfigurationValue<boolean>(
-    AppSetting.SHOW_OPEN_DIALOG_ON_STARTUP,
-  );
   const [enableStudioLogsSidebar = false] = useAppConfigurationValue<boolean>(
     AppSetting.SHOW_DEBUG_PANELS,
   );
@@ -242,19 +243,15 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
 
   const showSignInForm = currentUserRequired && currentUser == undefined;
 
-  const [showOpenDialog, setShowOpenDialog] = useState<
-    { view: OpenDialogViews; activeDataSource?: IDataSourceFactory } | undefined
-  >(isPlayerPresent || !showOpenDialogOnStartup || showSignInForm ? undefined : { view: "start" });
-
   // When a player is activated, hide the open dialog.
   useLayoutEffect(() => {
     if (
       playerPresence === PlayerPresence.PRESENT ||
       playerPresence === PlayerPresence.INITIALIZING
     ) {
-      setShowOpenDialog(undefined);
+      dataSourceDialogActions.close();
     }
-  }, [playerPresence]);
+  }, [playerPresence, dataSourceDialogActions]);
 
   useEffect(() => {
     // Focus on page load to enable keyboard interaction.
@@ -314,23 +311,17 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
 
   useNativeAppMenuEvent(
     "open-file",
-    useCallback(() => {
-      setShowOpenDialog({ view: "file" });
-    }, []),
+    useCallback(() => dataSourceDialogActions.open("file"), [dataSourceDialogActions]),
   );
 
   useNativeAppMenuEvent(
     "open-remote-file",
-    useCallback(() => {
-      setShowOpenDialog({ view: "remote" });
-    }, []),
+    useCallback(() => dataSourceDialogActions.open("remote"), [dataSourceDialogActions]),
   );
 
   useNativeAppMenuEvent(
     "open-sample-data",
-    useCallback(() => {
-      setShowOpenDialog({ view: "demo" });
-    }, []),
+    useCallback(() => dataSourceDialogActions.open("demo"), [dataSourceDialogActions]),
   );
 
   const nativeAppMenu = useNativeAppMenu();
@@ -346,7 +337,7 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
 
     for (const item of connectionSources) {
       nativeAppMenu.addFileEntry(item.displayName, () => {
-        setShowOpenDialog({ view: "connection", activeDataSource: item });
+        dataSourceDialogActions.open("connection", item);
       });
     }
 
@@ -355,7 +346,7 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
         nativeAppMenu.removeFileEntry(item.displayName);
       }
     };
-  }, [connectionSources, nativeAppMenu, selectSource]);
+  }, [connectionSources, dataSourceDialogActions, nativeAppMenu]);
 
   const { enqueueSnackbar } = useSnackbar();
 
@@ -466,12 +457,7 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
   // open the data source dialog.
   const DataSourceSidebarItem = useMemo(() => {
     return function DataSourceSidebarItemImpl() {
-      return (
-        <DataSourceSidebar
-          disableToolbar={enableNewTopNav}
-          onSelectDataSourceAction={() => setShowOpenDialog({ view: "start" })}
-        />
-      );
+      return <DataSourceSidebar disableToolbar={enableNewTopNav} />;
     };
   }, [enableNewTopNav]);
 
@@ -620,13 +606,7 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
       ]}
     >
       {showSignInForm && <SignInFormModal />}
-      {showOpenDialog != undefined && (
-        <OpenDialog
-          activeView={showOpenDialog.view}
-          activeDataSource={showOpenDialog.activeDataSource}
-          onDismiss={() => setShowOpenDialog(undefined)}
-        />
-      )}
+      {dataSourceDialog.open && <DataSourceDialog />}
       <DocumentDropListener onDrop={dropHandler} allowedExtensions={allowedDropExtensions} />
       <SyncAdapters />
       <KeyListener global keyDownHandlers={keyDownHandlers} />
@@ -641,7 +621,6 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
             onMaximizeWindow={props.onMaximizeWindow}
             onUnmaximizeWindow={props.onUnmaximizeWindow}
             onCloseWindow={props.onCloseWindow}
-            onSelectDataSourceAction={() => setShowOpenDialog({ view: "start" })}
           />
         )}
         <Sidebars
@@ -686,8 +665,31 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
 }
 
 export default function Workspace(props: WorkspaceProps): JSX.Element {
+  const [showOpenDialogOnStartup = true] = useAppConfigurationValue<boolean>(
+    AppSetting.SHOW_OPEN_DIALOG_ON_STARTUP,
+  );
+
+  const { currentUser } = useCurrentUser();
+
+  const { currentUserRequired } = useInitialDeepLinkState(props.deepLinks ?? DEFAULT_DEEPLINKS);
+
+  const isPlayerPresent = useMessagePipeline(selectPlayerIsPresent);
+
+  const showSignInForm = currentUserRequired && currentUser == undefined;
+
+  const initialItem: undefined | DataSourceDialogItem =
+    isPlayerPresent || !showOpenDialogOnStartup || showSignInForm ? undefined : "start";
+
+  const initialState: Pick<WorkspaceContextStore, "dataSourceDialog"> = {
+    dataSourceDialog: {
+      activeDataSource: undefined,
+      open: initialItem != undefined,
+      item: initialItem,
+    },
+  };
+
   return (
-    <WorkspaceContextProvider>
+    <WorkspaceContextProvider initialState={initialState}>
       <WorkspaceContent {...props} />
     </WorkspaceContextProvider>
   );

packages/studio-base/src/components/AppBar/DataSource.tsx
@@ -21,6 +21,7 @@ import {
 import Stack from "@foxglove/studio-base/components/Stack";
 import TextMiddleTruncate from "@foxglove/studio-base/components/TextMiddleTruncate";
 import WssErrorModal from "@foxglove/studio-base/components/WssErrorModal";
+import { useWorkspaceActions } from "@foxglove/studio-base/context/WorkspaceContext";
 import { PlayerPresence } from "@foxglove/studio-base/players/types";
 
 const LEFT_ICON_SIZE = 19;
@@ -96,17 +97,14 @@ const selectPlayerName = ({ playerState }: MessagePipelineContext) => playerStat
 const selectPlayerPresence = ({ playerState }: MessagePipelineContext) => playerState.presence;
 const selectPlayerProblems = ({ playerState }: MessagePipelineContext) => playerState.problems;
 
-export function DataSource({
-  onSelectDataSourceAction,
-}: {
-  onSelectDataSourceAction: () => void;
-}): JSX.Element {
+export function DataSource(): JSX.Element {
   const { t } = useTranslation("appBar");
   const { classes, cx } = useStyles();
 
   const playerName = useMessagePipeline(selectPlayerName);
   const playerPresence = useMessagePipeline(selectPlayerPresence);
   const playerProblems = useMessagePipeline(selectPlayerProblems) ?? [];
+  const { dataSourceDialogActions } = useWorkspaceActions();
 
   const reconnecting = playerPresence === PlayerPresence.RECONNECTING;
   const initializing = playerPresence === PlayerPresence.INITIALIZING;
@@ -120,9 +118,11 @@ export function DataSource({
 
   const [problemModal, setProblemModal] = useState<JSX.Element | undefined>(undefined);
 
+  const openDataSourceDialog = () => dataSourceDialogActions.open("start");
+
   if (playerPresence === PlayerPresence.NOT_PRESENT) {
     return (
-      <ButtonBase className={classes.button} color="inherit" onClick={onSelectDataSourceAction}>
+      <ButtonBase className={classes.button} color="inherit" onClick={openDataSourceDialog}>
         {t("openDataSource")}
       </ButtonBase>
     );
@@ -133,7 +133,7 @@ export function DataSource({
       {problemModal}
       <WssErrorModal playerProblems={playerProblems} />
       <Stack direction="row" alignItems="center">
-        <ButtonBase className={classes.button} onClick={onSelectDataSourceAction}>
+        <ButtonBase className={classes.button} onClick={openDataSourceDialog}>
           <div className={cx(classes.adornment, { [classes.adornmentError]: error })}>
             {loading && (
               <CircularProgress

packages/studio-base/src/components/AppBar/index.test.tsx
@@ -48,7 +48,6 @@ describe("<AppBar />", () => {
           onMaximizeWindow={mockMaximize}
           onUnmaximizeWindow={mockUnmaximize}
           onCloseWindow={mockClose}
-          onSelectDataSourceAction={() => {}}
         />
       </Wrapper>,
     );
@@ -71,7 +70,6 @@ describe("<AppBar />", () => {
           onUnmaximizeWindow={mockUnmaximize}
           onCloseWindow={mockClose}
           isMaximized
-          onSelectDataSourceAction={() => {}}
         />
       </Wrapper>,
     );

packages/studio-base/src/components/AppBar/index.tsx
@@ -180,7 +180,6 @@ type AppBarProps = CustomWindowControlsProps & {
   onDoubleClick?: () => void;
   debugDragRegion?: boolean;
   disableSignIn?: boolean;
-  onSelectDataSourceAction: () => void;
 };
 
 const selectCurrentLayoutId = ({ selectedLayout }: LayoutState) => selectedLayout?.id;
@@ -196,7 +195,6 @@ export function AppBar(props: AppBarProps): JSX.Element {
     onDoubleClick,
     onMaximizeWindow,
     onMinimizeWindow,
-    onSelectDataSourceAction,
     onUnmaximizeWindow,
     showCustomWindowControls = false,
   } = props;
@@ -265,7 +263,7 @@ export function AppBar(props: AppBarProps): JSX.Element {
           </div>
 
           <div className={classes.middle}>
-            <DataSource onSelectDataSourceAction={onSelectDataSourceAction} />
+            <DataSource />
           </div>
 
           <div className={classes.end}>

packages/studio-base/src/components/DataSourceDialog/Connection.stories.tsx
@@ -2,17 +2,37 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { StoryObj } from "@storybook/react";
+import { StoryFn, StoryObj } from "@storybook/react";
 
 import PlayerSelectionContext, {
   PlayerSelection,
 } from "@foxglove/studio-base/context/PlayerSelectionContext";
+import WorkspaceContextProvider from "@foxglove/studio-base/providers/WorkspaceContextProvider";
 
-import OpenDialog, { OpenDialogProps } from "./OpenDialog";
+import { DataSourceDialog } from "./DataSourceDialog";
+
+const Wrapper = (Story: StoryFn): JSX.Element => {
+  return (
+    <WorkspaceContextProvider
+      initialState={{
+        dataSourceDialog: {
+          activeDataSource: undefined,
+          item: "connection",
+          open: true,
+        },
+      }}
+    >
+      <PlayerSelectionContext.Provider value={playerSelection}>
+        <Story />
+      </PlayerSelectionContext.Provider>
+    </WorkspaceContextProvider>
+  );
+};
 
 export default {
-  title: "components/OpenDialog/Connection",
-  component: OpenDialog,
+  title: "components/DataSourceDialog/Connection",
+  component: DataSourceDialog,
+  decorators: [Wrapper],
 };
 
 // Connection
@@ -61,15 +81,8 @@ const playerSelection: PlayerSelection = {
   ],
 };
 
-const defaultProps: OpenDialogProps = { activeView: "connection", backdropAnimation: false };
-
 export const Light: StoryObj = {
-  render: () => (
-    <PlayerSelectionContext.Provider value={playerSelection}>
-      <OpenDialog {...defaultProps} />
-    </PlayerSelectionContext.Provider>
-  ),
-
+  render: () => <DataSourceDialog backdropAnimation={false} />,
   name: "Default (light)",
   parameters: { colorScheme: "light" },
 };
@@ -77,16 +90,11 @@ export const Light: StoryObj = {
 export const LightChinese: StoryObj = {
   ...Light,
   name: "Default Chinese",
-  parameters: { forceLanguage: "zh" },
+  parameters: { forceLanguage: "zh", colorScheme: "light" },
 };
 
 export const Dark: StoryObj = {
-  render: () => (
-    <PlayerSelectionContext.Provider value={playerSelection}>
-      <OpenDialog {...defaultProps} />
-    </PlayerSelectionContext.Provider>
-  ),
-
+  render: () => <DataSourceDialog backdropAnimation={false} />,
   name: "Default (dark)",
   parameters: { colorScheme: "dark" },
 };

packages/studio-base/src/components/DataSourceDialog/Connection.tsx
@@ -9,21 +9,16 @@ import { makeStyles } from "tss-react/mui";
 import { BuiltinIcon } from "@foxglove/studio-base/components/BuiltinIcon";
 import Stack from "@foxglove/studio-base/components/Stack";
 import { useAnalytics } from "@foxglove/studio-base/context/AnalyticsContext";
+import { usePlayerSelection } from "@foxglove/studio-base/context/PlayerSelectionContext";
 import {
-  IDataSourceFactory,
-  usePlayerSelection,
-} from "@foxglove/studio-base/context/PlayerSelectionContext";
+  WorkspaceContextStore,
+  useWorkspaceActions,
+  useWorkspaceStore,
+} from "@foxglove/studio-base/context/WorkspaceContext";
 import { AppEvent } from "@foxglove/studio-base/services/IAnalytics";
 
 import { FormField } from "./FormField";
 import View from "./View";
-
-type ConnectionProps = {
-  onBack?: () => void;
-  onCancel?: () => void;
-  availableSources: IDataSourceFactory[];
-  activeSource?: IDataSourceFactory;
-};
 
 const useStyles = makeStyles()((theme) => ({
   grid: {
@@ -86,25 +81,35 @@ const useStyles = makeStyles()((theme) => ({
   },
 }));
 
-export default function Connection(props: ConnectionProps): JSX.Element {
-  const { availableSources, activeSource, onCancel, onBack } = props;
-  const { classes } = useStyles();
+const selectDataSourceDialog = (store: WorkspaceContextStore) => store.dataSourceDialog;
 
+export default function Connection(): JSX.Element {
+  const { classes } = useStyles();
   const theme = useTheme();
   const mdUp = useMediaQuery(theme.breakpoints.up("md"));
 
-  const { selectSource } = usePlayerSelection();
+  const { activeDataSource } = useWorkspaceStore(selectDataSourceDialog);
+  const { dataSourceDialogActions } = useWorkspaceActions();
+
+  const { availableSources, selectSource } = usePlayerSelection();
   const analytics = useAnalytics();
+
+  // connectionSources is the list of availableSources supporting "connections"
+  const connectionSources = useMemo(() => {
+    return availableSources.filter((source) => {
+      return source.type === "connection" && source.hidden !== true;
+    });
+  }, [availableSources]);
 
   // List enabled sources before disabled sources so the default selected item is an available source
   const enabledSourcesFirst = useMemo(() => {
-    const enabledSources = availableSources.filter((source) => source.disabledReason == undefined);
-    const disabledSources = availableSources.filter((source) => source.disabledReason);
+    const enabledSources = connectionSources.filter((source) => source.disabledReason == undefined);
+    const disabledSources = connectionSources.filter((source) => source.disabledReason);
     return [...enabledSources, ...disabledSources];
-  }, [availableSources]);
+  }, [connectionSources]);
 
   const [selectedConnectionIdx, setSelectedConnectionIdx] = useState<number>(() => {
-    const foundIdx = availableSources.findIndex((source) => source === activeSource);
+    const foundIdx = connectionSources.findIndex((source) => source === activeDataSource);
     const selectedIdx = foundIdx < 0 ? 0 : foundIdx;
     void analytics.logEvent(AppEvent.DIALOG_SELECT_VIEW, {
       type: "live",
@@ -122,11 +127,11 @@ export default function Connection(props: ConnectionProps): JSX.Element {
   const [fieldValues, setFieldValues] = useState<Record<string, string | undefined>>({});
 
   useLayoutEffect(() => {
-    const connectionIdx = availableSources.findIndex((source) => source === activeSource);
+    const connectionIdx = connectionSources.findIndex((source) => source === activeDataSource);
     if (connectionIdx >= 0) {
       setSelectedConnectionIdx(connectionIdx);
     }
-  }, [activeSource, availableSources]);
+  }, [activeDataSource, connectionSources]);
 
   // clear field values when the user changes the source tab
   useLayoutEffect(() => {
@@ -144,8 +149,16 @@ export default function Connection(props: ConnectionProps): JSX.Element {
       return;
     }
     selectSource(selectedSource.id, { type: "connection", params: fieldValues });
-    onCancel?.();
-  }, [onCancel, selectedSource, fieldValues, selectSource]);
+    void analytics.logEvent(AppEvent.DIALOG_CLOSE, { activeDataSource });
+    dataSourceDialogActions.close();
+  }, [
+    selectedSource,
+    selectSource,
+    fieldValues,
+    analytics,
+    activeDataSource,
+    dataSourceDialogActions,
+  ]);
 
   const disableOpen = selectedSource?.disabledReason != undefined || fieldErrors.size > 0;
 
@@ -160,7 +173,7 @@ export default function Connection(props: ConnectionProps): JSX.Element {
   );
 
   return (
-    <View onBack={onBack} onCancel={onCancel} onOpen={disableOpen ? undefined : onOpen}>
+    <View onOpen={disableOpen ? undefined : onOpen}>
       <Stack className={classes.grid}>
         <header className={classes.header}>
           <Typography variant="h3" fontWeight={600} gutterBottom>

commit 5bd89239e07b07d15632dd3007b628c7a07ebef4

Only mount one copy of useInitialDeepLinkState (#5803)
**User-Facing Changes**
None (issue was not in a release)

**Description**
useInitialDeepLinkState calls selectSource. There were two copies of the
hook, so two calls to selectSource, meaning we would create two players.

Fixes FG-3044
Regressed in #5792

packages/studio-base/src/Workspace.tsx
@@ -150,7 +150,7 @@ function ExtensionsSidebar() {
 }
 
 type WorkspaceProps = CustomWindowControlsProps & {
-  deepLinks?: string[];
+  deepLinks?: string[]; // eslint-disable-line react/no-unused-prop-types
   appBarLeftInset?: number;
   onAppBarDoubleClick?: () => void;
 };
@@ -179,7 +179,8 @@ const selectWorkspaceRightSidebarItem = (store: WorkspaceContextStore) => store.
 const selectWorkspaceRightSidebarOpen = (store: WorkspaceContextStore) => store.rightSidebarOpen;
 const selectWorkspaceRightSidebarSize = (store: WorkspaceContextStore) => store.rightSidebarSize;
 
-function WorkspaceContent(props: WorkspaceProps): JSX.Element {
+type WorkspaceContentProps = WorkspaceProps & { showSignInForm: boolean };
+function WorkspaceContent(props: WorkspaceContentProps): JSX.Element {
   const { classes } = useStyles();
   const containerRef = useRef<HTMLDivElement>(ReactNull);
   const { availableSources, selectSource } = usePlayerSelection();
@@ -225,8 +226,6 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
   const { currentUser, signIn } = useCurrentUser();
   const supportsAccountSettings = signIn != undefined;
 
-  const { currentUserRequired } = useInitialDeepLinkState(props.deepLinks ?? DEFAULT_DEEPLINKS);
-
   useDefaultWebLaunchPreference();
 
   const [enableStudioLogsSidebar = false] = useAppConfigurationValue<boolean>(
@@ -240,8 +239,6 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
 
   const [initialEnableNewTopNav] = useState(currentEnableNewTopNav);
   const enableNewTopNav = isDesktopApp() ? initialEnableNewTopNav : currentEnableNewTopNav;
-
-  const showSignInForm = currentUserRequired && currentUser == undefined;
 
   // When a player is activated, hide the open dialog.
   useLayoutEffect(() => {
@@ -605,7 +602,7 @@ function WorkspaceContent(props: WorkspaceProps): JSX.Element {
         /* eslint-enable react/jsx-key */
       ]}
     >
-      {showSignInForm && <SignInFormModal />}
+      {props.showSignInForm && <SignInFormModal />}
       {dataSourceDialog.open && <DataSourceDialog />}
       <DocumentDropListener onDrop={dropHandler} allowedExtensions={allowedDropExtensions} />
       <SyncAdapters />
@@ -690,7 +687,7 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
 
   return (
     <WorkspaceContextProvider initialState={initialState}>
-      <WorkspaceContent {...props} />
+      <WorkspaceContent showSignInForm={showSignInForm} {...props} />
     </WorkspaceContextProvider>
   );
 }

packages/studio-base/src/hooks/useInitialDeepLinkState.ts
@@ -122,11 +122,26 @@ function useSyncTimeFromUrl(targetUrlState: AppURLState | undefined) {
 }
 
 /**
+ * Ensure only one copy of the hook is mounted so we don't trigger side effects like selectSource
+ * more than once.
+ */
+let useInitialDeepLinkStateMounted = false;
+/**
  * Restores our session state from any deep link we were passed on startup.
  */
 export function useInitialDeepLinkState(deepLinks: readonly string[]): {
   currentUserRequired: boolean;
 } {
+  useEffect(() => {
+    if (useInitialDeepLinkStateMounted) {
+      throw new Error("Invariant: only one copy of useInitialDeepLinkState may be mounted");
+    }
+    useInitialDeepLinkStateMounted = true;
+    return () => {
+      useInitialDeepLinkStateMounted = false;
+    };
+  }, []);
+
   const { availableSources } = usePlayerSelection();
   const targetUrlState = useMemo(
     () => (deepLinks[0] ? parseAppURLState(new URL(deepLinks[0])) : undefined),