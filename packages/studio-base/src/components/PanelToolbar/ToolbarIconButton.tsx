673e29790f06010d8e6a3c1fe20ad4d8e4b72435
Andrew Carr <2metres@users.noreply.github.com>
2023-05-11 14:24:50 +1000

    Remove muiStyled from ToolbarIconButton (#5991)
    **User-Facing Changes**
    None
    
    **Description**
    Removes muiStyled from ToolbarIconButton in favor of tss-react

--- packages/studio-base/src/components/PanelToolbar/ToolbarIconButton.tsx
+++ packages/studio-base/src/components/PanelToolbar/ToolbarIconButton.tsx
@@ -2,38 +2,29 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { IconButton, IconButtonProps, styled as muiStyled } from "@mui/material";
+import { IconButton, IconButtonProps } from "@mui/material";
 import { ForwardedRef, forwardRef } from "react";
+import { makeStyles } from "tss-react/mui";
 
-const StyledIconButton = muiStyled(IconButton, {
-  shouldForwardProp: (prop) => prop !== "subMenuActive",
-})<{ subMenuActive: boolean }>(({ subMenuActive, theme }) => ({
-  padding: theme.spacing(0.375),
-  fontSize: "0.875rem",
+const useStyles = makeStyles()((theme) => ({
+  root: {
+    padding: theme.spacing(0.375),
+    fontSize: "0.875rem",
 
-  ...(subMenuActive && {
-    visibility: "visible",
-  }),
-  ".MuiSvgIcon-root, svg:not(.MuiSvgIcon-root)": {
-    height: "1em",
-    width: "1em",
-    fontSize: "inherit",
+    ".MuiSvgIcon-root, svg:not(.MuiSvgIcon-root)": {
+      height: "1em",
+      width: "1em",
+      fontSize: "inherit",
+    },
   },
 }));
 
 export default forwardRef(function ToolbarIconButton(
-  props: {
-    subMenuActive?: boolean;
-    title?: string;
-  } & Partial<IconButtonProps>,
+  props: IconButtonProps,
   ref: ForwardedRef<HTMLButtonElement>,
 ): React.ReactElement {
-  return (
-    <StyledIconButton
-      ref={ref}
-      subMenuActive={props.subMenuActive === true}
-      aria-label={props.title}
-      {...props}
-    />
-  );
+  const { className, ...rest } = props;
+  const { classes, cx } = useStyles();
+
+  return <IconButton ref={ref} className={cx(classes.root, className)} {...rest} />;
 });
