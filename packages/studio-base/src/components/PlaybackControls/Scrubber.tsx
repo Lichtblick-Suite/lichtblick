Lichtblick-Suite/lichtblick

commit 810a587706b4cbe4ac648320466817eccd8b78c8

improve playback scrubber precision using pointer events (#4514)
**User-Facing Changes**
None

**Description**
Subtle improvement to precision of playback scrubber. Scrubber and
tooltip remain aligned.

Simplified a bit of code in the Slider component. This component is only
used for the playback controls and does not need to be so generic.

packages/studio-base/src/components/PlaybackControls/Scrubber.tsx
@@ -9,7 +9,13 @@ import { useLatest } from "react-use";
 import { makeStyles } from "tss-react/mui";
 import { v4 as uuidv4 } from "uuid";
 
-import { subtract as subtractTimes, toSec, fromSec, Time } from "@foxglove/rostime";
+import {
+  subtract as subtractTimes,
+  add as addTimes,
+  toSec,
+  fromSec,
+  Time,
+} from "@foxglove/rostime";
 import {
   MessagePipelineContext,
   useMessagePipeline,
@@ -74,26 +80,41 @@ export default function Scrubber(props: Props): JSX.Element {
 
   const setHoverValue = useSetHoverValue();
 
-  const [hoverX, setHoverX] = useState<undefined | number>();
-
-  const onChange = useCallback((value: number) => onSeek(fromSec(value)), [onSeek]);
+  const [hoverStamp, setHoverStamp] = useState<Time | undefined>();
 
   const latestStartTime = useLatest(startTime);
-  const onHoverOver = useCallback(
-    (_x: number, value: number) => {
-      if (!latestStartTime.current || hoverElRef.current == undefined) {
+  const latestEndTime = useLatest(endTime);
+
+  const onChange = useCallback(
+    (fraction: number) => {
+      if (!latestStartTime.current || !latestEndTime.current) {
         return;
       }
-      const stamp = fromSec(value);
-      const timeFromStart = subtractTimes(stamp, latestStartTime.current);
-      setHoverX(value);
+      onSeek(
+        addTimes(
+          latestStartTime.current,
+          fromSec(fraction * toSec(subtractTimes(latestEndTime.current, latestStartTime.current))),
+        ),
+      );
+    },
+    [onSeek, latestEndTime, latestStartTime],
+  );
+
+  const onHoverOver = useCallback(
+    (fraction: number) => {
+      if (!latestStartTime.current || !latestEndTime.current || hoverElRef.current == undefined) {
+        return;
+      }
+      const duration = toSec(subtractTimes(latestEndTime.current, latestStartTime.current));
+      const timeFromStart = fromSec(fraction * duration);
+      setHoverStamp(addTimes(latestStartTime.current, timeFromStart));
       setHoverValue({
         componentId: hoverComponentId,
         type: "PLAYBACK_SECONDS",
         value: toSec(timeFromStart),
       });
     },
-    [hoverComponentId, latestStartTime, setHoverValue],
+    [hoverComponentId, latestEndTime, latestStartTime, setHoverValue],
   );
 
   const clearHoverValue = useClearHoverValue();
@@ -117,8 +138,10 @@ export default function Scrubber(props: Props): JSX.Element {
 
   const min = startTime && toSec(startTime);
   const max = endTime && toSec(endTime);
-  const value = currentTime == undefined ? undefined : toSec(currentTime);
-  const step = ((max ?? 100) - (min ?? 0)) / 500;
+  const fraction =
+    currentTime && startTime && endTime
+      ? toSec(subtractTimes(currentTime, startTime)) / toSec(subtractTimes(endTime, startTime))
+      : undefined;
 
   const loading = presence === PlayerPresence.INITIALIZING || presence === PlayerPresence.BUFFERING;
 
@@ -136,7 +159,7 @@ export default function Scrubber(props: Props): JSX.Element {
 
   return (
     <Tooltip
-      title={hoverX != undefined ? <PlaybackControlsTooltipContent hoverXPosition={hoverX} /> : ""}
+      title={hoverStamp != undefined ? <PlaybackControlsTooltipContent stamp={hoverStamp} /> : ""}
       placement="top"
       disableInteractive
       TransitionComponent={Fade}
@@ -185,11 +208,8 @@ export default function Scrubber(props: Props): JSX.Element {
         </Stack>
         <Stack fullHeight fullWidth position="absolute" flex={1}>
           <Slider
-            min={min ?? 0}
-            max={max ?? 100}
             disabled={min == undefined || max == undefined}
-            step={step}
-            value={value}
+            fraction={fraction}
             onHoverOver={onHoverOver}
             onHoverOut={onHoverOut}
             onChange={onChange}

packages/studio-base/src/components/PlaybackControls/Slider.tsx
@@ -3,29 +3,16 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { clamp } from "lodash";
-import {
-  useCallback,
-  useEffect,
-  useRef,
-  ReactNode,
-  useMemo,
-  useState,
-  useLayoutEffect,
-} from "react";
+import { useCallback, useEffect, useRef, ReactNode, useState, useLayoutEffect } from "react";
 import { makeStyles } from "tss-react/mui";
 
-import Logger from "@foxglove/log";
-
-const log = Logger.getLogger(__filename);
+import { scaleValue } from "@foxglove/den/math";
 
 type Props = {
-  value: number | undefined;
-  min: number;
-  max: number;
+  fraction: number | undefined;
   disabled?: boolean;
-  step?: number;
   onChange: (value: number) => void;
-  onHoverOver?: (clientX: number, value: number) => void;
+  onHoverOver?: (value: number) => void;
   onHoverOut?: () => void;
   renderSlider?: (value?: number) => ReactNode;
 };
@@ -62,10 +49,7 @@ function defaultRenderSlider(value: number | undefined, className: string): Reac
 
 export default function Slider(props: Props): JSX.Element {
   const {
-    value,
-    step = 0,
-    max = 0,
-    min = 0,
+    fraction,
     disabled = false,
     renderSlider = defaultRenderSlider,
     onHoverOver,
@@ -76,22 +60,14 @@ export default function Slider(props: Props): JSX.Element {
 
   const elRef = useRef<HTMLDivElement | ReactNull>(ReactNull);
 
-  const getValueAtMouse = useCallback(
-    (ev: React.MouseEvent | MouseEvent): number => {
-      if (!elRef.current) {
-        return 0;
-      }
-      const { left, width } = elRef.current.getBoundingClientRect();
-      const { clientX } = ev;
-      const t = (clientX - left) / width;
-      let interpolated = min + t * (max - min);
-      if (step !== 0) {
-        interpolated = Math.round(interpolated / step) * step;
-      }
-      return clamp(interpolated, min, max);
-    },
-    [max, min, step],
-  );
+  const getValueAtMouse = useCallback((ev: React.MouseEvent | MouseEvent): number => {
+    if (!elRef.current) {
+      return 0;
+    }
+    const { left, right } = elRef.current.getBoundingClientRect();
+    const scaled = scaleValue(ev.clientX, left, right, 0, 1);
+    return clamp(scaled, 0, 1);
+  }, []);
 
   const [mouseDown, setMouseDown] = useState(false);
   const mouseDownRef = useRef(mouseDown);
@@ -116,23 +92,22 @@ export default function Slider(props: Props): JSX.Element {
     }
   }, [onHoverOut]);
 
-  const onMouseUp = useCallback((): void => {
+  const onPointerUp = useCallback((): void => {
     setMouseDown(false);
     if (!mouseInsideRef.current) {
       onHoverOut?.();
     }
   }, [onHoverOut]);
 
-  const onMouseMove = useCallback(
-    (ev: React.MouseEvent | MouseEvent): void => {
+  const onPointerMove = useCallback(
+    (ev: React.PointerEvent | PointerEvent): void => {
       const val = getValueAtMouse(ev);
       if (disabled) {
         return;
       }
 
       if (elRef.current) {
-        const { left, right } = elRef.current.getBoundingClientRect();
-        onHoverOver?.(clamp(ev.clientX, left, right), val);
+        onHoverOver?.(val);
       }
       if (!mouseDownRef.current) {
         return;
@@ -142,8 +117,8 @@ export default function Slider(props: Props): JSX.Element {
     [disabled, getValueAtMouse, onChange, onHoverOver],
   );
 
-  const onMouseDown = useCallback(
-    (ev: React.MouseEvent<HTMLDivElement>): void => {
+  const onPointerDown = useCallback(
+    (ev: React.PointerEvent<HTMLDivElement>): void => {
       if (disabled) {
         return;
       }
@@ -158,40 +133,27 @@ export default function Slider(props: Props): JSX.Element {
   );
 
   useEffect(() => {
-    if (max < min) {
-      const msg = `Slider component given invalid range: ${min}, ${max}`;
-      log.error(new Error(msg));
-    }
-  }, [min, max]);
-
-  const sliderValue = useMemo(() => {
-    return value != undefined && max > min ? (value - min) / (max - min) : undefined;
-  }, [max, min, value]);
-
-  useEffect(() => {
     if (mouseDown) {
-      window.addEventListener("mouseup", onMouseUp);
-      window.addEventListener("mousemove", onMouseMove);
+      window.addEventListener("pointerup", onPointerUp);
       return () => {
-        window.removeEventListener("mouseup", onMouseUp);
-        window.removeEventListener("mousemove", onMouseMove);
+        window.removeEventListener("pointerup", onPointerUp);
       };
     }
     return undefined;
-  }, [mouseDown, onMouseMove, onMouseUp]);
+  }, [mouseDown, onPointerMove, onPointerUp]);
 
   return (
     <div
       ref={elRef}
-      onMouseDown={onMouseDown}
-      onMouseMove={onMouseMove}
+      onPointerDown={onPointerDown}
+      onPointerMove={onPointerMove}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
       className={cx(classes.root, {
         [classes.rootDisabled]: disabled,
       })}
     >
-      {renderSlider(sliderValue, classes.range)}
+      {renderSlider(fraction, classes.range)}
     </div>
   );
 }

packages/studio-base/src/components/PlaybackControls/PlaybackControlsTooltipContent.tsx
@@ -7,7 +7,7 @@ import { isEmpty } from "lodash";
 import { Fragment } from "react";
 import { makeStyles } from "tss-react/mui";
 
-import { subtract as subtractTimes, toSec, fromSec } from "@foxglove/rostime";
+import { subtract as subtractTimes, toSec, Time } from "@foxglove/rostime";
 import {
   MessagePipelineContext,
   useMessagePipeline,
@@ -50,10 +50,8 @@ const useStyles = makeStyles()((theme) => ({
 const selectHoveredEvents = (store: TimelineInteractionStateStore) => store.eventsAtHoverValue;
 const selectStartTime = (ctx: MessagePipelineContext) => ctx.playerState.activeData?.startTime;
 
-export function PlaybackControlsTooltipContent(params: {
-  hoverXPosition: number;
-}): ReactNull | JSX.Element {
-  const { hoverXPosition } = params;
+export function PlaybackControlsTooltipContent(params: { stamp: Time }): ReactNull | JSX.Element {
+  const { stamp } = params;
   const { formatTime, timeFormat } = useAppTimeFormat();
   const hoveredEvents = useTimelineInteractionState(selectHoveredEvents);
   const startTime = useMessagePipeline(selectStartTime);
@@ -63,7 +61,6 @@ export function PlaybackControlsTooltipContent(params: {
     return ReactNull;
   }
 
-  const stamp = fromSec(hoverXPosition);
   const timeFromStart = subtractTimes(stamp, startTime);
 
   const tooltipItems: PlaybackControlsTooltipItem[] = [];

packages/studio-base/src/components/PlaybackControls/Slider.stories.tsx
@@ -42,32 +42,30 @@ export default {
 };
 
 export const Examples: Story = () => {
-  const [value, setValue] = useState(50);
-  const [draggableValue, setDraggableValue] = useState(25);
+  const [value, setValue] = useState(0.5);
+  const [draggableValue, setDraggableValue] = useState(0.25);
   return (
     <Box padding={4}>
       <p>standard (clickable)</p>
       <Box bgcolor="error.light" height={30} width={300}>
-        <Slider min={10} max={200} onChange={(v) => setValue(v)} value={value} />
+        <Slider onChange={(v) => setValue(v)} fraction={value} />
       </Box>
       <p>disabled (not clickable)</p>
       <Box bgcolor="error.light" height={30} width={300}>
-        <Slider disabled min={10} max={200} onChange={(v) => setValue(v)} value={value} />
+        <Slider disabled onChange={(v) => setValue(v)} fraction={value} />
       </Box>
       <p>no value</p>
       <Box bgcolor="error.light" height={30} width={300}>
         <Slider
-          min={10}
-          max={200}
           onChange={() => {
             // no-op
           }}
-          value={undefined}
+          fraction={undefined}
         />
       </Box>
       <p>draggable</p>
       <Box bgcolor="info.main" height={20} width={500}>
-        <Slider min={10} max={200} onChange={(v) => setDraggableValue(v)} value={draggableValue} />
+        <Slider onChange={(v) => setDraggableValue(v)} fraction={draggableValue} />
       </Box>
     </Box>
   );
@@ -75,17 +73,15 @@ export const Examples: Story = () => {
 
 export const CustomRenderer: Story = () => {
   const { classes } = useStyles();
-  const [draggableValue, setDraggableValue] = useState(25);
+  const [draggableValue, setDraggableValue] = useState(0.25);
 
   return (
     <Box padding={4}>
       <p>Customize slider UI using renderSlider</p>
       <Box bgcolor="info.main" height={20} width={500}>
         <Slider
-          min={10}
-          max={200}
           onChange={(v) => setDraggableValue(v)}
-          value={draggableValue}
+          fraction={draggableValue}
           renderSlider={(width) => (
             <>
               <div className={classes.customRange} style={{ width: `${(width ?? 0) * 100}%` }} />

commit 50cf7b623ba307c5e5f5ccc77972ce7c5aeaecc4

Fix moving mouse off playback bar while scrubbing (#6265)
**User-Facing Changes**
Not worth calling out in release notes

**Description**

- Allows scrubbing to continue even if the mouse moves outside the
playback bar or window
- Tooltip remains visible the entire time

I made this work in https://github.com/foxglove/studio/pull/2912, then I
broke it again in https://github.com/foxglove/studio/pull/4514. The
pendulum swings back again.

Additionally I believe the behavior of showing the tooltip even when the
mouse is outside the bar changed in
https://github.com/foxglove/studio/pull/4392.

packages/studio-base/src/components/PlaybackControls/Scrubber.tsx
@@ -2,9 +2,9 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { Fade, Tooltip } from "@mui/material";
+import { Fade, PopperProps, Tooltip } from "@mui/material";
 import type { Instance } from "@popperjs/core";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useLatest } from "react-use";
 import { makeStyles } from "tss-react/mui";
 import { v4 as uuidv4 } from "uuid";
@@ -31,7 +31,7 @@ import { EventsOverlay } from "./EventsOverlay";
 import PlaybackBarHoverTicks from "./PlaybackBarHoverTicks";
 import { PlaybackControlsTooltipContent } from "./PlaybackControlsTooltipContent";
 import { ProgressPlot } from "./ProgressPlot";
-import Slider from "./Slider";
+import Slider, { HoverOverEvent } from "./Slider";
 
 const useStyles = makeStyles()((theme) => ({
   marker: {
@@ -70,7 +70,6 @@ export default function Scrubber(props: Props): JSX.Element {
   const { classes, cx } = useStyles();
 
   const [hoverComponentId] = useState<string>(() => uuidv4());
-  const hoverElRef = useRef<HTMLDivElement>(ReactNull);
 
   const startTime = useMessagePipeline(selectStartTime);
   const currentTime = useMessagePipeline(selectCurrentTime);
@@ -80,7 +79,13 @@ export default function Scrubber(props: Props): JSX.Element {
 
   const setHoverValue = useSetHoverValue();
 
-  const [hoverStamp, setHoverStamp] = useState<Time | undefined>();
+  type HoverInfo = {
+    stamp: Time;
+    clientX: number;
+    clientY: number;
+  };
+  const [hoverInfo, setHoverInfo] = useState<HoverInfo | undefined>();
+  const latestHoverInfo = useLatest(hoverInfo);
 
   const latestStartTime = useLatest(startTime);
   const latestEndTime = useLatest(endTime);
@@ -101,13 +106,13 @@ export default function Scrubber(props: Props): JSX.Element {
   );
 
   const onHoverOver = useCallback(
-    (fraction: number) => {
-      if (!latestStartTime.current || !latestEndTime.current || hoverElRef.current == undefined) {
+    ({ fraction, clientX, clientY }: HoverOverEvent) => {
+      if (!latestStartTime.current || !latestEndTime.current) {
         return;
       }
       const duration = toSec(subtractTimes(latestEndTime.current, latestStartTime.current));
       const timeFromStart = fromSec(fraction * duration);
-      setHoverStamp(addTimes(latestStartTime.current, timeFromStart));
+      setHoverInfo({ stamp: addTimes(latestStartTime.current, timeFromStart), clientX, clientY });
       setHoverValue({
         componentId: hoverComponentId,
         type: "PLAYBACK_SECONDS",
@@ -121,6 +126,7 @@ export default function Scrubber(props: Props): JSX.Element {
 
   const onHoverOut = useCallback(() => {
     clearHoverValue(hoverComponentId);
+    setHoverInfo(undefined);
   }, [clearHoverValue, hoverComponentId]);
 
   // Clean up the hover value when we are unmounted -- important for storybook.
@@ -147,57 +153,60 @@ export default function Scrubber(props: Props): JSX.Element {
 
   const popperRef = React.useRef<Instance>(ReactNull);
 
-  const positionRef = React.useRef({ x: 0, y: 0 });
+  const isHovered = hoverInfo != undefined;
+  const popperProps: Partial<PopperProps> = useMemo(
+    () => ({
+      open: isHovered, // Keep the tooltip visible while dragging even when the mouse is outside the playback bar
+      popperRef,
+      modifiers: [
+        {
+          name: "computeStyles",
+          options: {
+            gpuAcceleration: false, // Fixes hairline seam on arrow in chrome.
+          },
+        },
+        {
+          name: "offset",
+          options: {
+            // Offset popper to hug the track better.
+            offset: [0, 4],
+          },
+        },
+      ],
+      anchorEl: {
+        getBoundingClientRect: () => {
+          return new DOMRect(
+            latestHoverInfo.current?.clientX ?? 0,
+            latestHoverInfo.current?.clientY ?? 0,
+            0,
+            0,
+          );
+        },
+      },
+    }),
+    [isHovered, latestHoverInfo],
+  );
 
-  const handlePointerMove = (event: React.PointerEvent) => {
-    positionRef.current = { x: event.clientX, y: event.clientY };
-
+  useEffect(() => {
     if (popperRef.current != undefined) {
       void popperRef.current.update();
     }
-  };
+  }, [hoverInfo]);
 
   return (
     <Tooltip
-      title={hoverStamp != undefined ? <PlaybackControlsTooltipContent stamp={hoverStamp} /> : ""}
+      title={
+        hoverInfo != undefined ? <PlaybackControlsTooltipContent stamp={hoverInfo.stamp} /> : ""
+      }
       placement="top"
       disableInteractive
       TransitionComponent={Fade}
       TransitionProps={{ timeout: 0 }}
-      PopperProps={{
-        popperRef,
-        modifiers: [
-          {
-            name: "computeStyles",
-            options: {
-              gpuAcceleration: false, // Fixes hairline seam on arrow in chrome.
-            },
-          },
-          {
-            name: "offset",
-            options: {
-              // Offset popper to hug the track better.
-              offset: [0, -12],
-            },
-          },
-        ],
-        anchorEl: {
-          getBoundingClientRect: () => {
-            return new DOMRect(
-              positionRef.current.x,
-              hoverElRef.current?.getBoundingClientRect().y ?? 0,
-              0,
-              0,
-            );
-          },
-        },
-      }}
+      PopperProps={popperProps}
     >
       <Stack
-        ref={hoverElRef}
         direction="row"
         flexGrow={1}
-        onPointerMove={handlePointerMove}
         alignItems="center"
         position="relative"
         style={{ height: 32 }}

packages/studio-base/src/components/PlaybackControls/Slider.tsx
@@ -8,11 +8,20 @@ import { makeStyles } from "tss-react/mui";
 
 import { scaleValue } from "@foxglove/den/math";
 
+export type HoverOverEvent = {
+  /** Hovered `fraction` value */
+  fraction: number;
+  /** Current hovered X position in client coordinates */
+  clientX: number;
+  /** Current hovered Y position in client coordinates */
+  clientY: number;
+};
+
 type Props = {
   fraction: number | undefined;
   disabled?: boolean;
   onChange: (value: number) => void;
-  onHoverOver?: (value: number) => void;
+  onHoverOver?: (event: HoverOverEvent) => void;
   onHoverOut?: () => void;
   renderSlider?: (value?: number) => ReactNode;
 };
@@ -101,13 +110,24 @@ export default function Slider(props: Props): JSX.Element {
 
   const onPointerMove = useCallback(
     (ev: React.PointerEvent | PointerEvent): void => {
-      const val = getValueAtMouse(ev);
+      if (mouseDownRef.current && ev.currentTarget !== window) {
+        // onPointerMove is used on the <div/> for hovering, and on the window for dragging. While
+        // dragging we only want to pay attention to the window events (otherwise we'd be handling
+        // each event twice).
+        return;
+      }
       if (disabled) {
         return;
       }
 
+      const val = getValueAtMouse(ev);
       if (elRef.current) {
-        onHoverOver?.(val);
+        const elRect = elRef.current.getBoundingClientRect();
+        onHoverOver?.({
+          fraction: val,
+          clientX: ev.clientX,
+          clientY: elRect.y + elRect.height / 2,
+        });
       }
       if (!mouseDownRef.current) {
         return;
@@ -135,8 +155,10 @@ export default function Slider(props: Props): JSX.Element {
   useEffect(() => {
     if (mouseDown) {
       window.addEventListener("pointerup", onPointerUp);
+      window.addEventListener("pointermove", onPointerMove);
       return () => {
         window.removeEventListener("pointerup", onPointerUp);
+        window.removeEventListener("pointermove", onPointerMove);
       };
     }
     return undefined;