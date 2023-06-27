68df870edbddc81bec34ae69b4562c82ba2f9c29
Andrew Carr <2metres@users.noreply.github.com>
2023-06-27 11:55:20 +1000

    Change tracking event to correct event (#6283)
    **User-Facing Changes**
    None
    
    **Description**
    Noticed that we were using the wrong event type

--- packages/studio-base/src/components/AppBar/UserMenu.tsx
+++ packages/studio-base/src/components/AppBar/UserMenu.tsx
@@ -80,7 +80,7 @@ export function UserMenu({
   }, [beginSignOut, confirm]);
 
   const onSignInClick = useCallback(() => {
-    void analytics.logEvent(AppEvent.DIALOG_CLICK_CTA, {
+    void analytics.logEvent(AppEvent.APP_BAR_CLICK_CTA, {
       user: currentUserType,
       cta: "sign-in",
     });
@@ -115,7 +115,7 @@ export function UserMenu({
   }, [analytics, currentUserType]);
 
   const onSlackClick = useCallback(() => {
-    void analytics.logEvent(AppEvent.DIALOG_CLICK_CTA, {
+    void analytics.logEvent(AppEvent.APP_BAR_CLICK_CTA, {
       user: currentUserType,
       cta: "join-slack",
     });
