766e67211aa864a27d30be1e978e21883b01fde1
Roman Shtylman <roman@foxglove.dev>
2023-10-24 15:05:45 -0700

    Update strPack.ts with link to chrome bug report (#7018)
    Add a link to the chrome bug report to strPack to make it more obvious
    what this is for and when we might consider removing it.

--- packages/studio-base/src/util/strPack.ts
+++ packages/studio-base/src/util/strPack.ts
@@ -57,6 +57,8 @@ const packValue = (value: unknown, map: Mapping): unknown => {
  * Deduplicate all string references in the given data structure. This is
  * useful to do after `postMessage()`, since `structuredClone()` duplicates
  * strings.
+ *
+ * See: https://bugs.chromium.org/p/chromium/issues/detail?id=1487682&q=&can=4
  */
 export default function strPack<T>(data: T): T {
   const map: Mapping = {};
