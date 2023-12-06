415f53cf0029008f22d5bb0fa0286bc1d9d8a993
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-12-06 15:57:47 -0800

    Rename "settings" menu item to "visualization settings" (#7211)
    **User-Facing Changes**
    None
    
    **Description**
    Rename "Settings" to "Visualization settings"
    
    Relates to FG-6039

--- packages/studio-base/src/i18n/en/appBar.ts
+++ packages/studio-base/src/i18n/en/appBar.ts
@@ -23,7 +23,7 @@ export const appBar = {
   openLocalFile: "Open local file…",
   recentDataSources: "Recent data sources",
   recentlyViewed: "Recently viewed",
-  settings: "Settings",
+  settings: "Visualization settings",
   showLeftSidebar: "Show left sidebar",
   showRightSidebar: "Show right sidebar",
   signIn: "Sign in",
