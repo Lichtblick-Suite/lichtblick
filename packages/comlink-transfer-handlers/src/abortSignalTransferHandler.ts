26f1b2b0360b7286b61e10a1a68aa32a6444e317
Roman Shtylman <roman@foxglove.dev>
2024-01-22 11:04:47 -0800

    Use foxglove fork of comlink (#7355)
    Point at the foxglove fork of comlink. This will let us incorporate our
    changes to comlink without waiting for upstream to accept and release
    them.
    
    Currently our fork does not have any changes and matches upstream 4.4.1.

--- packages/comlink-transfer-handlers/src/abortSignalTransferHandler.ts
+++ packages/comlink-transfer-handlers/src/abortSignalTransferHandler.ts
@@ -2,7 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { TransferHandler } from "comlink";
+import type { TransferHandler } from "comlink";
 
 const isAbortSignal = (val: unknown): val is AbortSignal => val instanceof AbortSignal;
 
