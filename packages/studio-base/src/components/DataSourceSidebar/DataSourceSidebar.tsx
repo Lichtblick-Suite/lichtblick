5de27ab7620143223295ab4eb020bea516fa806f
Alexandre Neuwald CTW <Alexandre.Neuwald@ctw.bmwgroup.com>
2024-04-08 15:39:06 +0100

    fix lint problems

--- packages/studio-base/src/components/DataSourceSidebar/DataSourceSidebar.tsx
+++ packages/studio-base/src/components/DataSourceSidebar/DataSourceSidebar.tsx
@@ -2,6 +2,8 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+/* eslint-disable @foxglove/no-restricted-imports */
+
 import AddIcon from "@mui/icons-material/Add";
 import {
   CircularProgress,
