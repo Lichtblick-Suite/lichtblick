laisspportugal/studio

Allow the user to link settings to message converters depending on the panel type and the message type

packages/studio-base/src/PanelAPI/useConfigById.ts
@@ -2,6 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import * as _ from "lodash-es";
 import { useCallback } from "react";
 import { DeepPartial } from "ts-essentials";
 
@@ -10,6 +11,10 @@ import {
   useCurrentLayoutActions,
   useCurrentLayoutSelector,
 } from "@foxglove/studio-base/context/CurrentLayoutContext";
+import {
+  getExtensionPanelSettings,
+  useExtensionCatalog,
+} from "@foxglove/studio-base/context/ExtensionCatalogContext";
 import { SaveConfig } from "@foxglove/studio-base/types/panels";
 import { maybeCast } from "@foxglove/studio-base/util/maybeCast";
 
@@ -21,15 +26,24 @@ export default function useConfigById<Config extends Record<string, unknown>>(
   panelId: string | undefined,
 ): [Config | undefined, SaveConfig<Config>] {
   const { getCurrentLayoutState, savePanelConfigs } = useCurrentLayoutActions();
+  const extensionSettings = useExtensionCatalog(getExtensionPanelSettings);
 
   const configSelector = useCallback(
     (state: DeepPartial<LayoutState>) => {
       if (panelId == undefined) {
         return undefined;
       }
-      return maybeCast<Config>(state.selectedLayout?.data?.configById?.[panelId]);
+      const stateConfig = maybeCast<Config>(state.selectedLayout?.data?.configById?.[panelId]);
+      const topics = Object.keys(stateConfig?.topics ?? {});
+      const topicsSettings = _.merge(
+        {},
+        ...topics.map((topic) => ({ [topic]: extensionSettings[topic]?.defaultConfig })),
+        stateConfig?.topics,
+      );
+
+      return maybeCast<Config>({ ...stateConfig, topics: topicsSettings });
     },
-    [panelId],
+    [panelId, extensionSettings],
   );
 
   const config = useCurrentLayoutSelector(configSelector);

packages/studio-base/src/components/PanelExtensionAdapter/PanelExtensionAdapter.tsx
@@ -3,6 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { useTheme } from "@mui/material";
+import { produce } from "immer";
 import { CSSProperties, useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { useLatest } from "react-use";
 import { v4 as uuid } from "uuid";
@@ -17,6 +18,7 @@ import {
   ParameterValue,
   RenderState,
   SettingsTree,
+  SettingsTreeAction,
   Subscription,
   Time,
   VariableValue,
@@ -31,6 +33,7 @@ import PanelToolbar from "@foxglove/studio-base/components/PanelToolbar";
 import { useAppConfiguration } from "@foxglove/studio-base/context/AppConfigurationContext";
 import {
   ExtensionCatalog,
+  getExtensionPanelSettings,
   useExtensionCatalog,
 } from "@foxglove/studio-base/context/ExtensionCatalogContext";
 import {
@@ -51,9 +54,10 @@ import {
 } from "@foxglove/studio-base/providers/PanelStateContextProvider";
 import { PanelConfig, SaveConfig } from "@foxglove/studio-base/types/panels";
 import { assertNever } from "@foxglove/studio-base/util/assertNever";
+import { maybeCast } from "@foxglove/studio-base/util/maybeCast";
 
 import { PanelConfigVersionError } from "./PanelConfigVersionError";
-import { initRenderStateBuilder } from "./renderState";
+import { RenderStateConfig, initRenderStateBuilder } from "./renderState";
 import { BuiltinPanelExtensionContext } from "./types";
 import { useSharedPanelState } from "./useSharedPanelState";
 
@@ -112,7 +116,7 @@ function PanelExtensionAdapter(
   //
   // We store the config in a ref to avoid re-initializing the panel when the react config
   // changes.
-  const initialState = useLatest(config);
+  const initialState = useLatest(maybeCast<RenderStateConfig>(config));
 
   const messagePipelineContext = useMessagePipeline(selectContext);
 
@@ -121,7 +125,7 @@ function PanelExtensionAdapter(
 
   const { capabilities, profile: dataSourceProfile, presence: playerPresence } = playerState;
 
-  const { openSiblingPanel, setMessagePathDropConfig } = usePanelContext();
+  const { openSiblingPanel, setMessagePathDropConfig, type: panelName } = usePanelContext();
 
   const [panelId] = useState(() => uuid());
   const isMounted = useSynchronousMountedState();
@@ -237,6 +241,7 @@ function PanelExtensionAdapter(
       sortedTopics,
       subscriptions: localSubscriptions,
       watchedFields,
+      config: initialState.current,
     });
 
     if (!renderState) {
@@ -285,9 +290,12 @@ function PanelExtensionAdapter(
     sharedPanelState,
     sortedTopics,
     watchedFields,
+    initialState,
   ]);
 
   const updatePanelSettingsTree = usePanelSettingsTreeUpdate();
+
+  const extensionsSettings = useExtensionCatalog(getExtensionPanelSettings);
 
   type PartialPanelExtensionContext = Omit<BuiltinPanelExtensionContext, "panelElement">;
   const partialExtensionContext = useMemo<PartialPanelExtensionContext>(() => {
@@ -308,6 +316,20 @@ function PanelExtensionAdapter(
             assertNever(position, `Unsupported position for addPanel: ${position}`);
         }
       },
+    };
+
+    const extensionSettingsActionHandler = (action: SettingsTreeAction) => {
+      const {
+        payload: { path },
+      } = action;
+
+      saveConfig(
+        produce<{ topics: Record<string, unknown> }>((draft) => {
+          if (path[0] === "topics" && path[1] != undefined) {
+            extensionsSettings[panelName]?.[path[1]]?.handler(action, draft.topics[path[1]]);
+          }
+        }),
+      );
     };
 
     return {
@@ -499,7 +521,11 @@ function PanelExtensionAdapter(
         if (!isMounted()) {
           return;
         }
-        updatePanelSettingsTree(settings);
+        const actionHandler: typeof settings.actionHandler = (action) => {
+          settings.actionHandler(action);
+          extensionSettingsActionHandler(action);
+        };
+        updatePanelSettingsTree({ ...settings, actionHandler });
       },
 
       setDefaultPanelTitle: (title: string) => {
@@ -514,22 +540,24 @@ function PanelExtensionAdapter(
       },
     };
   }, [
-    capabilities,
-    clearHoverValue,
-    dataSourceProfile,
-    getMessagePipelineContext,
     initialState,
+    seekPlayback,
+    dataSourceProfile,
+    setSharedPanelState,
+    capabilities,
     isMounted,
     openSiblingPanel,
-    panelId,
     saveConfig,
-    seekPlayback,
-    setDefaultPanelTitle,
+    extensionsSettings,
+    panelName,
+    getMessagePipelineContext,
     setGlobalVariables,
+    clearHoverValue,
     setHoverValue,
-    setSharedPanelState,
     setSubscriptions,
+    panelId,
     updatePanelSettingsTree,
+    setDefaultPanelTitle,
     setMessagePathDropConfig,
   ]);
 

packages/studio-base/src/components/PanelSettings/index.tsx
@@ -3,6 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { Divider, Typography } from "@mui/material";
+import * as _ from "lodash-es";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useUnmount } from "react-use";
@@ -22,6 +23,10 @@ import {
   useCurrentLayoutSelector,
   useSelectedPanels,
 } from "@foxglove/studio-base/context/CurrentLayoutContext";
+import {
+  getExtensionPanelSettings,
+  useExtensionCatalog,
+} from "@foxglove/studio-base/context/ExtensionCatalogContext";
 import { usePanelCatalog } from "@foxglove/studio-base/context/PanelCatalogContext";
 import {
   PanelStateStore,
@@ -31,6 +36,7 @@ import { useAppConfigurationValue } from "@foxglove/studio-base/hooks";
 import { PanelConfig } from "@foxglove/studio-base/types/panels";
 import { TAB_PANEL_TYPE } from "@foxglove/studio-base/util/globalConstants";
 import { getPanelTypeFromId } from "@foxglove/studio-base/util/layout";
+import { maybeCast } from "@foxglove/studio-base/util/maybeCast";
 
 const singlePanelIdSelector = (state: LayoutState) =>
   typeof state.selectedLayout?.data?.layout === "string"
@@ -143,9 +149,26 @@ export default function PanelSettings({
 
   const [config] = useConfigById(selectedPanelId);
 
-  const settingsTree = usePanelStateStore((state) =>
-    selectedPanelId ? state.settingsTrees[selectedPanelId] : undefined,
-  );
+  const extensionSettings = useExtensionCatalog(getExtensionPanelSettings);
+
+  const settingsTree = usePanelStateStore((state) => {
+    if (selectedPanelId) {
+      const set = state.settingsTrees[selectedPanelId];
+      if (set && panelType) {
+        const topics = Object.keys(set.nodes.topics?.children ?? {});
+        const topicsConfig = maybeCast<{ topics: Record<string, unknown> }>(config)?.topics;
+        const topicsSettings = _.merge(
+          {},
+          ...topics.map((topic) => ({
+            [topic]: extensionSettings[panelType]?.[topic]?.settings(topicsConfig?.[topic]),
+          })),
+        );
+
+        return { ...set, nodes: _.merge({}, set.nodes, { topics: { children: topicsSettings } }) };
+      }
+    }
+    return undefined;
+  });
 
   const resetToDefaults = useCallback(() => {
     if (selectedPanelId) {

Lichtblick-Suite/lichtblick

packages/suite-base/src/PanelAPI/useConfigById.ts
@@ -5,6 +5,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import * as _ from "lodash-es";
 import { useCallback } from "react";
 import { DeepPartial } from "ts-essentials";
 
@@ -13,6 +14,10 @@ import {
   useCurrentLayoutActions,
   useCurrentLayoutSelector,
 } from "@lichtblick/suite-base/context/CurrentLayoutContext";
+import {
+  getExtensionPanelSettings,
+  useExtensionCatalog,
+} from "@lichtblick/suite-base/context/ExtensionCatalogContext";
 import { SaveConfig } from "@lichtblick/suite-base/types/panels";
 import { maybeCast } from "@lichtblick/suite-base/util/maybeCast";
 
@@ -24,15 +29,24 @@ export default function useConfigById<Config extends Record<string, unknown>>(
   panelId: string | undefined,
 ): [Config | undefined, SaveConfig<Config>] {
   const { getCurrentLayoutState, savePanelConfigs } = useCurrentLayoutActions();
+  const extensionSettings = useExtensionCatalog(getExtensionPanelSettings);
 
   const configSelector = useCallback(
     (state: DeepPartial<LayoutState>) => {
       if (panelId == undefined) {
         return undefined;
       }
-      return maybeCast<Config>(state.selectedLayout?.data?.configById?.[panelId]);
+      const stateConfig = maybeCast<Config>(state.selectedLayout?.data?.configById?.[panelId]);
+      const topics = Object.keys(stateConfig?.topics ?? {});
+      const topicsSettings = _.merge(
+        {},
+        ...topics.map((topic) => ({ [topic]: extensionSettings[topic]?.defaultConfig })),
+        stateConfig?.topics,
+      );
+
+      return maybeCast<Config>({ ...stateConfig, topics: topicsSettings });
     },
-    [panelId],
+    [panelId, extensionSettings],
   );
 
   const config = useCurrentLayoutSelector(configSelector);

packages/suite-base/src/components/PanelExtensionAdapter/PanelExtensionAdapter.tsx
@@ -7,6 +7,7 @@
 
 import { fromSec, toSec } from "@foxglove/rostime";
 import { useTheme } from "@mui/material";
+import { produce } from "immer";
 import { CSSProperties, useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { useLatest } from "react-use";
 import { v4 as uuid } from "uuid";
@@ -20,6 +21,7 @@ import {
   ParameterValue,
   RenderState,
   SettingsTree,
+  SettingsTreeAction,
   Subscription,
   Time,
   VariableValue,
@@ -34,6 +36,7 @@ import PanelToolbar from "@lichtblick/suite-base/components/PanelToolbar";
 import { useAppConfiguration } from "@lichtblick/suite-base/context/AppConfigurationContext";
 import {
   ExtensionCatalog,
+  getExtensionPanelSettings,
   useExtensionCatalog,
 } from "@lichtblick/suite-base/context/ExtensionCatalogContext";
 import {
@@ -56,9 +59,10 @@ import { PanelConfig, SaveConfig } from "@lichtblick/suite-base/types/panels";
 import { assertNever } from "@lichtblick/suite-base/util/assertNever";
 
 import { PanelConfigVersionError } from "./PanelConfigVersionError";
-import { initRenderStateBuilder } from "./renderState";
+import { RenderStateConfig, initRenderStateBuilder } from "./renderState";
 import { BuiltinPanelExtensionContext } from "./types";
 import { useSharedPanelState } from "./useSharedPanelState";
+import { maybeCast } from "@lichtblick/suite-base/util/maybeCast";
 
 const log = Logger.getLogger(__filename);
 
@@ -115,7 +119,7 @@ function PanelExtensionAdapter(
   //
   // We store the config in a ref to avoid re-initializing the panel when the react config
   // changes.
-  const initialState = useLatest(config);
+  const initialState = useLatest(maybeCast<RenderStateConfig>(config));
 
   const messagePipelineContext = useMessagePipeline(selectContext);
 
@@ -124,7 +128,7 @@ function PanelExtensionAdapter(
 
   const { capabilities, profile: dataSourceProfile, presence: playerPresence } = playerState;
 
-  const { openSiblingPanel, setMessagePathDropConfig } = usePanelContext();
+  const { openSiblingPanel, setMessagePathDropConfig, type: panelName } = usePanelContext();
 
   const [panelId] = useState(() => uuid());
   const isMounted = useSynchronousMountedState();
@@ -240,6 +244,7 @@ function PanelExtensionAdapter(
       sortedTopics,
       subscriptions: localSubscriptions,
       watchedFields,
+      config: initialState.current,
     });
 
     if (!renderState) {
@@ -288,9 +293,12 @@ function PanelExtensionAdapter(
     sharedPanelState,
     sortedTopics,
     watchedFields,
+    initialState,
   ]);
 
   const updatePanelSettingsTree = usePanelSettingsTreeUpdate();
+
+  const extensionsSettings = useExtensionCatalog(getExtensionPanelSettings);
 
   type PartialPanelExtensionContext = Omit<BuiltinPanelExtensionContext, "panelElement">;
 
@@ -312,6 +320,20 @@ function PanelExtensionAdapter(
             assertNever(position, `Unsupported position for addPanel: ${position}`);
         }
       },
+    };
+
+    const extensionSettingsActionHandler = (action: SettingsTreeAction) => {
+      const {
+        payload: { path },
+      } = action;
+
+      saveConfig(
+        produce<{ topics: Record<string, unknown> }>((draft) => {
+          if (path[0] === "topics" && path[1] != undefined) {
+            extensionsSettings[panelName]?.[path[1]]?.handler(action, draft.topics[path[1]]);
+          }
+        }),
+      );
     };
 
     return {
@@ -505,7 +527,11 @@ function PanelExtensionAdapter(
         if (!isMounted()) {
           return;
         }
-        updatePanelSettingsTree(settings);
+        const actionHandler: typeof settings.actionHandler = (action) => {
+          settings.actionHandler(action);
+          extensionSettingsActionHandler(action);
+        };
+        updatePanelSettingsTree({ ...settings, actionHandler });
       },
 
       setDefaultPanelTitle: (title: string) => {
@@ -522,22 +548,24 @@ function PanelExtensionAdapter(
     // Disable this rule because the metadata function. If used, it will break.
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
-    capabilities,
-    clearHoverValue,
-    dataSourceProfile,
-    getMessagePipelineContext,
     initialState,
+    seekPlayback,
+    dataSourceProfile,
+    setSharedPanelState,
+    capabilities,
     isMounted,
     openSiblingPanel,
-    panelId,
     saveConfig,
-    seekPlayback,
-    setDefaultPanelTitle,
+    extensionsSettings,
+    panelName,
+    getMessagePipelineContext,
     setGlobalVariables,
+    clearHoverValue,
     setHoverValue,
-    setSharedPanelState,
     setSubscriptions,
+    panelId,
     updatePanelSettingsTree,
+    setDefaultPanelTitle,
     setMessagePathDropConfig,
   ]);
 

packages/suite-base/src/components/PanelSettings/index.tsx
@@ -6,6 +6,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { Divider, Typography } from "@mui/material";
+import * as _ from "lodash-es";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useUnmount } from "react-use";
@@ -25,6 +26,10 @@ import {
   useCurrentLayoutSelector,
   useSelectedPanels,
 } from "@lichtblick/suite-base/context/CurrentLayoutContext";
+import {
+  getExtensionPanelSettings,
+  useExtensionCatalog,
+} from "@lichtblick/suite-base/context/ExtensionCatalogContext";
 import { usePanelCatalog } from "@lichtblick/suite-base/context/PanelCatalogContext";
 import {
   PanelStateStore,
@@ -34,6 +39,7 @@ import { useAppConfigurationValue } from "@lichtblick/suite-base/hooks";
 import { PanelConfig } from "@lichtblick/suite-base/types/panels";
 import { TAB_PANEL_TYPE } from "@lichtblick/suite-base/util/globalConstants";
 import { getPanelTypeFromId } from "@lichtblick/suite-base/util/layout";
+import { maybeCast } from "@lichtblick/suite-base/util/maybeCast";
 
 const singlePanelIdSelector = (state: LayoutState) =>
   typeof state.selectedLayout?.data?.layout === "string"
@@ -146,9 +152,26 @@ export default function PanelSettings({
 
   const [config] = useConfigById(selectedPanelId);
 
-  const settingsTree = usePanelStateStore((state) =>
-    selectedPanelId ? state.settingsTrees[selectedPanelId] : undefined,
-  );
+  const extensionSettings = useExtensionCatalog(getExtensionPanelSettings);
+
+  const settingsTree = usePanelStateStore((state) => {
+    if (selectedPanelId) {
+      const set = state.settingsTrees[selectedPanelId];
+      if (set && panelType) {
+        const topics = Object.keys(set.nodes.topics?.children ?? {});
+        const topicsConfig = maybeCast<{ topics: Record<string, unknown> }>(config)?.topics;
+        const topicsSettings = _.merge(
+          {},
+          ...topics.map((topic) => ({
+            [topic]: extensionSettings[panelType]?.[topic]?.settings(topicsConfig?.[topic]),
+          })),
+        );
+
+        return { ...set, nodes: _.merge({}, set.nodes, { topics: { children: topicsSettings } }) };
+      }
+    }
+    return undefined;
+  });
 
   const resetToDefaults = useCallback(() => {
     if (selectedPanelId) {