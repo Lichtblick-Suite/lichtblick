8d7d9268a8c0ede747912cd14fa5d939f957aacd
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2024-09-10 18:36:21 +0100

    removed deplicated imports
    Signed-off-by: Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>

--- packages/suite-base/src/panels/createSyncRoot.test.tsx
+++ packages/suite-base/src/panels/createSyncRoot.test.tsx
@@ -5,8 +5,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
-import { screen } from "@testing-library/react";
-import { act } from "@testing-library/react";
+import { act, screen } from "@testing-library/react";
 
 import { createSyncRoot } from "@lichtblick/suite-base/panels/createSyncRoot";
 
