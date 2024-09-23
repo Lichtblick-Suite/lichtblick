0b121900691c430bd54e62e252b29e68f26231e6
Alexandre Neuwald CTW <Alexandre.Neuwald@ctw.bmwgroup.com>
2024-09-23 11:19:36 +0100

    fix lint

--- packages/suite-base/src/players/VelodynePlayer.ts
+++ packages/suite-base/src/players/VelodynePlayer.ts
@@ -5,7 +5,6 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { Sockets, UdpRemoteInfo, UdpSocketRenderer } from "@foxglove/electron-socket/renderer";
 import { Model, RawPacket, ReturnMode, packetRate } from "@foxglove/velodyne-cloud";
 import { v4 as uuidv4 } from "uuid";
 
