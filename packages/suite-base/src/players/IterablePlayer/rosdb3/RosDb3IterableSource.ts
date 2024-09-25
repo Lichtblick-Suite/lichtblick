760b09b19e5943368d74f8a4835172b4230a83fb
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2024-09-25 16:46:12 +0100

    fixed conflict
    Signed-off-by: Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>

--- packages/suite-base/src/players/IterablePlayer/rosdb3/RosDb3IterableSource.ts
+++ packages/suite-base/src/players/IterablePlayer/rosdb3/RosDb3IterableSource.ts
@@ -5,9 +5,8 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { stringify } from "@foxglove/rosmsg";
-
 import { ROS2_TO_DEFINITIONS, Rosbag2, SqliteSqljs } from "@lichtblick/rosbag2-web";
+import { stringify } from "@lichtblick/rosmsg";
 import { Time, add as addTime } from "@lichtblick/rostime";
 import { MessageEvent } from "@lichtblick/suite";
 import { estimateObjectSize } from "@lichtblick/suite-base/players/messageMemoryEstimation";
