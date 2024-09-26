220b6b7f138c42c4cdc8f2f962bad9b1153273b4
Luiz Bezerra <luluiz@users.noreply.github.com>
2024-09-26 09:53:09 +0100

    Update rosbag2-web dependency (#195)
    **User-Facing Changes**
    N/A
    
    **Description**
    Updated to @lichtblick/rosbag2-web
    
    <!-- link relevant GitHub issues -->
    <!-- add `docs` label if this PR requires documentation updates -->
    <!-- add relevant metric tracking for experimental / new features -->
    
    **Checklist**
    
    - [x] The web version was tested and it is running ok
    - [x] The desktop version was tested and it is running ok
    
    ---------
    
    Signed-off-by: Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>

--- packages/suite-base/src/players/IterablePlayer/rosdb3/RosDb3IterableSource.ts
+++ packages/suite-base/src/players/IterablePlayer/rosdb3/RosDb3IterableSource.ts
@@ -5,8 +5,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { ROS2_TO_DEFINITIONS, Rosbag2, SqliteSqljs } from "@foxglove/rosbag2-web";
-
+import { ROS2_TO_DEFINITIONS, Rosbag2, SqliteSqljs } from "@lichtblick/rosbag2-web";
 import { stringify } from "@lichtblick/rosmsg";
 import { Time, add as addTime } from "@lichtblick/rostime";
 import { MessageEvent } from "@lichtblick/suite";
