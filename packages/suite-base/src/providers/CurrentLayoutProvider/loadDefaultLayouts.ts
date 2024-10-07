939257ca373f2c3d73bb69504b75418bcba7de9e
Alexandre Neuwald CTW <Alexandre.Neuwald@ctw.bmwgroup.com>
2024-10-07 14:19:35 +0100

    Apply destructuring suggestions from code review
    Review from Luiz
    
    Co-authored-by: Luiz Bezerra <luluiz@users.noreply.github.com>
    Signed-off-by: Alexandre Neuwald CTW <Alexandre.Neuwald@ctw.bmwgroup.com>

--- packages/suite-base/src/providers/CurrentLayoutProvider/loadDefaultLayouts.ts
+++ packages/suite-base/src/providers/CurrentLayoutProvider/loadDefaultLayouts.ts
@@ -24,7 +24,7 @@ export const loadDefaultLayouts = async (
   try {
     const currentLayouts = await layoutManager.getLayouts();
     const currentLayoutsFroms = new Set(currentLayouts.map((layout) => layout.from));
-
+    const currentLayoutsFroms = new Set(currentLayouts.map(({ from }) => from));
     const loaderPromises = loaders.map(async (loader) => await loader.fetchLayouts());
     const loaderResults = await Promise.allSettled(loaderPromises);
 
@@ -32,11 +32,13 @@ export const loadDefaultLayouts = async (
       .filter(isFulfilled)
       .flatMap((result) => result.value)
       .filter((layout) => !currentLayoutsFroms.has(layout.from));
-
+      .flatMap(({ value }) => value)
+      .filter(({ from }) => !currentLayoutsFroms.has(from));
     // Log errors cause failed to fetch some layout from a specific loader
     loaderResults.filter(isRejected).forEach((result) => {
       log.error(`Failed to fetch layouts from loader: ${result.reason}`);
-    });
+    loaderResults.filter(isRejected).forEach(({ reason }) => {
+      log.error(`Failed to fetch layouts from loader: ${reason}`);
 
     const savedPromises = newLayouts.map(
       async (layout) =>
@@ -48,11 +50,12 @@ export const loadDefaultLayouts = async (
 
     // Try to save all layouts
     const saveResults = await Promise.allSettled(savedPromises);
-
+    const savedResults = await Promise.allSettled(savedPromises);
     // Log errors cause failed to save a layout
     saveResults.filter(isRejected).forEach((result) => {
       log.error(`Failed to save layout: ${result.reason}`);
-    });
+    saveResults.filter(isRejected).forEach(({ reason }) => {
+      log.error(`Failed to save layout: ${reason}`);
   } catch (err) {
     log.error(`Loading default layouts failed: ${err}`);
   }
