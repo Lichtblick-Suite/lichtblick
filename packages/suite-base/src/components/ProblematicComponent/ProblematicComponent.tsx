63885b95e8b0b4f51e7d486846fdf474bf6bd60d
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2024-10-10 10:44:31 +0100

    lint
    Signed-off-by: Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>

--- packages/suite-base/src/components/ProblematicComponent/ProblematicComponent.tsx
+++ packages/suite-base/src/components/ProblematicComponent/ProblematicComponent.tsx
@@ -5,36 +5,16 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 type Props = {
   userName: string;
 };
 
 const ProblematicComponent: React.FC<Props> = ({ userName }) => {
-  const [count, setCount] = useState<number>(0);
-  // const [data, setData] = useState<string | undefined>("");
-  // const [unnusedData,] = useState<any>(null); // unnused var
-
-  // Complexity issue - bad logic
-  const handleClick = () => {
-    setCount(0);
-  };
-
-  useEffect(() => {
-    fetch("/api/data")
-      .then(async (response) => await response.json())
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }, []);
-
   return (
     <div>
-      <h1>Hello, {userName}</h1>
-      <p>Counter: {count}</p>
-      <button onClick={handleClick}>Increment</button>
-      {/* {data ? <div>{data.name}</div> : <p>Loading...</p>} */}
+      {userName}
     </div>
   );
 };
