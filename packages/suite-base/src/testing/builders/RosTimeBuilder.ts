8e2938a02d6fe5fb8ec8785789f930524011e13a
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2024-10-28 08:38:19 +0000

    RosTime builder
    Signed-off-by: Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>

--- packages/suite-base/src/testing/builders/RosTimeBuilder.ts
+++ packages/suite-base/src/testing/builders/RosTimeBuilder.ts
@@ -13,9 +13,16 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
-// import { Time } from "@lichtblick/rostime";
-// import BasicBuilder from "@lichtblick/suite-base/testing/builders/BasicBuilder";
-// import { defaults } from "@lichtblick/suite-base/testing/builders/utilities";
+import { Time } from "@lichtblick/rostime";
+import BasicBuilder from "@lichtblick/suite-base/testing/builders/BasicBuilder";
+import { defaults } from "@lichtblick/suite-base/testing/builders/utilities";
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export default class RosTimeBuilder {}
+export default class RosTimeBuilder {
+  public static time(props: Partial<Time> = {}): Time {
+    return defaults<Time>(props, {
+      sec: BasicBuilder.number(),
+      nsec: BasicBuilder.number(),
+    });
+  }
+}
