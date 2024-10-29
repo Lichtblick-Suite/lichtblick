ced7de0746fe8830376dcba7ec10fb7e2d1a7562
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2024-10-29 11:39:39 +0000

    removede unncessary license
    Signed-off-by: Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>

--- packages/suite-base/src/components/PanelSettings/settingsTree.ts
+++ packages/suite-base/src/components/PanelSettings/settingsTree.ts
@@ -1,10 +1,6 @@
 // SPDX-FileCopyrightText: Copyright (C) 2023-2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
 // SPDX-License-Identifier: MPL-2.0
 
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/
-
 import { Immutable, PanelSettings, SettingsTree, SettingsTreeNode } from "@lichtblick/suite";
 import { PanelStateStore } from "@lichtblick/suite-base/context/PanelStateContext";
 import { maybeCast } from "@lichtblick/suite-base/util/maybeCast";
