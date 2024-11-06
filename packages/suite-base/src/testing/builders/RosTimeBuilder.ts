8edac9c4130f0c13c8842849410a7082b700d215
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2024-11-06 18:15:12 +0000

    added eslint rule

--- packages/suite-base/src/testing/builders/RosTimeBuilder.ts
+++ packages/suite-base/src/testing/builders/RosTimeBuilder.ts
@@ -5,7 +5,6 @@ import { Time } from "@lichtblick/rostime";
 import BasicBuilder from "@lichtblick/suite-base/testing/builders/BasicBuilder";
 import { defaults } from "@lichtblick/suite-base/testing/builders/utilities";
 
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export default class RosTimeBuilder {
   public static time(props: Partial<Time> = {}): Time {
     return defaults<Time>(props, {
