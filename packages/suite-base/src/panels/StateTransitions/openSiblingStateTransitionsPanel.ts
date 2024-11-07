70457280d5fa10503a98bc367e1eadaeb0dd2629
Lais Portugal <Lais.Silva.Portugal@criticaltechworks.com>
2024-11-07 15:40:19 +0000

    Fix lint

--- packages/suite-base/src/panels/StateTransitions/openSiblingStateTransitionsPanel.ts
+++ packages/suite-base/src/panels/StateTransitions/openSiblingStateTransitionsPanel.ts
@@ -5,8 +5,6 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import * as _ from "lodash-es";
-
 import { StateTransitionConfig } from "@lichtblick/suite-base/panels/StateTransitions/types";
 import { OpenSiblingPanel, PanelConfig } from "@lichtblick/suite-base/types/panels";
 
