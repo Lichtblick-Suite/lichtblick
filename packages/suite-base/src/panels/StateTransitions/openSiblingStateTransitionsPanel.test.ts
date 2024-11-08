83b2841df56f09b81c83babd24f7e775e3d75cd3
Lais Portugal <Lais.Silva.Portugal@criticaltechworks.com>
2024-11-08 16:43:33 +0000

    lint

--- packages/suite-base/src/panels/StateTransitions/openSiblingStateTransitionsPanel.test.ts
+++ packages/suite-base/src/panels/StateTransitions/openSiblingStateTransitionsPanel.test.ts
@@ -8,9 +8,9 @@ import { OpenSiblingPanel } from "@lichtblick/suite-base/types/panels";
 import { openSiblingStateTransitionsPanel } from "./openSiblingStateTransitionsPanel";
 
 type IOpenSiblingStateTransisiontsPanelSetup = {
-  topicName: string,
-  config:Partial<StateTransitionConfig>
-}
+  topicName: string;
+  config: Partial<StateTransitionConfig>;
+};
 describe("openSiblingStateTransitionsPanel", () => {
   let mockOpenSiblingPanel: OpenSiblingPanel;
   let topicName: string;
@@ -21,21 +21,24 @@ describe("openSiblingStateTransitionsPanel", () => {
     topicName = BasicBuilder.string();
   });
 
-  function setup({topicName = undefined, config = {}}: Partial<IOpenSiblingStateTransisiontsPanelSetup> = {}) {
+  function setup({
+    topicName = undefined,
+    config = {},
+  }: Partial<IOpenSiblingStateTransisiontsPanelSetup> = {}) {
     const topicNameDefault: string = topicName ?? BasicBuilder.string();
     const configDefault: StateTransitionConfig = {
       paths: [],
       isSynced: false,
-      ...config
+      ...config,
     };
-    return {topicName: topicNameDefault, config: configDefault};
+    return { topicName: topicNameDefault, config: configDefault };
   }
 
   it("should call openSiblingPanel with correct parameters", () => {
-    const { config, topicName} = setup();
+    const { config, topicName } = setup();
     openSiblingStateTransitionsPanel(mockOpenSiblingPanel, topicName);
     const siblingConfigCreator = (mockOpenSiblingPanel as jest.Mock).mock.calls[0][0]
-    .siblingConfigCreator;
+      .siblingConfigCreator;
     const newConfig = siblingConfigCreator(config);
 
     expect(mockOpenSiblingPanel).toHaveBeenCalledWith({
@@ -47,13 +50,12 @@ describe("openSiblingStateTransitionsPanel", () => {
   });
 
   it("should not duplicate paths in the config", () => {
-    const topicName = BasicBuilder.string();
     const { config } = setup({
       topicName,
       config: {
-        paths: [{ value: topicName , timestampMethod: "receiveTime" }],
+        paths: [{ value: topicName, timestampMethod: "receiveTime" }],
         isSynced: false,
-      }
+      },
     });
     openSiblingStateTransitionsPanel(mockOpenSiblingPanel, topicName);
     const siblingConfigCreator = (mockOpenSiblingPanel as jest.Mock).mock.calls[0][0]
