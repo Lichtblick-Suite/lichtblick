eb20d144c436872da70dc4991bd2934918b8021d
ctw-joao-luis <joao.miguel.luis@criticaltechworks.com>
2024-11-13 09:34:00 +0000

    constants move to constats.ts

--- packages/suite-base/src/players/messageMemoryEstimation.test.ts
+++ packages/suite-base/src/players/messageMemoryEstimation.test.ts
@@ -5,10 +5,10 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { OBJECT_BASE_SIZE } from "./constants";
 import {
   estimateMessageObjectSize,
   estimateMessageFieldSizes,
-  OBJECT_BASE_SIZE,
   estimateObjectSize,
 } from "./messageMemoryEstimation";
 
