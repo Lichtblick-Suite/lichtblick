d69eb7fc22c0fc3c3a2abc946ad3bdb82fb0b2cc
ctw-joao-luis <joao.miguel.luis@criticaltechworks.com>
2024-11-22 11:01:54 +0000

    removed linkProps.title property from linkRenderer

--- packages/suite-base/src/components/TextContent.tsx
+++ packages/suite-base/src/components/TextContent.tsx
@@ -181,7 +181,6 @@ export default function TextContent(
             handleLink(event, linkProps.href ?? "");
           }}
           target="_blank"
-          title={linkProps.title}
         >
           {linkProps.children}
         </Link>
