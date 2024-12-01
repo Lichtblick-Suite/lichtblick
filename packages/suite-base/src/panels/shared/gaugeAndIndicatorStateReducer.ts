8969a19ff9944cd316a200719193fe049d04bc19
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2024-12-01 23:40:57 +0000

    sonar issue: reduced cognitive complexity

--- packages/suite-base/src/panels/shared/gaugeAndIndicatorStateReducer.ts
+++ packages/suite-base/src/panels/shared/gaugeAndIndicatorStateReducer.ts
@@ -3,6 +3,7 @@
 import * as _ from "lodash-es";
 
 import { parseMessagePath } from "@lichtblick/message-path";
+import { MessageEvent } from "@lichtblick/suite";
 import { simpleGetMessagePathDataItems } from "@lichtblick/suite-base/components/MessagePathSyntax/simpleGetMessagePathDataItems";
 import {
   GaugeAndIndicatorState,
@@ -16,6 +17,75 @@ export function getSingleDataItem(results: unknown[]): unknown {
   throw new Error("Message path produced multiple results");
 }
 
+function handleFrameActionState({
+  messages,
+  state,
+}: {
+  messages: readonly MessageEvent[];
+  state: GaugeAndIndicatorState;
+}): GaugeAndIndicatorState {
+  if (state.pathParseError != undefined) {
+    return { ...state, latestMessage: _.last(messages), error: undefined };
+  }
+  if (!state.parsedPath) {
+    return { ...state, error: undefined };
+  }
+
+  let latestMatchingQueriedData = state.latestMatchingQueriedData;
+  let latestMessage = state.latestMessage;
+  for (const message of messages) {
+    if (message.topic !== state.parsedPath.topicName) {
+      continue;
+    }
+
+    const data = getSingleDataItem(simpleGetMessagePathDataItems(message, state.parsedPath));
+    if (data != undefined) {
+      latestMatchingQueriedData = data;
+      latestMessage = message;
+    }
+  }
+
+  return { ...state, latestMessage, latestMatchingQueriedData, error: undefined };
+}
+
+function handlePathActionState({
+  path,
+  state,
+}: {
+  path: string;
+  state: GaugeAndIndicatorState;
+}): GaugeAndIndicatorState {
+  const newPath = parseMessagePath(path);
+  let pathParseError: string | undefined;
+  if (
+    newPath?.messagePath.some(
+      (part) =>
+        (part.type === "filter" && typeof part.value === "object") ||
+        (part.type === "slice" && (typeof part.start === "object" || typeof part.end === "object")),
+    ) === true
+  ) {
+    pathParseError = "Message paths using variables are not currently supported";
+  }
+  let latestMatchingQueriedData: unknown;
+  let error: Error | undefined;
+  try {
+    latestMatchingQueriedData =
+      newPath && pathParseError == undefined && state.latestMessage
+        ? getSingleDataItem(simpleGetMessagePathDataItems(state.latestMessage, newPath))
+        : undefined;
+  } catch (err: unknown) {
+    error = err as Error;
+  }
+  return {
+    ...state,
+    error,
+    latestMatchingQueriedData,
+    parsedPath: newPath,
+    path,
+    pathParseError,
+  };
+}
+
 export function stateReducer(
   state: GaugeAndIndicatorState,
   action: GaugeAndIndicatorAction,
@@ -23,60 +93,10 @@ export function stateReducer(
   try {
     switch (action.type) {
       case "frame": {
-        if (state.pathParseError != undefined) {
-          return { ...state, latestMessage: _.last(action.messages), error: undefined };
-        }
-        if (!state.parsedPath) {
-          return { ...state, error: undefined };
-        }
-
-        let latestMatchingQueriedData = state.latestMatchingQueriedData;
-        let latestMessage = state.latestMessage;
-        for (const message of action.messages) {
-          if (message.topic !== state.parsedPath.topicName) {
-            continue;
-          }
-
-          const data = getSingleDataItem(simpleGetMessagePathDataItems(message, state.parsedPath));
-          if (data != undefined) {
-            latestMatchingQueriedData = data;
-            latestMessage = message;
-          }
-        }
-
-        return { ...state, latestMessage, latestMatchingQueriedData, error: undefined };
+        return handleFrameActionState({ state, messages: action.messages });
       }
       case "path": {
-        const newPath = parseMessagePath(action.path);
-        let pathParseError: string | undefined;
-        if (
-          newPath?.messagePath.some(
-            (part) =>
-              (part.type === "filter" && typeof part.value === "object") ||
-              (part.type === "slice" &&
-                (typeof part.start === "object" || typeof part.end === "object")),
-          ) === true
-        ) {
-          pathParseError = "Message paths using variables are not currently supported";
-        }
-        let latestMatchingQueriedData: unknown;
-        let error: Error | undefined;
-        try {
-          latestMatchingQueriedData =
-            newPath && pathParseError == undefined && state.latestMessage
-              ? getSingleDataItem(simpleGetMessagePathDataItems(state.latestMessage, newPath))
-              : undefined;
-        } catch (err: unknown) {
-          error = err as Error;
-        }
-        return {
-          ...state,
-          error,
-          latestMatchingQueriedData,
-          parsedPath: newPath,
-          path: action.path,
-          pathParseError,
-        };
+        return handlePathActionState({ state, path: action.path });
       }
       case "seek":
         return {
