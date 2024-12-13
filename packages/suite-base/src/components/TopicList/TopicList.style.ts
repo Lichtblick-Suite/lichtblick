9d2ae92959909296b20b103f30bacdaad5553834
ctw-joao-luis <joao.miguel.luis@criticaltechworks.com>
2024-12-13 16:57:59 +0000

    separting styles to its own files

---
+++ packages/suite-base/src/components/TopicList/TopicList.style.ts
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: Copyright (C) 2023-2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
+// SPDX-License-Identifier: MPL-2.0
+
+import { makeStyles } from "tss-react/mui";
+
+export const useStyles = makeStyles()((theme) => ({
+  root: {
+    width: "100%",
+    height: "100%",
+    overflow: "hidden",
+    display: "flex",
+    flexDirection: "column",
+    containerType: "inline-size",
+  },
+  filterBar: {
+    top: 0,
+    zIndex: theme.zIndex.appBar,
+    padding: theme.spacing(0.5),
+    position: "sticky",
+    backgroundColor: theme.palette.background.paper,
+  },
+  skeletonText: {
+    marginTop: theme.spacing(0.5),
+    marginBottom: theme.spacing(0.5),
+  },
+}));
