0a1d1890b860e666afe027a3c6dcee849fa6a540
portugal <lais.sp.portugal@ctw.bmwgroup.com>
2024-12-18 09:31:59 +0000

    Resize icons in the application (#311)
    **User-Facing Changes**
    
    The taskbar and application icons were resized to a smaller size:
    - Taskbar icon: 
    
    ![image](https://github.com/user-attachments/assets/5bb4b05a-d3a4-458b-b641-d737736396a7)
    - Application icon:
    
    ![image](https://github.com/user-attachments/assets/dfc02ca6-cf17-4991-ad67-8ce0e39d89ac)
    
    **Description**
    This PR aims to resize the icons in the application to make them
    smaller.

--- packages/suite-base/src/components/LichtblickLogo.tsx
+++ packages/suite-base/src/components/LichtblickLogo.tsx
@@ -16,7 +16,7 @@ export function LichtblickLogo(props: SvgIconProps): React.JSX.Element {
         data-name="Layer 2"
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 140.88 140.86"
+        viewBox="-20 -20 180 180"
       >
         <defs>
           <clipPath id="clippath">
