f4c7a04cf252ba24de7ce9bba69404bb9a0d68f4
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2025-01-17 21:15:22 +0000

    improved useSubscriptions

--- packages/suite-base/src/panels/Plot/hooks/useSubscriptions.test.ts
+++ packages/suite-base/src/panels/Plot/hooks/useSubscriptions.test.ts
@@ -6,15 +6,39 @@
 import { renderHook } from "@testing-library/react";
 
 import { parseMessagePath } from "@lichtblick/message-path";
+import { fillInGlobalVariablesInPath } from "@lichtblick/suite-base/components/MessagePathSyntax/useCachedGetMessagePathDataItems";
 import { useMessagePipeline } from "@lichtblick/suite-base/components/MessagePipeline";
 import useGlobalVariables from "@lichtblick/suite-base/hooks/useGlobalVariables";
-import { PlotConfig } from "@lichtblick/suite-base/panels/Plot/config";
+import { PlotConfig, PlotXAxisVal } from "@lichtblick/suite-base/panels/Plot/config";
 import BasicBuilder from "@lichtblick/suite-base/testing/builders/BasicBuilder";
+import GlobalVariableBuilder from "@lichtblick/suite-base/testing/builders/GlobalVariableBuilder";
+import PlotBuilder from "@lichtblick/suite-base/testing/builders/PlotBuilder";
 
 import useSubscriptions from "./useSubscriptions";
+import { isReferenceLinePlotPathType } from "../config";
+import { pathToSubscribePayload } from "../subscription";
 
 jest.mock("@lichtblick/suite-base/components/MessagePipeline", () => ({
   useMessagePipeline: jest.fn(),
+}));
+
+jest.mock("@lichtblick/message-path", () => ({
+  parseMessagePath: jest.fn(),
+}));
+
+jest.mock(
+  "@lichtblick/suite-base/components/MessagePathSyntax/useCachedGetMessagePathDataItems",
+  () => ({
+    fillInGlobalVariablesInPath: jest.fn(),
+  }),
+);
+
+jest.mock("../config", () => ({
+  isReferenceLinePlotPathType: jest.fn(),
+}));
+
+jest.mock("../subscription", () => ({
+  pathToSubscribePayload: jest.fn(),
 }));
 
 jest.mock("@lichtblick/suite-base/hooks/useGlobalVariables", () => ({
@@ -22,181 +46,197 @@ jest.mock("@lichtblick/suite-base/hooks/useGlobalVariables", () => ({
   default: jest.fn(),
 }));
 
-jest.mock("@lichtblick/message-path", () => ({
-  parseMessagePath: jest.fn(),
-}));
-
-afterEach(() => {
-  jest.clearAllMocks();
-});
-
 describe("useSubscriptions", () => {
-  let setSubscriptions: jest.Mock;
-  let globalVariables: any;
-  const defaultConfig: PlotConfig = {
-    paths: [{ value: BasicBuilder.string() }],
-    xAxisVal: BasicBuilder.string(),
-  } as any;
-  const testSubscriber = BasicBuilder.string();
+  const setSubscriptions = jest.fn();
+  const globalVariables = GlobalVariableBuilder.globalVariables();
 
-  const setupMocks = () => {
-    setSubscriptions = jest.fn();
-    (useMessagePipeline as jest.Mock).mockReturnValue(setSubscriptions);
+  (useMessagePipeline as jest.Mock).mockReturnValue(setSubscriptions);
+  (useGlobalVariables as jest.Mock).mockReturnValue({ globalVariables });
 
-    globalVariables = { someVariable: BasicBuilder.string() };
-    (useGlobalVariables as jest.Mock).mockReturnValue({ globalVariables });
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    jest.mock("@lichtblick/message-path", () => ({
-      parseMessagePath: jest.fn(),
-    }));
-  };
+  const setup = (
+    override: {
+      config?: Partial<PlotConfig>;
+      subscriberId?: string;
+    } = {},
+  ) => {
+    const config: PlotConfig = {
+      ...PlotBuilder.config(),
+      ...override.config,
+    } as unknown as PlotConfig;
 
-  const setup = (config: PlotConfig = defaultConfig, subscriberId: string = testSubscriber) => {
-    setupMocks();
-    return renderHook(() => {
-      useSubscriptions(config, subscriberId);
-    });
+    const subscriberId = Object.hasOwn(override, "subscriberId")
+      ? override.subscriberId!
+      : BasicBuilder.string();
+
+    return {
+      ...renderHook(() => {
+        useSubscriptions(config, subscriberId);
+      }),
+      config,
+      subscriberId,
+    };
   };
 
   describe("Setting subscriptions", () => {
-    it("sets subscriptions correctly for series paths", () => {
-      setup();
-      expect(setSubscriptions).toHaveBeenCalledWith(testSubscriber, expect.any(Array));
+    it("should set subscriptions when preload type is full", () => {
+      const parsedPath = BasicBuilder.string();
+      const filledInGlobalVarsPath = BasicBuilder.string();
+      (isReferenceLinePlotPathType as jest.Mock).mockImplementation(() => false);
+      (parseMessagePath as jest.Mock).mockImplementation(() => parsedPath);
+      (fillInGlobalVariablesInPath as jest.Mock).mockImplementation(() => filledInGlobalVarsPath);
+      (pathToSubscribePayload as jest.Mock).mockImplementation(() => "");
+      const { subscriberId, config } = setup({
+        config: {
+          xAxisVal: "timestamp",
+        },
+      });
+
+      expect(setSubscriptions).toHaveBeenCalledWith(subscriberId, expect.any(Array));
+      expect(isReferenceLinePlotPathType).toHaveBeenCalledTimes(config.paths.length);
+      expect(isReferenceLinePlotPathType).toHaveBeenNthCalledWith(1, config.paths[0]);
+      expect(isReferenceLinePlotPathType).toHaveBeenNthCalledWith(2, config.paths[1]);
+      expect(isReferenceLinePlotPathType).toHaveBeenNthCalledWith(3, config.paths[2]);
+      expect(parseMessagePath).toHaveBeenCalledTimes(config.paths.length);
+      expect(parseMessagePath).toHaveBeenNthCalledWith(1, config.paths[0]!.value);
+      expect(parseMessagePath).toHaveBeenNthCalledWith(2, config.paths[1]!.value);
+      expect(parseMessagePath).toHaveBeenNthCalledWith(3, config.paths[2]!.value);
+      expect(fillInGlobalVariablesInPath).toHaveBeenCalledTimes(config.paths.length);
+      expect(fillInGlobalVariablesInPath).toHaveBeenLastCalledWith(parsedPath, globalVariables);
+      expect(pathToSubscribePayload).toHaveBeenCalledTimes(config.paths.length);
+      expect(pathToSubscribePayload).toHaveBeenLastCalledWith(filledInGlobalVarsPath, "full");
     });
 
-    it("sets subscriptions correctly for xAxisPath", () => {
-      const customConfig: PlotConfig = {
-        paths: [],
-        xAxisVal: BasicBuilder.string(),
-        xAxisPath: { value: BasicBuilder.string() },
-      } as any;
+    it("should set subscriptions when preload type is partial", () => {
+      const parsedPath = BasicBuilder.string();
+      const filledInGlobalVarsPath = BasicBuilder.string();
+      (isReferenceLinePlotPathType as jest.Mock).mockImplementation(() => false);
+      (parseMessagePath as jest.Mock).mockImplementation(() => parsedPath);
+      (fillInGlobalVariablesInPath as jest.Mock).mockImplementation(() => filledInGlobalVarsPath);
+      (pathToSubscribePayload as jest.Mock).mockImplementation(() => "");
+      const { subscriberId, config } = setup({
+        config: {
+          xAxisVal: "index",
+        },
+      });
 
-      setup(customConfig);
-
-      expect(setSubscriptions).toHaveBeenCalledWith(testSubscriber, expect.any(Array));
+      expect(parseMessagePath).toHaveBeenCalledTimes(config.paths.length);
+      expect(parseMessagePath).toHaveBeenNthCalledWith(1, config.paths[0]!.value);
+      expect(parseMessagePath).toHaveBeenNthCalledWith(2, config.paths[1]!.value);
+      expect(parseMessagePath).toHaveBeenNthCalledWith(3, config.paths[2]!.value);
+      expect(isReferenceLinePlotPathType).toHaveBeenCalledTimes(config.paths.length);
+      expect(isReferenceLinePlotPathType).toHaveBeenLastCalledWith(config.paths[2]);
+      expect(fillInGlobalVariablesInPath).toHaveBeenCalledTimes(config.paths.length);
+      expect(fillInGlobalVariablesInPath).toHaveBeenLastCalledWith(parsedPath, globalVariables);
+      expect(pathToSubscribePayload).toHaveBeenCalledTimes(config.paths.length);
+      expect(pathToSubscribePayload).toHaveBeenLastCalledWith(filledInGlobalVarsPath, "partial");
+      expect(setSubscriptions).toHaveBeenCalledWith(subscriberId, expect.any(Array));
     });
 
-    it("sets subscriptions correctly for custom xAxisVal", () => {
-      const customXAxisConfig: PlotConfig = {
-        paths: [{ value: BasicBuilder.string() }],
-        xAxisVal: "custom",
-        xAxisPath: { value: BasicBuilder.string() },
-      } as any;
+    it("should set subscriptions when xAxisVal is custom", () => {
+      const parsedPath = BasicBuilder.string();
+      const filledInGlobalVarsPath = BasicBuilder.string();
+      (isReferenceLinePlotPathType as jest.Mock).mockImplementation(() => false);
+      (parseMessagePath as jest.Mock).mockImplementation(() => parsedPath);
+      (fillInGlobalVariablesInPath as jest.Mock).mockImplementation(() => filledInGlobalVarsPath);
+      (pathToSubscribePayload as jest.Mock).mockImplementation(() => "");
 
-      setup(customXAxisConfig);
+      const { subscriberId, config } = setup({
+        config: {
+          paths: [],
+          xAxisVal: "custom",
+        },
+      });
 
-      expect(setSubscriptions).toHaveBeenCalledWith(testSubscriber, expect.any(Array));
+      expect(parseMessagePath).toHaveBeenCalledTimes(1);
+      expect(parseMessagePath).toHaveBeenCalledWith(config.xAxisPath?.value);
+      expect(fillInGlobalVariablesInPath).toHaveBeenCalledTimes(1);
+      expect(fillInGlobalVariablesInPath).toHaveBeenCalledWith(parsedPath, globalVariables);
+      expect(pathToSubscribePayload).toHaveBeenCalledTimes(1);
+      expect(pathToSubscribePayload).toHaveBeenCalledWith(filledInGlobalVarsPath, "full");
+      expect(setSubscriptions).toHaveBeenCalledWith(subscriberId, expect.any(Array));
     });
 
-    it("sets subscriptions correctly for currentCustom xAxisVal", () => {
-      const customXAxisConfig: PlotConfig = {
-        paths: [{ value: BasicBuilder.string() }],
-        xAxisVal: "currentCustom",
-        xAxisPath: { value: BasicBuilder.string() },
-      } as any;
+    it("should set subscriptions when xAxisVal is currentCustom", () => {
+      const parsedPath = BasicBuilder.string();
+      const filledInGlobalVarsPath = BasicBuilder.string();
+      (isReferenceLinePlotPathType as jest.Mock).mockImplementation(() => false);
+      (parseMessagePath as jest.Mock).mockImplementation(() => parsedPath);
+      (fillInGlobalVariablesInPath as jest.Mock).mockImplementation(() => filledInGlobalVarsPath);
+      (pathToSubscribePayload as jest.Mock).mockImplementation(() => "");
 
-      setup(customXAxisConfig);
+      const { subscriberId, config } = setup({
+        config: {
+          paths: [],
+          xAxisVal: "currentCustom",
+        },
+      });
 
-      expect(setSubscriptions).toHaveBeenCalledWith(testSubscriber, expect.any(Array));
+      expect(parseMessagePath).toHaveBeenCalledTimes(1);
+      expect(parseMessagePath).toHaveBeenCalledWith(config.xAxisPath?.value);
+      expect(fillInGlobalVariablesInPath).toHaveBeenCalledTimes(1);
+      expect(fillInGlobalVariablesInPath).toHaveBeenCalledWith(parsedPath, globalVariables);
+      expect(pathToSubscribePayload).toHaveBeenCalledTimes(1);
+      expect(pathToSubscribePayload).toHaveBeenCalledWith(filledInGlobalVarsPath, "partial");
+      expect(setSubscriptions).toHaveBeenCalledWith(subscriberId, expect.any(Array));
     });
   });
 
   describe("Unsubscribing", () => {
     it("unsubscribes on unmount", () => {
-      const { unmount } = setup();
+      const { unmount, subscriberId } = setup();
 
       unmount();
 
-      expect(setSubscriptions).toHaveBeenCalledWith(testSubscriber, []);
+      expect(setSubscriptions).toHaveBeenCalledWith(subscriberId, []);
     });
   });
 
   describe("Handling invalid paths", () => {
     it("does not set subscriptions for invalid paths", () => {
-      const invalidConfig: PlotConfig = {
-        paths: [{ value: BasicBuilder.string() }],
-        xAxisVal: BasicBuilder.string(),
-      } as any;
+      const { subscriberId } = setup({
+        config: {
+          xAxisVal: BasicBuilder.string() as PlotXAxisVal,
+        },
+      });
 
-      setup(invalidConfig);
-
-      expect(setSubscriptions).toHaveBeenCalledWith(testSubscriber, []);
+      expect(setSubscriptions).toHaveBeenCalledWith(subscriberId, []);
     });
 
-    it("does not set subscriptions for reference line paths", () => {
-      const referenceLineConfig: PlotConfig = {
-        paths: [{ value: BasicBuilder.string(), type: "referenceLine" }],
-        xAxisVal: BasicBuilder.string(),
-      } as any;
+    it("should not handle paths when isReferenceLinePlotPathType is true", () => {
+      (isReferenceLinePlotPathType as jest.Mock).mockImplementation(() => true);
+      const { subscriberId } = setup({
+        config: {
+          paths: PlotBuilder.paths(1),
+          xAxisPath: undefined,
+        },
+      });
 
-      setup(referenceLineConfig);
-
-      expect(setSubscriptions).toHaveBeenCalledWith(testSubscriber, []);
-    });
-
-    it("sets subscriptions for non-reference line paths", () => {
-      const nonReferenceLineConfig: PlotConfig = {
-        paths: [{ value: BasicBuilder.string(), type: "series" }],
-        xAxisVal: BasicBuilder.string(),
-      } as any;
-
-      setup(nonReferenceLineConfig);
-
-      expect(setSubscriptions).toHaveBeenCalledWith(testSubscriber, expect.any(Array));
-    });
-
-    it("does not set subscriptions when parsedPath is undefined", () => {
-      (parseMessagePath as jest.Mock).mockReturnValue(undefined);
-      const mockPath = BasicBuilder.string();
-      const configWithInvalidPath: PlotConfig = {
-        paths: [{ value: mockPath }],
-        xAxisVal: BasicBuilder.string(),
-      } as any;
-
-      setup(configWithInvalidPath);
-
-      expect(setSubscriptions).toHaveBeenCalledWith(testSubscriber, []);
-      expect(parseMessagePath).toHaveBeenCalledWith(mockPath);
-    });
-
-    it("does not set subscriptions for undefined xAxisPath", () => {
-      const invalidXAxisConfig: PlotConfig = {
-        paths: [],
-        xAxisVal: BasicBuilder.string(),
-        xAxisPath: undefined,
-      } as any;
-
-      setup(invalidXAxisConfig);
-
-      expect(setSubscriptions).toHaveBeenCalledWith(testSubscriber, []);
+      expect(isReferenceLinePlotPathType).toHaveBeenCalledTimes(1);
       expect(parseMessagePath).not.toHaveBeenCalled();
+      expect(fillInGlobalVariablesInPath).not.toHaveBeenCalled();
+      expect(pathToSubscribePayload).not.toHaveBeenCalled();
+      expect(setSubscriptions).toHaveBeenCalledWith(subscriberId, expect.any(Array));
     });
 
-    it("does not set subscriptions for null xAxisPath", () => {
-      const invalidXAxisConfig: PlotConfig = {
-        paths: [],
-        xAxisVal: BasicBuilder.string(),
-        xAxisPath: ReactNull,
-      } as any;
+    it("should not handle paths when parsedPath is undefined", () => {
+      (isReferenceLinePlotPathType as jest.Mock).mockImplementation(() => false);
+      (parseMessagePath as jest.Mock).mockImplementation(() => undefined);
+      const { subscriberId } = setup({
+        config: {
+          paths: PlotBuilder.paths(1),
+          xAxisPath: undefined,
+        },
+      });
 
-      setup(invalidXAxisConfig);
-
-      expect(setSubscriptions).toHaveBeenCalledWith(testSubscriber, []);
-      expect(parseMessagePath).not.toHaveBeenCalled();
-    });
-
-    it("does not set subscriptions when parsedPath is null", () => {
-      (parseMessagePath as jest.Mock).mockReturnValue(ReactNull);
-      const mockPath = BasicBuilder.string();
-      const configWithInvalidPath: PlotConfig = {
-        paths: [{ value: mockPath }],
-        xAxisVal: BasicBuilder.string(),
-      } as any;
-
-      setup(configWithInvalidPath);
-
-      expect(setSubscriptions).toHaveBeenCalledWith(testSubscriber, []);
-      expect(parseMessagePath).toHaveBeenCalledWith(mockPath);
+      expect(isReferenceLinePlotPathType).toHaveBeenCalledTimes(1);
+      expect(parseMessagePath).toHaveBeenCalledTimes(1);
+      expect(fillInGlobalVariablesInPath).not.toHaveBeenCalled();
+      expect(pathToSubscribePayload).not.toHaveBeenCalled();
+      expect(setSubscriptions).toHaveBeenCalledWith(subscriberId, expect.any(Array));
     });
   });
 });
