dead073ed10c4eb3cd89dd5ebd06cc97a2ea8a8b
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2025-01-30 11:24:28 +0000

    removed unnecessary imports

--- packages/suite-base/src/panels/Indicator/Indicator.test.tsx
+++ packages/suite-base/src/panels/Indicator/Indicator.test.tsx
@@ -3,12 +3,10 @@
 // SPDX-License-Identifier: MPL-2.0
 
 import { userEvent } from "@storybook/testing-library";
-import { act, render, screen, waitFor } from "@testing-library/react";
-import * as _ from "lodash-es";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import { parseMessagePath } from "@lichtblick/message-path";
-import { Immutable, PanelExtensionContext, RenderState } from "@lichtblick/suite";
+import { PanelExtensionContext } from "@lichtblick/suite";
 import MockPanelContextProvider from "@lichtblick/suite-base/components/MockPanelContextProvider";
 import { PanelExtensionAdapter } from "@lichtblick/suite-base/components/PanelExtensionAdapter";
 import Indicator from "@lichtblick/suite-base/panels/Indicator";
@@ -17,7 +15,6 @@ import { getMatchingRule } from "@lichtblick/suite-base/panels/Indicator/getMatc
 import {
   IndicatorConfig,
   IndicatorProps,
-  IndicatorRule,
   IndicatorStyle,
 } from "@lichtblick/suite-base/panels/Indicator/types";
 import PanelSetup from "@lichtblick/suite-base/stories/PanelSetup";
@@ -77,14 +74,7 @@ describe("Indicator Component", () => {
     };
 
     const saveConfig = () => {};
-    // const initPanel = jest.fn();
-    const initPanel = jest.fn((context: PanelExtensionContext) => {
-      context.watch("currentFrame");
-      context.watch("didSeek");
-      context.watch("variables");
-      context.subscribe([{ topic: BasicBuilder.string(), preload: false }]);
-      context.onRender = () => {};
-    });
+    const initPanel = jest.fn();
 
     const ui: React.ReactElement = (
       <ThemeProvider isDark>
@@ -161,74 +151,4 @@ describe("Indicator Component", () => {
       expect(screen.getByText(matchingRule.label)).toBeTruthy();
     },
   );
-
-  // it("calls context.saveState on config change", () => {
-  //   const newConfig = IndicatorBuilder.config();
-  //   const { props } = setup();
-
-  //   props.context.saveState(newConfig);
-
-  //   const saveState = jest.spyOn(props.context, "saveState");
-  //   expect(saveState).toHaveBeenCalledWith(newConfig);
-  // });
-
-  // it("calls context.setDefaultPanelTitle with undefined when path is empty string", () => {
-  //   const { props } = setup({
-  //     configOverride: { path: "" },
-  //   });
-
-  //   props.context.setDefaultPanelTitle(undefined);
-
-  //   const setDefaultPanelTitle = jest.spyOn(props.context, "setDefaultPanelTitle");
-  //   expect(setDefaultPanelTitle).toHaveBeenCalledWith(undefined);
-  //   expect(setDefaultPanelTitle).toHaveBeenCalledTimes(1);
-  // });
-
-  // it("calls context.setDefaultPanelTitle with path when path is populated", async () => {
-  //   const { props, config } = setup({
-  //     configOverride: { path: BasicBuilder.string() },
-  //   });
-
-  //   // props.context.setDefaultPanelTitle(config.path);
-  //   console.log("AQUIIII", (props.context.setDefaultPanelTitle as jest.Mock).mock.calls);
-  //   await act(async () => {});
-
-  //   const setDefaultPanelTitle = jest.spyOn(props.context, "setDefaultPanelTitle");
-  //   expect(setDefaultPanelTitle).toHaveBeenCalledWith(config.path);
-  //   expect(setDefaultPanelTitle).toHaveBeenCalledTimes(1);
-  // });
-
-  // it("should subscribe when topicName is defined", async () => {
-  //   const parsedPath = { topicName: "test-topic" };
-  //   (parseMessagePath as jest.Mock).mockReturnValue(parsedPath);
-  //   const { props, config, rerender, saveConfig, initPanel, unmount } = setup();
-
-  //   // eslint-disable-next-line @typescript-eslint/no-deprecated
-  //   props.context.subscribe(parsedPath);
-
-  //   console.log("subscribe CALLS: ", (props.context.subscribe as jest.Mock).mock.calls);
-
-  //   const subscribe = jest.spyOn(props.context, "subscribe");
-  //   // const unsubscribeAll = jest.spyOn(props.context, "unsubscribeAll");
-  //   expect(subscribe).toHaveBeenCalled();
-  //   // unmount();
-  // });
-
-  // it("subscribes to topic when parsedPath.topicName is defined", () => {
-  //   const { props, parsedPath } = setup();
-
-  //   props.context.subscribe([{ topic: parsedPath.topicName, preload: false }]);
-
-  //   const subscribe = jest.spyOn(props.context, "subscribe");
-  //   expect(subscribe).toHaveBeenCalledWith([{ topic: parsedPath.topicName, preload: false }]);
-  //   expect(subscribe).toHaveBeenCalledTimes(1);
-  // });
-
-  // it("handles context.onRender with didSeek and renderState.variables", () => {
-  //   const { props } = setup();
-  //   const renderState = { didSeek: true, variables: BasicBuilder.stringMap() };
-
-  //   props.context.onRender!(renderState, jest.fn());
-  //   expect(props.context.onRender).toBeDefined();
-  // });
 });
