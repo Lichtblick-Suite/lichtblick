f36a8844b2dbe1650f816ea8d7d9e05db2dcaa05
Alexandre Neuwald <alexandre.neuwald@criticaltechworks.com>
2025-02-11 12:09:18 +0000

    fix lint

--- packages/suite-base/src/players/IterablePlayer/shared/MultiIterableSource.test.ts
+++ packages/suite-base/src/players/IterablePlayer/shared/MultiIterableSource.test.ts
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: Copyright (C) 2023-2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
 // SPDX-License-Identifier: MPL-2.0
 
-import { get } from "http";
-
 import { MultiSource } from "@lichtblick/suite-base/players/IterablePlayer/shared/types";
 import BasicBuilder from "@lichtblick/suite-base/testing/builders/BasicBuilder";
 import InitilizationSourceBuilder from "@lichtblick/suite-base/testing/builders/InitilizationSourceBuilder";
