720c656164e0fc3006d229c25c64f3ca9c833948
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2025-02-14 16:38:24 +0000

    moved new code into branch

--- packages/suite-base/src/context/Workspace/useOpenFile.tsx
+++ packages/suite-base/src/context/Workspace/useOpenFile.tsx
@@ -15,6 +15,8 @@ import {
 } from "@lichtblick/suite-base/context/PlayerSelectionContext";
 import showOpenFilePicker from "@lichtblick/suite-base/util/showOpenFilePicker";
 
+export const MCAP_ACCEPT_TYPE = "application/octet-stream";
+
 export function useOpenFile(sources: readonly IDataSourceFactory[]): () => Promise<void> {
   const { selectSource } = usePlayerSelection();
 
@@ -39,7 +41,7 @@ export function useOpenFile(sources: readonly IDataSourceFactory[]): () => Promi
       types: [
         {
           description: allExtensions.join(", "),
-          accept: { "application/octet-stream": allExtensions },
+          accept: { [MCAP_ACCEPT_TYPE]: allExtensions },
         },
       ],
     });
@@ -75,6 +77,11 @@ export function useOpenFile(sources: readonly IDataSourceFactory[]): () => Promi
         source.type === "file" &&
         source.supportedFileTypes.includes(extension!),
     );
+
+    if (matchingSources.length === 0) {
+      throwErrorAndSnackbar(`Cannot find a source to handle files with extension ${extension}`);
+    }
+
     if (matchingSources.length > 1) {
       throwErrorAndSnackbar(
         `The file extension "${extension}" is not supported. Please select files with the following extensions: ${allExtensions.join(", ")}.`,
@@ -88,11 +95,9 @@ export function useOpenFile(sources: readonly IDataSourceFactory[]): () => Promi
       throwErrorAndSnackbar(`The application only support multiple files for MCAP extension.`);
     }
 
-    const foundSource = matchingSources[0];
-    if (!foundSource) {
-      throwErrorAndSnackbar(`Cannot find a source to handle files with extension ${extension}`);
-    }
-
-    selectSource(foundSource!.id, { type: "file", files: processedFiles.map((item) => item.file) });
+    selectSource(matchingSources[0]!.id, {
+      type: "file",
+      files: processedFiles.map((item) => item.file),
+    });
   }, [allExtensions, selectSource, sources]);
 }
