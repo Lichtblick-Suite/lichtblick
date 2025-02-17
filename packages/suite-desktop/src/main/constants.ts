7eabff2abdf8b753d21e93a02b902ead929a1e17
ctw-joao-luis <Joao.Luis@ctw.bmwgroup.com>
2025-02-17 09:37:39 +0000

    CLI flag to open file(s) or a directory on Lichtblick (#365)
    **User-Facing Changes**
    With this feature Lichtblick users will be able to open .mcap files
    directly from the CLI (something that was already possible, but now its
    doable using the --source= parameter).
    What's completely new is the ability to also pass paths to directories
    and Lichtblick will search for .mcap files inside of it.
    
    ### **Its important to note that, for now, this change will only be
    available for MCAP files**
    
    
    
    For instance on Windowns if you pass something like: 
    
    `lichtblick.exe
    --source=~/Folder/mcapFile.mcap,~/Folder2/mcapFile2.mcap`
    
    Lichtblick will open both files 
    
    If you pass something like: 
    
    `lichtblick.exe --source=~/Folder`
    
    Lichblick will search for any .mcap file inside that folder and try to
    open it.
    
    
    **Description**
    
    The command-line arguments are first captured in process.argv and
    filtered for the --source flag.
    If multiple paths are specified, they are split using a comma as a
    delimiter and converted into absolute paths, handling home directory
    expansion if necessary. If more than one path is detected, they are
    treated as separate files and returned as a list. If only one path is
    provided and it is a directory, the logic attempts to extract .mcap
    files from it.
    This ensures proper handling of both individual files and directories.
    
    <img width="839" alt="image"
    src="https://github.com/user-attachments/assets/8014c464-77b7-4009-a121-1a12a41afe32"
    />
    
    <!-- link relevant GitHub issues -->
    <!-- add `docs` label if this PR requires documentation updates -->
    <!-- add relevant metric tracking for experimental / new features -->
    
    **Checklist**
    
    - [X] The web version was tested and it is running ok
    - [X] The desktop version was tested and it is running ok
    - [X] This change is covered by unit tests
    - [X] Files constants.ts, types.ts and *.style.ts have been checked and
    relevant code snippets have been relocated
    
    ---------
    
    Signed-off-by: ctw-joao-luis <Joao.Luis@ctw.bmwgroup.com>
    Co-authored-by: Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>

---
+++ packages/suite-desktop/src/main/constants.ts
@@ -0,0 +1,4 @@
+// SPDX-FileCopyrightText: Copyright (C) 2023-2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
+// SPDX-License-Identifier: MPL-2.0
+
+export const allowedExtensions = [".mcap"];
