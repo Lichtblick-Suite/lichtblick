1e1dc4cd96b72dfba91ad01709182b5ce7dafc4e
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2025-03-11 21:30:21 +0000

    removed unnused imports

--- packages/suite-base/src/providers/helpers/buildContributionPoints.test.ts
+++ packages/suite-base/src/providers/helpers/buildContributionPoints.test.ts
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: Copyright (C) 2023-2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
 // SPDX-License-Identifier: MPL-2.0
 
-import Logger from "@lichtblick/log";
 import {
   RegisterMessageConverterArgs,
   TopicAliasFunction,
