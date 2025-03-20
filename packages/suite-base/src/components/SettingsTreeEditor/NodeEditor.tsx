c4f850cc5b92d0e84a5a57ff7665897a6ef9cf87
Alexandre Neuwald <alexandre.neuwald@criticaltechworks.com>
2025-03-20 16:10:22 +0000

    put exclamation back

--- packages/suite-base/src/components/SettingsTreeEditor/NodeEditor.tsx
+++ packages/suite-base/src/components/SettingsTreeEditor/NodeEditor.tsx
@@ -159,7 +159,8 @@ function NodeEditorComponent(props: NodeEditorProps): React.JSX.Element {
   );
 
   const fieldEditors = useMemo(
-    () => entries.filter(([, field]) => field).map(([key, field]) => renderFieldEditor(key, field)),
+    () =>
+      entries.filter(([, field]) => field).map(([key, field]) => renderFieldEditor(key, field!)),
     [entries, renderFieldEditor],
   );
 
